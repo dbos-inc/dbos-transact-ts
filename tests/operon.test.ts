403e6114de2d4f83866cfa7ab6f206649d1c08e0
Qian Li <qian.li@dbos.dev>
2023-07-13 15:05:39 -0700

    clean up

--- tests/operon.test.ts
+++ tests/operon.test.ts
@@ -2,59 +2,52 @@ import { Operon, FunctionContext, registerFunction,registerWorkflow, WorkflowCon
 
 describe('testing operon', () => {
   test('should run a simple workflow', async() => {
+    const username = process.env.DB_USER || 'postgres';
     const operon: Operon = new Operon({
-      user: process.env.DB_USER || 'postgres',
+      user: username,
       password: process.env.DB_PASSWORD || 'dbos',
       connectionTimeoutMillis:  3000
     });
 
     const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
-      console.log("hello from function: " + name);
-      const { rows }= await functionCtxt.client.query('select current_user;');
+      const { rows }= await functionCtxt.client.query(`select current_user from current_user where current_user=$1;`, [name]);
       return JSON.stringify(rows[0]);
     });
 
     const testWorkflow = registerWorkflow(async (workflowCtxt: WorkflowContext, name: string) => {
-      console.log("hello from workflow: " + name);
       const funcResult: string = await testFunction(workflowCtxt, name);
-      console.log("workflow got function output: " + funcResult);
       return funcResult;
     });
 
-    const workflowResult: string = await testWorkflow(operon, 'dbos');
-    console.log(workflowResult);
+    const workflowResult: string = await testWorkflow(operon, username);
 
-    expect(JSON.parse(workflowResult)).toEqual({"current_user": process.env.DB_USER || 'postgres'});
+    expect(JSON.parse(workflowResult)).toEqual({"current_user": username});
     
     await operon.pool.end();
   });
 
   test('tight loop function calls', async() => {
+    const username = process.env.DB_USER || 'postgres';
     const operon: Operon = new Operon({
-      user: process.env.DB_USER || 'postgres',
+      user: username,
       password: process.env.DB_PASSWORD || 'dbos',
       connectionTimeoutMillis:  3000,
       max: 10
     });
 
     const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
-      console.log("hello from function: " + name);
-      const { rows }= await functionCtxt.client.query('select current_user;');
+      const { rows }= await functionCtxt.client.query(`select current_user from current_user where current_user=$1;`, [name]);
       return JSON.stringify(rows[0]);
     });
 
     const testWorkflow = registerWorkflow(async (workflowCtxt: WorkflowContext, name: string) => {
-      console.log("hello from workflow: " + name);
       const funcResult: string = await testFunction(workflowCtxt, name);
-      console.log("workflow got function output: " + funcResult);
       return funcResult;
     });
 
     for (let i = 0; i < 100; i++) {
-      const workflowResult: string = await testWorkflow(operon, 'dbos');
-      console.log(workflowResult);
-      console.log(operon.pool.totalCount);
-      expect(JSON.parse(workflowResult)).toEqual({"current_user": process.env.DB_USER || 'postgres'});
+      const workflowResult: string = await testWorkflow(operon, username);
+      expect(JSON.parse(workflowResult)).toEqual({"current_user": username});
     }
     await operon.pool.end();
   });
