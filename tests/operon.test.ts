9a8d1947cc4886ed88b6b5909bb6502223f8370b
Qian Li <qian.li@dbos.dev>
2023-07-13 14:49:38 -0700

    Add tight loop test

--- tests/operon.test.ts
+++ tests/operon.test.ts
@@ -28,4 +28,34 @@ describe('testing operon', () => {
     
     await operon.pool.end();
   });
+
+  test('tight loop function calls', async() => {
+    const operon: Operon = new Operon({
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || 'dbos',
+      connectionTimeoutMillis:  3000,
+      max: 10
+    });
+
+    const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
+      console.log("hello from function: " + name);
+      const { rows }= await functionCtxt.client.query('select current_user;');
+      return JSON.stringify(rows[0]);
+    });
+
+    const testWorkflow = registerWorkflow(async (workflowCtxt: WorkflowContext, name: string) => {
+      console.log("hello from workflow: " + name);
+      const funcResult: string = await testFunction(workflowCtxt, name);
+      console.log("workflow got function output: " + funcResult);
+      return funcResult;
+    });
+
+    for (let i = 0; i < 100; i++) {
+      const workflowResult: string = await testWorkflow(operon, 'dbos');
+      console.log(workflowResult);
+      console.log(operon.pool.totalCount);
+      expect(JSON.parse(workflowResult)).toEqual({"current_user": process.env.DB_USER || 'postgres'});
+    }
+    await operon.pool.end();
+  });
 })
