2b13121c112bfbfaf36c2ab8c9ddea5cc222b242
Qian Li <qian.li@dbos.dev>
2023-07-14 15:14:24 -0700

    add test, refactor tests

--- tests/operon.test.ts
+++ tests/operon.test.ts
@@ -2,16 +2,28 @@
 
 import { Operon, FunctionContext, registerFunction,registerWorkflow, WorkflowContext } from "src/";
 
+let operon: Operon;
+const username: string = process.env.DB_USER || 'postgres';
+
 describe('operon-tests', () => {
-  test('simple-function', async() => {
-    const username = process.env.DB_USER || 'postgres';
-    const operon: Operon = new Operon({
+
+  beforeEach(async () => {
+    operon = new Operon({
       user: username,
       password: process.env.DB_PASSWORD || 'dbos',
       connectionTimeoutMillis:  3000
     });
     await operon.resetOperonTables();
+    await operon.pool.query("DROP TABLE IF EXISTS OperonKv;");
+    await operon.pool.query("CREATE TABLE IF NOT EXISTS OperonKv (id SERIAL PRIMARY KEY, value TEXT);");
+  });
 
+  afterEach(async () => {
+    await operon.pool.end();
+  });
+
+
+  test('simple-function', async() => {
     const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
       const { rows } = await functionCtxt.client.query(`select current_user from current_user where current_user=$1;`, [name]);
       return JSON.stringify(rows[0]);
@@ -25,20 +37,10 @@ describe('operon-tests', () => {
     const workflowResult: string = await testWorkflow(operon, {}, username);
 
     expect(JSON.parse(workflowResult)).toEqual({"current_user": username});
-    await operon.pool.end();
   });
 
 
   test('tight-loop', async() => {
-    const username = process.env.DB_USER || 'postgres';
-    const operon: Operon = new Operon({
-      user: username,
-      password: process.env.DB_PASSWORD || 'dbos',
-      connectionTimeoutMillis:  3000,
-      max: 10
-    });
-    await operon.resetOperonTables();
-
     const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
       const { rows }= await functionCtxt.client.query(`select current_user from current_user where current_user=$1;`, [name]);
       return JSON.stringify(rows[0]);
@@ -53,24 +55,10 @@ describe('operon-tests', () => {
       const workflowResult: string = await testWorkflow(operon, {idempotencyKey: String(i)}, username);
       expect(JSON.parse(workflowResult)).toEqual({"current_user": username});
     }
-
-    await operon.pool.end();
   });
   
 
   test('abort-function', async() => {
-    const username = process.env.DB_USER || 'postgres';
-    const operon: Operon = new Operon({
-      user: username,
-      password: process.env.DB_PASSWORD || 'dbos',
-      connectionTimeoutMillis:  3000,
-      max: 2
-    });
-    await operon.resetOperonTables();
-
-    await operon.pool.query("DROP TABLE IF EXISTS OperonKv;");
-    await operon.pool.query("CREATE TABLE IF NOT EXISTS OperonKv (id SERIAL PRIMARY KEY, value TEXT);");
-
     const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
       const { rows }= await functionCtxt.client.query("INSERT INTO OperonKv(value) VALUES ($1) RETURNING id", [name]);
       if (name === "fail") {
@@ -103,7 +91,40 @@ describe('operon-tests', () => {
     // Should not appear in the database.
     const workflowResult: number = await testWorkflow(operon, {}, "fail");
     expect(workflowResult).toEqual(-1);
+  });
 
-    await operon.pool.end();
+
+  test('oaoo-simple', async() => {
+    const testFunction = registerFunction(async (functionCtxt: FunctionContext, name: string) => {
+      const { rows }= await functionCtxt.client.query("INSERT INTO OperonKv(value) VALUES ($1) RETURNING id", [name]);
+      return Number(rows[0].id);
+    });
+
+    const testFunctionRead = registerFunction(async (functionCtxt: FunctionContext, id: number) => {
+      const { rows }= await functionCtxt.client.query("SELECT id FROM OperonKv WHERE id=$1", [id]);
+      if (rows.length > 0) {
+        return Number(rows[0].id);
+      } else {
+        // Cannot find, return a negative number.
+        return -1;
+      }
+    });
+
+    const testWorkflow = registerWorkflow(async (workflowCtxt: WorkflowContext, name: string) => {
+      const funcResult: number = await testFunction(workflowCtxt, name);
+      const checkResult: number = await testFunctionRead(workflowCtxt, funcResult);
+      return checkResult;
+    });
+
+    for (let i = 0; i < 10; i++) {
+      const workflowResult: number = await testWorkflow(operon, {idempotencyKey: "testkey" + String(i)}, username);
+      expect(workflowResult).toEqual(i + 1);
+    }
+
+    // Rerun with the same idempotency key should return the same output.
+    for (let i = 0; i < 10; i++) {
+      const workflowResult: number = await testWorkflow(operon, {idempotencyKey: "testkey" + String(i)}, username);
+      expect(workflowResult).toEqual(i + 1);
+    }
   });
 });
