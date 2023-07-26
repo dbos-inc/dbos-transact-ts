c7562afbca96b8445ba2d010993d4263d3f46a81
Qian Li <qian.li@dbos.dev>
2023-07-26 11:11:46 -0700

    remove redundant msg

--- src/error.ts
+++ src/error.ts
@@ -1,10 +1,8 @@
 export class OperonError extends Error {
-  readonly msg: string;
   readonly code: number;
 
   constructor(code: number, msg: string) {
     super(msg);
     this.code = code;
-    this.msg = msg;
   }
 }
