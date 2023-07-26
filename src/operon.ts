ce5e9b5ddc5093b4c428ce21cce308375019091a
maxdml <maxdml@dbos.dev>
2023-07-26 13:15:36 -0700

    use a module to comply with ES5 preferred style

--- src/operon.ts
+++ src/operon.ts
@@ -1,11 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Pool, PoolConfig } from 'pg';
+import { OperonConfig } from './operon.config';
+import { Pool } from 'pg';
 import { OperonWorkflow, WorkflowContext, WorkflowParams } from './workflow';
 import { v1 as uuidv1 } from 'uuid';
 import { OperonTransaction } from './transaction';
-import { OperonError } from './error';
-import YAML from 'yaml'
-import fs from 'fs'
 
 export interface operon__FunctionOutputs {
     workflow_id: string;
@@ -19,49 +17,10 @@ export interface operon__Notifications {
   message: string;
 }
 
-namespace OperonConfig {
-    const configFile: string = "operon-config.yaml";
-
-    export class Config {
-        readonly pool: Pool;
-        // We will add operonRoles: Role[] here in a next PR
-        // We will add a "debug" flag here to be used in other parts of the codebase
-
-        constructor() {
-            // Check if configFile is a valid file
-            fs.stat(configFile, (error: NodeJS.ErrnoException | null, stats: fs.Stats) => {
-                if (error) {
-                    throw new OperonError(`Config file ${configFile} does not exist`);
-                } else if (!stats.isFile()) {
-                    throw new OperonError(`Config file ${configFile} is not a valid file`);
-                }
-            });
-
-            // Logic to parse configFile
-            const configFileContent: string = fs.readFileSync(configFile, 'utf8')
-            const parsedConfig: any = YAML.parse(configFileContent) // XXX We could maybe have a typed format for the config...
-            const dbConfig: any = parsedConfig.database;
-            // Use config provided password first then attempt to parse env vars
-            const dbPassword: string =
-                dbConfig.password ||
-                process.env.DB_PASSWORD ||
-                process.env.PGPASSWORD;
-            this.pool = new Pool({
-                host: dbConfig.hostname,
-                port: dbConfig.port,
-                user: dbConfig.username,
-                password: dbPassword,
-                connectionTimeoutMillis: dbConfig.connectionTimeoutMillis,
-                database: 'postgres', // For now we use the default postgres database
-            });
-        }
-    }
-}
-
 export class Operon {
   pool: Pool;
   constructor() {
-    const config = new OperonConfig.Config();
+    const config = new OperonConfig();
     this.pool = config.pool;
   }
 
