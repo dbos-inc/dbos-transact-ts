7fb346e1ba30f761a73da1d25eea34dd97971566
maxdml <maxdml@dbos.dev>
2023-07-27 14:36:59 -0700

    update variable name

--- src/error.ts
+++ src/error.ts
@@ -10,9 +10,9 @@ export class OperonError extends Error {
 
 const PermissionDeniedError = 2;
 export class OperonPermissionDeniedError extends OperonError {
-  constructor(runAs: string, workflowName: string | undefined, workflowId: string | undefined) {
+  constructor(userName: string, workflowName: string | undefined, workflowId: string | undefined) {
     const msg =
-        `User ${runAs} does not have permission to run workflow ${workflowName} (id: ${workflowId})`;
+        `User ${userName} does not have permission to run workflow ${workflowName} (id: ${workflowId})`;
     super(msg, PermissionDeniedError);
   }
 }
