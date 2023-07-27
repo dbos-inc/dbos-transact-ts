38f1b6206fd9580a4f905697a57ab9468da59d0e
maxdml <maxdml@dbos.dev>
2023-07-27 10:24:07 -0700

    fill back imports

--- src/operon.ts
+++ src/operon.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { OperonConfig } from './operon.config';
-import { Pool } from 'pg';
+import { Pool, PoolClient, Notification } from 'pg';
 import { OperonWorkflow, WorkflowContext, WorkflowParams } from './workflow';
 import { v1 as uuidv1 } from 'uuid';
 import { OperonTransaction } from './transaction';
