1882d7beb999e24926d2d216c1dcc9b6746b74e4
maxdml <maxdml@dbos.dev>
2023-07-27 12:25:12 -0700

    new export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { Operon } from './operon';
 export { OperonConfig, DatabaseConfig } from './operon.config';
 export { TransactionContext } from './transaction';
-export { WorkflowContext, WorkflowParams } from './workflow';
+export { WorkflowContext, WorkflowConfig, WorkflowParams } from './workflow';
 export { CommunicatorContext } from './communicator';
 export { OperonError } from './error';
 export { User } from './users';
