3369c98ded9b260ebb9b0045f5cea2c3a0d3375c
Chuck B <chuck.bear@dbos.dev>
2023-08-14 18:21:29 -0400

    Add decorators to index

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,19 @@ export {
   OperonTopicPermissionDeniedError,
   OperonWorkflowPermissionDeniedError
 } from './error';
+export {
+  OperonFieldType,
+  OperonDataType,
+  OperonMethodRegistrationBase,
+  forEachMethod,
+  LogLevel,
+  LogMask,
+  LogEventType,
+  // BaseLogEvent, // Would be OK to export for some uses I think?
+
+  required,
+  skipLogging,
+  paramName,
+  loglevel,
+  logged,
+} from './decorators';
