b16e16e92fb47026dc9dd22d6f38b231eaa3add0
maxdml <maxdml@dbos.dev>
2023-08-18 09:53:48 -0700

    typo

--- src/system_database.ts
+++ src/system_database.ts
@@ -122,7 +122,7 @@ export class PostgresSystemDatabase implements SystemDatabase {
     try {
       await this.pool.query("INSERT INTO operon.operation_outputs (workflow_uuid, function_id, error) VALUES ($1, $2, $3);", [workflowUUID, functionID, serialErr]);
     } catch (error) {
-      const err: DatabaseError = error as DatabaseError;=
+      const err: DatabaseError = error as DatabaseError;
       if (err.code === "40001" || err.code === "23505") {
         // Serialization and primary key conflict (Postgres).
         throw new OperonWorkflowConflictUUIDError(workflowUUID);
