af53636dbd4d7e0801700053668647cd7910ef4f
Peter Kraft <petereliaskraft@gmail.com>
2023-08-23 16:49:29 -0700

    comments

--- src/provenance/provenance_daemon.ts
+++ src/provenance/provenance_daemon.ts
@@ -21,6 +21,19 @@ interface wal2jsonChange {
   columnvalues: string[];
 }
 
+
+/**
+ * A class implementing a daemon which collects and exports provenance information,
+ * specifically a record of all INSERTs, UPDATEs, and DELETEs in the target database and their associated transactions.
+ * Only one daemon is needed per database. The daemon need not run in the same process as Operon.
+ * The daemon has three requirements and will fail to launch if either is not met:
+ *  1.  The postgres databsae must be configured with wal_level=logical.
+ *  2.  An open replication slot must be available.
+ *  3.  The wal2json Postgres plugin must be installed. It is installed by default on most cloud databases, including RDS.
+ *
+ * Because the daemon depends on Postgres logical replication, it can only export information on updates and deletes in tables
+ * whose rows are uniquely identifiable, either because the table has a primary key or a replica identity.
+ */
 export class ProvenanceDaemon {
   readonly client;
   readonly daemonID;
@@ -28,6 +41,10 @@ export class ProvenanceDaemon {
   readonly telemetryCollector: TelemetryCollector;
   initialized = false;
 
+  /**
+   * @param operonConfig An Operon config defining exporters and database connection information.
+   * @param slotName  The name of a logical replication slot.  This slot is persistent and must be deleted if the daemon is no longer to be used.
+   */
   constructor(operonConfig: OperonConfig, readonly slotName: string) {
     this.client = new Client(operonConfig.poolConfig);
     this.daemonID = setInterval(() => {
@@ -50,7 +67,7 @@ export class ProvenanceDaemon {
   async start() {
     await this.client.connect();
     // Create a logical replication slot with the given name.
-    try {
+    try { // TODO: Make this robust to daemon crashes, so records are not consumed until they've been exported.
       await this.client.query("SELECT pg_create_logical_replication_slot($1, 'wal2json');", [this.slotName]);
     } catch (error) {
       const err: DatabaseError = error as DatabaseError;
