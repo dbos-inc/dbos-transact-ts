d699484800b1c62bda8e7982129ff224a3295dc8
Harry Pierson <harry.pierson@dbos.dev>
2023-09-20 11:55:11 -0700

    fix @ArgSource

--- src/httpServer/handler.ts
+++ src/httpServer/handler.ts
@@ -29,41 +29,14 @@ export interface HttpEnpoint {
   url: string,
 }
 
-// export interface OperonHandlerRegistrationBase extends OperonMethodRegistrationBase
-// {
-//   apiType: APITypes;
-//   apiURL: string;
-//   args: OperonHandlerParameter[];
-// }
-
-// export class OperonHandlerRegistration<This, Args extends unknown[], Return> extends OperonMethodRegistration<This, Args, Return> {
-//   apiType: APITypes = APITypes.GET;
-//   apiURL: string = "";
-
-//   args: OperonHandlerParameter[] = [];
-//   constructor(origFunc: (this: This, ...args: Args) => Promise<Return>) {
-//     super(origFunc);
-//   }
-// }
-
-// export class OperonHandlerParameter extends OperonParameter {
-//   argSource: ArgSources = ArgSources.DEFAULT;
-
-//   // eslint-disable-next-line @typescript-eslint/ban-types
-//   constructor(idx: number, at: Function) {
-//     super(idx, at);
-//   }
-// }
-
 export function GetApi(url: string) {
-  function apidec<This, Ctx extends OperonContext, Args extends unknown[], Return>(
+  return function <This, Ctx extends OperonContext, Args extends unknown[], Return>(
     target: object,
     propertyKey: string,
     _inDescriptor: TypedPropertyDescriptor<(this: This, ctx: Ctx, ...args: Args) => Promise<Return>>
   ) {
     Reflect.defineMetadata("operon:http-endpoint", { type: APITypes.GET, url}, target, propertyKey);
   }
-  return apidec;
 }
 
 export function PostApi(url: string) {
@@ -79,6 +52,9 @@ export function PostApi(url: string) {
 export function ArgSource(source: ArgSources) {
   return function (target: object, propertyKey: string | symbol, parameterIndex: number) {
     const params = Reflect.getOwnMetadata("operon:http-arg-source", target, propertyKey) as Array<ArgSources> ?? [];
+    if (params.length === 0) {
+      Reflect.defineMetadata("operon:http-arg-source", params, target, propertyKey);
+    }
     params[parameterIndex] = source;
   };
 }
