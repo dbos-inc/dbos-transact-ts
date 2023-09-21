a1bb55f540c029cb5b086195a6eec754c1449c1c
Peter Kraft <petereliaskraft@gmail.com>
2023-09-21 14:51:25 -0700

    fix linter

--- src/operon-runtime/runtime.ts
+++ src/operon-runtime/runtime.ts
@@ -1,16 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Operon } from '../operon';
 import { OperonHttpServer } from '../httpServer/server';
 import * as fs from 'fs';
+import { isObject } from 'lodash';
+
+interface ModuleExports {
+  [key: string]: any;
+}
 
 /**
  * Load an application's Operon functions, assumed to be in src/userFunctions.ts (which is compiled to dist/userFunction.js).
  */
-function loadFunctions() {
+function loadFunctions() : ModuleExports | null {
   const workingDirectory = process.cwd();
   console.log("Current directory:", workingDirectory);
   const userFunctions = workingDirectory + "/dist/userFunctions.js";
   if (fs.existsSync(userFunctions)) {
-    return require(userFunctions);
+    /* eslint-disable-next-line @typescript-eslint/no-var-requires */
+    return require(userFunctions) as ModuleExports;
   } else {
     return null;
   }
@@ -26,9 +33,11 @@ export async function startServer(port: number) {
     return;
   }
 
-  const classes: any[] = [];
-  for (let key in exports) {
-    classes.push(exports[key]);
+  const classes: object[] = [];
+  for (const key in exports) {
+    if (isObject(exports[key])) {
+      classes.push(exports[key] as object);
+    }
   }
   // Initialize Operon.
   const operon: Operon = new Operon();
