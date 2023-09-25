53001d0dcb9aa1afd79c21aff7cc04bc57f8386f
Peter Kraft <petereliaskraft@gmail.com>
2023-09-25 21:27:46 +0000

    more deploy

--- src/operon-runtime/deploy.ts
+++ src/operon-runtime/deploy.ts
@@ -1,20 +1,38 @@
 import axios from "axios";
 import { execSync } from "child_process";
+import fs from 'fs';
+import FormData from 'form-data';
 
 export async function deploy(appName: string, host: string) {
 
-    const response = await axios.post(
-        `http://${host}:8080/application/register`,
-        {
-            name: appName,
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
+    try {
+        const register = await axios.post(
+            `http://${host}:8080/application/register`,
+            {
+                name: appName,
             },
-        },
-    );
-    const uuid = response.data as string;
-    execSync(`mkdir -p operon_deploy`);
-    execSync(`zip -ry operon_deploy/${uuid}.zip ./* -x "operon_deploy/*"`)
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            },
+        );
+        const uuid = register.data as string;
+        execSync(`mkdir -p operon_deploy`);
+        execSync(`zip -ry operon_deploy/${uuid}.zip ./* -x "operon_deploy/*"`)
+
+        const formData = new FormData();
+        formData.append('app_archive', fs.createReadStream(`operon_deploy/${uuid}.zip`));
+
+        const upload = await axios.post(`http://localhost:8080/application/${uuid}`, formData, {
+            headers: {
+                ...formData.getHeaders(),
+            },
+        });
+        console.log(`Deploying ${appName} successful`);
+        console.log(`${appName} ID: ${uuid}`)
+    } catch (e) {
+        console.log(`Deploying ${appName} failed`);
+        throw e;
+    }
 }
