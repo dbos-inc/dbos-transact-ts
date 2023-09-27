65f43a0f56649fd576d39b3c3ec67139cfcba119
Harry Pierson <harry.pierson@dbos.dev>
2023-09-26 17:30:54 -0700

    generate example table if not exists

--- examples/hello/src/app.ts
+++ examples/hello/src/app.ts
@@ -6,6 +6,7 @@ import Jabber from 'jabber';
 async function main(name: string) {
     const operon = new Operon();
     operon.useNodePostgres();
+    await operon.userDatabase.query("CREATE TABLE IF NOT EXISTS OperonHello (greeting_id SERIAL PRIMARY KEY, greeting TEXT);");
     await operon.init(Hello);
   
     const result = await operon.workflow(Hello.helloWorkflow, {}, name).getResult();
