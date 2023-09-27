da0e49caa1879c893cb38c33eb4e4c59a0561de9
Harry Pierson <harry.pierson@dbos.dev>
2023-09-27 10:10:05 -0700

    minor updtes

--- examples/hello/src/userFunctions.ts
+++ examples/hello/src/userFunctions.ts
@@ -1,14 +1,14 @@
 import { AsyncLocalStorage } from 'node:async_hooks';
-import { TransactionContext, WorkflowContext, OperonTransaction, OperonWorkflow, GetApi, Operon } from 'operon'
+import { TransactionContext, WorkflowContext, OperonTransaction, OperonWorkflow, GetApi } from 'operon'
 
 export class Hello {
 
   @OperonTransaction()
-  static helloFunction(ctx: TransactionContext, name: string): Promise<string> {
+  static async helloFunction(ctx: TransactionContext, name: string): Promise<string> {
     // for OperonTransaction/Communicator methods, wrap the call in an AsyncLocalStorage.run call 
     // so the function can retrieve the Transaction/CommunicatorContext via base class protected
     // properties that access the ALS instances.
-    return txALS.run(ctx, () => {
+    return await txALS.run(ctx, (): Promise<string> => {
       // create a new instance of the $Hello object and call the function
       // Note, every wf/tx/comm call will be on a different instance of the object
       const obj = new $Hello();
@@ -17,17 +17,18 @@ export class Hello {
   }
 
   @OperonWorkflow()
+  @GetApi('/greeting/:name')
   static async helloWorkflow(ctx: WorkflowContext, name: string) {
 
     // create a custom this object that swaps existing tx/comm methods for ones that use ctx.transaction/external 
     const $this = {
-      helloFunction: (name: string) => {
+      helloFunction: (name: string): Promise<string> => {
         return ctx.transaction(Hello.helloFunction, name);
       }
     };
 
     // wrap the call in the workflow context AsyncLocalStorage
-    return await wfALS.run(ctx, () => {
+    return await wfALS.run(ctx, (): Promise<string> => {
       const obj = new $Hello();
       
       // use .call so we can swap in the custom this instance that handles all the tx/comm methods correctly
