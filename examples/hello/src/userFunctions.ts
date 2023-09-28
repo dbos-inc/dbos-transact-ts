40f8f64c78b6fc983662979002da70a3b3ce16f8
Harry Pierson <harry.pierson@dbos.dev>
2023-09-28 14:33:05 -0700

    cleanup hello sample

--- examples/hello/src/userFunctions.ts
+++ examples/hello/src/userFunctions.ts
@@ -26,14 +26,6 @@ export class Hello {
   @OperonWorkflow()
   @GetApi('/greeting/:name')
   static async helloWorkflow(wfCtxt: WorkflowContext, name: string) {
-
-    // WorkflowContext has a new 'proxy' method that returns a developer-friendly object that enables direct invocation of transaction and communicator method;
-    // The proxy method takes a class of static Operon operations and returns an object that includes *only* transaction and communicator methods 
-    // that can be invoked directly: proxy.someMethod(param1, param2) instead of wfCtxt.transaction(Class.someMethod, param1, param2);
-
-    // TS mapping types are used to filter out non-transaction and communicator methods as well as to remove the Transaction/CommunicatorContext parameter
-    // from the method signature.
-
     wfCtxt.log("Hello, workflow!");
     const encodedName = btoa(name);
     const decodedName = await wfCtxt.invoke(Hello).helloExternal(encodedName);
