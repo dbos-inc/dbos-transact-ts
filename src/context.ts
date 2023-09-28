722855b545fd62a88207d21fa795724a7ded36b6
maxdml <maxdml@dbos.dev>
2023-09-28 08:16:57 -0700

    add config getter

--- src/context.ts
+++ src/context.ts
@@ -2,6 +2,7 @@ import { Span } from "@opentelemetry/sdk-trace-base";
 import { IncomingMessage } from "http";
 import { Logger } from "./telemetry/logs";
 import { LogSeverity } from "./telemetry/signals";
+import { has, get } from "lodash";
 
 export class OperonContext {
   request?: IncomingMessage; // Raw incoming HTTP request.
@@ -9,9 +10,6 @@ export class OperonContext {
   authenticatedUser: string = ""; ///< The user that has been authenticated
   authenticatedRoles: string[] = []; ///< All roles the user has according to authentication
   assumedRole: string = ""; ///< Role in use - that user has and provided authorization to current function
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  applicationConfig?: any; // applicationConfiguration
 
   workflowUUID: string = "";
 
@@ -25,6 +23,20 @@ export class OperonContext {
     }
   }
 
+  /*** Application configuration ***/
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  applicationConfig?: any; // applicationConfiguration
+  getConfig(key: string): any {
+    if (!this.applicationConfig) {
+      return undefined;
+    }
+    if (!has(this.applicationConfig, key)) {
+      return undefined;
+    }
+    return get(this.applicationConfig, key);
+  }
+
+  /*** Logging methods ***/
   info(message: string): void {
     this.logger.log(this, LogSeverity.Info, message);
   }
