0f839e3ed3c68c3473edb85534a9b3fbba27ab3b
Harry Pierson <harry.pierson@dbos.dev>
2023-09-28 09:05:56 -0700

    fix up build breaks

--- examples/hello/src/userFunctions.ts
+++ examples/hello/src/userFunctions.ts
@@ -1,17 +1,16 @@
 import { TransactionContext, WorkflowContext, OperonTransaction, OperonWorkflow, GetApi, OperonCommunicator, CommunicatorContext } from 'operon'
 
-const delay = (ms:number) => new Promise(resolve => setTimeout(resolve, ms))
-
 export class Hello {
 
   @OperonCommunicator()
-  static async helloExternal(ctxt: CommunicatorContext, encodedName: string) {
+  static async helloExternal(commCtxt: CommunicatorContext, encodedName: string) {
+    commCtxt.log(`decoding ${encodedName} via httpbin`)
     try {
       const url = `https://httpbin.org/base64/${encodeURIComponent(encodedName)}`;
       const res = await fetch(url);
       return await res.text();
     } catch {
-      ctxt.log("high", "httpbin/base64 failed, decoding locally");
+      commCtxt.warn("httpbin/base64 failed, decoding locally");
       return atob(encodedName);
     }
   }
@@ -20,13 +19,13 @@ export class Hello {
   static async helloFunction(txnCtxt: TransactionContext, name: string) {
     const greeting = `Hello, ${name}!`
     const { rows } = await txnCtxt.pgClient.query<{greeting_id: number}>("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
-    txnCtxt.warn(`Inserted greeting ${rows[0].greeting_id}: ${greeting}`)
+    txnCtxt.log(`Inserted greeting ${rows[0].greeting_id}: ${greeting}`)
     return `Greeting ${rows[0].greeting_id}: ${greeting}`;
   }
 
   @OperonWorkflow()
   @GetApi('/greeting/:name')
-  static async helloWorkflow(workflowCtxt: WorkflowContext, name: string) {
+  static async helloWorkflow(wfCtxt: WorkflowContext, name: string) {
     
     // Added a new 'proxy' method to WorkflowContext that returns a developer-friendly object that enables direct invocation
     // proxy object only includes methods decorated with @OperonTransaction + converts the direct call to an indirect call thru context.transaction
@@ -34,10 +33,10 @@ export class Hello {
 
     // TS mapping types are used create a developer friendly type signature for proxy's return value. See the top of workflow.ts for more info
 
-    workflowCtxt.log("Hello, workflow!");
+    wfCtxt.log("Hello, workflow!");
     const encodedName = btoa(name);
-    const decodedName = await ctxt.external(Hello.helloExternal, encodedName);
-    return await ctxt.proxy(Hello).helloFunction(decodedName);
+    const decodedName = await wfCtxt.external(Hello.helloExternal, encodedName);
+    return await wfCtxt.proxy(Hello).helloFunction(decodedName);
   }
 }
 
