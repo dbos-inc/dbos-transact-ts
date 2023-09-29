e1d0e19f0a18714f2b2166bf72fdc5e88daef5cb
maxdml <maxdml@dbos.dev>
2023-09-29 10:59:50 -0700

    revert format changes

--- examples/hello/src/userFunctions.ts
+++ examples/hello/src/userFunctions.ts
@@ -1,15 +1,7 @@
-import {
-  TransactionContext,
-  WorkflowContext,
-  OperonTransaction,
-  OperonWorkflow,
-  GetApi,
-  OperonCommunicator,
-  CommunicatorContext,
-  HandlerContext,
-} from "operon";
+import { TransactionContext, WorkflowContext, OperonTransaction, OperonWorkflow, GetApi, OperonCommunicator, CommunicatorContext, HandlerContext } from 'operon'
 
 export class Hello {
+
   @OperonCommunicator()
   static async helloExternal(commCtxt: CommunicatorContext, encodedName: string) {
     commCtxt.log(`decoding ${encodedName} via httpbin`)
@@ -25,17 +17,15 @@ export class Hello {
 
   @OperonTransaction()
   static async helloFunction(txnCtxt: TransactionContext, name: string) {
-    const greeting = `Hello, ${name}!`;
-    const { rows } = await txnCtxt.pgClient.query<{ greeting_id: number }>(
-      "INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id",
-      [greeting]
-    );
-    txnCtxt.warn(`Inserted greeting ${rows[0].greeting_id}: ${greeting}`);
+    const greeting = `Hello, ${name}!`
+    const { rows } = await txnCtxt.pgClient.query<{ greeting_id: number }>("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
+    txnCtxt.log(`Inserted greeting ${rows[0].greeting_id}: ${greeting}`)
     return `Greeting ${rows[0].greeting_id}: ${greeting}`;
   }
 
   @OperonWorkflow()
   static async helloWorkflow(wfCtxt: WorkflowContext, name: string) {
+    wfCtxt.log("Hello, workflow!");
     const encodedName = btoa(name);
     const decodedName = await wfCtxt.invoke(Hello).helloExternal(encodedName);
     return await wfCtxt.invoke(Hello).helloFunction(decodedName);
