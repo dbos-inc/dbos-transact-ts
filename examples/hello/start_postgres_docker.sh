ff90baf4462d282726427404a7fe2e8ffe90dcfc
Peter Kraft <petereliaskraft@gmail.com>
2023-10-05 14:42:18 -0700

    more scripts

--- examples/hello/start_postgres_docker.sh
+++ examples/hello/start_postgres_docker.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
 # Start Postgres in a local Docker container
-docker run --name=operon-db --env=POSTGRES_PASSWORD=dbos --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:latest
+docker run --rm --name=operon-db --env=POSTGRES_PASSWORD=dbos --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:latest
+
+# Wait for PostgreSQL to start
+echo "Waiting for PostgreSQL to start..."
+for i in {1..30}; do
+  if docker exec operon-db pg_isready -U postgres > /dev/null; then
+    echo "PostgreSQL started!"
+    break
+  fi
+  sleep 1
+done
+
+docker exec operon-db psql -U postgres -c "CREATE DATABASE hello;"
