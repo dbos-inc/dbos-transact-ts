a99d2ba98ed2cd2d470a1bf7ba846cdb202e85c2
Peter Kraft <petereliaskraft@gmail.com>
2023-10-05 12:09:05 -0700

    more tests

--- tests/knex.test.ts
+++ tests/knex.test.ts
@@ -1,4 +1,4 @@
-import { Operon, OperonTransaction, TransactionContext } from "../src";
+import { Operon, OperonTransaction, OperonWorkflow, TransactionContext, WorkflowContext } from "../src";
 import { OperonConfig } from "../src/operon";
 import { UserDatabaseName } from "../src/user_database";
 import { TestKvTable, generateOperonTestConfig, setupOperonTestDb } from "./helpers";
@@ -7,10 +7,23 @@ const testTableName = "operon_test_kv";
 
 class TestClass {
   @OperonTransaction()
-  static async testTx(txnCtxt: TransactionContext, name: string) {
+  static async testInsert(txnCtxt: TransactionContext, name: string) {
     const result = await txnCtxt.knex<TestKvTable>(testTableName)
       .insert({ value: name })
       .returning('id');
+    return result[0].id!;
+  }
+
+  @OperonTransaction()
+  static async testSelect(txnCtxt: TransactionContext, id: number) {
+    const result = await txnCtxt.knex<TestKvTable>(testTableName).select("value").where({id: id});
+    return result[0].value!;
+  }
+
+  @OperonWorkflow()
+  static async testWf(ctxt: WorkflowContext, name: string) {
+    const id = await ctxt.invoke(TestClass).testInsert(name);
+    const result = await ctxt.invoke(TestClass).testSelect(id);
     return result;
   }
 }
@@ -25,7 +38,6 @@ describe("knex-tests", () => {
     await setupOperonTestDb(config);
   })
 
-
   beforeEach(async () => {
     operon = new Operon(config);
     await operon.init(TestClass);
@@ -39,9 +51,12 @@ describe("knex-tests", () => {
     await operon.destroy();
   });
 
-
   test("simple-knex", async () => {
-    const result = await operon.transaction(TestClass.testTx, {}, "bob");
-    expect(result[0].id).toEqual(1);
+    const insertResult = await operon.transaction(TestClass.testInsert, {}, "test-one");
+    expect(insertResult).toEqual(1);
+    const selectResult = await operon.transaction(TestClass.testSelect, {}, 1);
+    expect(selectResult).toEqual("test-one");
+    const wfResult = await operon.workflow(TestClass.testWf, {}, "test-two").then(x => x.getResult());
+    expect(wfResult).toEqual("test-two");
   });
 });
