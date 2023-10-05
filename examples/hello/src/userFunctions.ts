660474ea93b4e328942ee0d15d858f1fe0ed0441
Harry Pierson <harry.pierson@dbos.dev>
2023-10-05 16:23:26 -0700

    update type alias in hello example

--- examples/hello/src/userFunctions.ts
+++ examples/hello/src/userFunctions.ts
@@ -1,12 +1,12 @@
 import { TransactionContext, OperonTransaction, GetApi } from '@dbos-inc/operon'
 import { PoolClient } from 'pg';
 
-type HelloTransactionContext = TransactionContext<PoolClient>;
+type PGTransactionContext = TransactionContext<PoolClient>;
 
 export class Hello {
   @GetApi('/greeting/:name')
   @OperonTransaction()
-  static async helloFunction(txnCtxt: HelloTransactionContext, name: string) {
+  static async helloFunction(txnCtxt: PGTransactionContext, name: string) {
     const greeting = `Hello, ${name}!`
     const { rows } = await txnCtxt.client.query<{ greeting_id: number }>("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
     return `Greeting ${rows[0].greeting_id}: Hello, ${name}!`;
