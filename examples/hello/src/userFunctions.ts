5dbadf5e36e11b3d19c9d6adbbc36dc4cc3df5e3
Harry Pierson <harry.pierson@dbos.dev>
2023-10-05 15:07:01 -0700

    update hello example

--- examples/hello/src/userFunctions.ts
+++ examples/hello/src/userFunctions.ts
@@ -1,11 +1,14 @@
 import { TransactionContext, OperonTransaction, GetApi } from '@dbos-inc/operon'
+import { PoolClient } from 'pg';
+
+type TestTransactionContext = TransactionContext<PoolClient>;
 
 export class Hello {
   @GetApi('/greeting/:name')
   @OperonTransaction()
-  static async helloFunction(txnCtxt: TransactionContext, name: string) {
+  static async helloFunction(txnCtxt: TestTransactionContext, name: string) {
     const greeting = `Hello, ${name}!`
-    const { rows } = await txnCtxt.pgClient.query<{ greeting_id: number }>("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
+    const { rows } = await txnCtxt.client.query<{ greeting_id: number }>("INSERT INTO OperonHello(greeting) VALUES ($1) RETURNING greeting_id", [greeting])
     return `Greeting ${rows[0].greeting_id}: Hello, ${name}!`;
   }
 }
