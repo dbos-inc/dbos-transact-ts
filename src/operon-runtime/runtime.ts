213d2eea450e8e7696821b27176bb8da9c88280d
manojdbos <manoj.khangaonkar@dbos.dev>
2023-10-10 13:13:22 -0700

    rename userFunctions to operations

--- src/operon-runtime/runtime.ts
+++ src/operon-runtime/runtime.ts
@@ -51,7 +51,7 @@ export class OperonRuntime {
    */
   private loadFunctions(): Promise<ModuleExports> | null {
     const workingDirectory = process.cwd();
-    const userFunctions = workingDirectory + "/dist/userFunctions.js";
+    const userFunctions = workingDirectory + "/dist/operations.js";
     if (fs.existsSync(userFunctions)) {
       /* eslint-disable-next-line @typescript-eslint/no-var-requires */
       return import(userFunctions) as Promise<ModuleExports>;
