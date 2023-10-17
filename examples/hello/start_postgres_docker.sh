090154660ec0a5eea3601ead04f0592c92309219
Qian Li <qian.li@dbos.dev>
2023-10-16 17:53:36 -0700

    Fix postgres script (#138)

--- examples/hello/start_postgres_docker.sh
+++ examples/hello/start_postgres_docker.sh
@@ -7,12 +7,12 @@ if [[ -z "${PGPASSWORD}" ]]; then
 fi
 
 # Start Postgres in a local Docker container
-docker run --rm --name=operon-db --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:latest
+docker run --rm --name=operon-db --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:15.4
 
 # Wait for PostgreSQL to start
 echo "Waiting for PostgreSQL to start..."
 for i in {1..30}; do
-  if docker exec operon-db pg_isready -U postgres > /dev/null; then
+  if docker exec operon-db pg_isready -U postgres | grep -q "accepting connections"; then
     echo "PostgreSQL started!"
     break
   fi
