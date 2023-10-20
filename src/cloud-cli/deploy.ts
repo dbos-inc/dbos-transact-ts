d7fd9a7d446e08299f980dac4641ad312dc773cb
maxdml <maxdml@dbos.dev>
2023-10-20 23:17:30 +0000

    update login & deploy CLI to use usernames

--- src/cloud-cli/deploy.ts
+++ src/cloud-cli/deploy.ts
@@ -2,15 +2,17 @@ import axios from "axios";
 import { execSync } from "child_process";
 import fs from "fs";
 import FormData from "form-data";
-import { operonEnvPath } from "./login";
+import { OperonCloudCredentials, operonEnvPath } from "./login";
 
 export async function deploy(appName: string, host: string) {
-  let userToken = fs.readFileSync(`./${operonEnvPath}/credentials`).toString("utf-8");
-  userToken = userToken.replace(/\r|\n/g, ''); // Trim the trailing /r /n.
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const userCredentials = JSON.parse(fs.readFileSync(`./${operonEnvPath}/credentials`).toString("utf-8")) as OperonCloudCredentials;
+  const userName = userCredentials.userName;
+  const userToken = userCredentials.token.replace(/\r|\n/g, ''); // Trim the trailing /r /n.
   const bearerToken = "Bearer " + userToken;
   try {
-    const register = await axios.post(
-      `http://${host}:8080/application/register`,
+    const register = await axios.put(
+      `http://${host}:8080/${userName}/application`,
       {
         name: appName,
       },
@@ -30,7 +32,7 @@ export async function deploy(appName: string, host: string) {
     const formData = new FormData();
     formData.append("app_archive", fs.createReadStream(`operon_deploy/${uuid}.zip`));
 
-    await axios.post(`http://${host}:8080/application/${uuid}`, formData, {
+    await axios.post(`http://${host}:8080/${userName}/application/${appName}`, formData, {
       headers: {
         ...formData.getHeaders(),
         Authorization: bearerToken,
@@ -39,7 +41,10 @@ export async function deploy(appName: string, host: string) {
     console.log(`Successfully deployed: ${appName}`);
     console.log(`${appName} ID: ${uuid}`);
   } catch (e) {
-    console.log(`Deploying ${appName} failed`);
-    throw e;
+    if (axios.isAxiosError(e)) {
+      console.error(`failed to deploy application ${appName}: ${e.response?.data}`);
+    } else {
+      console.error(`failed to deploy application ${appName}`);
+    }
   }
 }
