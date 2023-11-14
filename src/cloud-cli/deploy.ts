9712f8248801dce5da2aa846a0173beb6e48af1b
maxdml <maxdml@dbos.dev>
2023-11-13 17:06:19 -0800

    - split app register and deploy - add list apps subcommand

--- src/cloud-cli/deploy.ts
+++ src/cloud-cli/deploy.ts
@@ -5,33 +5,19 @@ import FormData from "form-data";
 import { createGlobalLogger } from "../telemetry/logs";
 import { getCloudCredentials } from "./utils";
 
-export async function deploy(appName: string, host: string, port: string, machines: number) {
+export async function deployAppCode(appName: string, host: string, port: string) {
   const logger = createGlobalLogger();
   const userCredentials = getCloudCredentials();
   const bearerToken = "Bearer " + userCredentials.token;
 
   try {
-    const register = await axios.put(
-      `http://${host}:${port}/${userCredentials.userName}/application`,
-      {
-        name: appName,
-        max_vms: machines
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: bearerToken,
-        },
-      }
-    );
-    const uuid = register.data as string;
     execSync(`mkdir -p operon_deploy`);
     execSync(`envsubst < operon-config.yaml > operon_deploy/operon-config.yaml`);
-    execSync(`zip -ry operon_deploy/${uuid}.zip ./* -x operon_deploy/* operon-config.yaml > /dev/null`);
-    execSync(`zip -j operon_deploy/${uuid}.zip operon_deploy/operon-config.yaml > /dev/null`);
+    execSync(`zip -ry operon_deploy/${appName}.zip ./* -x operon_deploy/* operon-config.yaml > /dev/null`);
+    execSync(`zip -j operon_deploy/${appName}.zip operon_deploy/operon-config.yaml > /dev/null`);
 
     const formData = new FormData();
-    formData.append("app_archive", fs.createReadStream(`operon_deploy/${uuid}.zip`));
+    formData.append("app_archive", fs.createReadStream(`operon_deploy/${appName}.zip`));
 
     await axios.post(`http://${host}:${port}/${userCredentials.userName}/application/${appName}`, formData, {
       headers: {
@@ -40,7 +26,7 @@ export async function deploy(appName: string, host: string, port: string, machin
       },
     });
     logger.info(`Successfully deployed: ${appName}`);
-    logger.info(`${appName} ID: ${uuid}`);
+    logger.info(`${appName} ID: ${appName}`);
   } catch (e) {
     if (axios.isAxiosError(e) && e.response) {
       logger.error(`failed to deploy application ${appName}: ${e.response?.data}`);
---
+++ src/cloud-cli/list-apps.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { createGlobalLogger } from "../telemetry/logs";
+import { getCloudCredentials } from "./utils";
+
+export async function listApps(host: string, port: string) {
+  const logger = createGlobalLogger();
+  const userCredentials = getCloudCredentials();
+  const bearerToken = "Bearer " + userCredentials.token;
+
+  try {
+    const list = await axios.get(
+      `http://${host}:${port}/${userCredentials.userName}/application`,
+      {
+        headers: {
+          Authorization: bearerToken,
+        },
+      }
+    );
+    console.log(list.data);
+  } catch (e) {
+    if (axios.isAxiosError(e) && e.response) {
+      logger.error(`failed to list applications: ${e.response?.data}`);
+    } else {
+      logger.error(`failed to list applications: ${(e as Error).message}`);
+    }
+  }
+}
