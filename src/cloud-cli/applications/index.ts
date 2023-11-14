026f9648f00bc4ccfd3d4ee298f2f2c8e4745e78
maxdml <maxdml@dbos.dev>
2023-11-14 10:47:53 -0800

    lint

--- src/cloud-cli/applications/index.ts
+++ src/cloud-cli/applications/index.ts
@@ -1,5 +1,5 @@
-export { registerApp } from './register-app';
+export { registerApp } from './register-app';
 export { listApps } from './list-apps';
 export { deleteApp } from './delete-app';
-export { deployAppCode } from './deploy-app-code';
+export { deployAppCode } from './deploy-app-code';
 export { getAppLogs } from './get-app-logs';
