c8f6250ab4f140b59a098c423c252fced8482bc1
maxdml <maxdml@dbos.dev>
2023-12-08 20:04:47 -0800

    first jab at OTLP transport

--- src/telemetry/logs.ts
+++ src/telemetry/logs.ts
@@ -1,6 +1,8 @@
 import { transports, createLogger, format, Logger as IWinstonLogger } from "winston";
+import TransportStream = require("winston-transport");
 import { getApplicationVersion } from "../dbos-runtime/applicationVersion";
 import { DBOSContext } from "../context";
+import { LogRecord, SeverityNumber } from "@opentelemetry/api-logs";
 
 export interface LoggerConfig {
   logLevel?: string;
@@ -14,30 +16,30 @@ export interface WinstonLogger extends IWinstonLogger {
 }
 
 type ContextualMetadata = {
+  includeContextMetadata: boolean;
   workflowUUID: string;
   authenticatedUser: string;
   traceId: string;
   spanId: string;
-  [key: string]: string | undefined; // Adding an index signature
+  [key: string]: string | boolean | undefined;
 };
 
 /* This is a wrapper around a global Winston logger. It holds a reference to the global logger.
  * This class is expected to be instantiated by new DBOSContext such that they can share context information.
  **/
 export class Logger {
-  readonly metadata?: ContextualMetadata;
+  readonly metadata: ContextualMetadata;
   constructor(
     private readonly globalLogger: WinstonLogger,
     private readonly ctx: DBOSContext
   ) {
-    if (this.globalLogger.addContextMetadata) {
-      this.metadata = {
-        workflowUUID: this.ctx.workflowUUID,
-        authenticatedUser: this.ctx.authenticatedUser,
-        traceId: this.ctx.span.spanContext().traceId,
-        spanId: this.ctx.span.spanContext().spanId,
-      };
-    }
+    this.metadata = {
+      includeContextMetadata: this.globalLogger.addContextMetadata,
+      workflowUUID: this.ctx.workflowUUID,
+      authenticatedUser: this.ctx.authenticatedUser,
+      traceId: this.ctx.span.spanContext().traceId,
+      spanId: this.ctx.span.spanContext().spanId,
+    };
   }
 
   info(message: string): void {
@@ -76,6 +78,8 @@ export function createGlobalLogger(config?: LoggerConfig): WinstonLogger {
         level: config?.logLevel || "info",
         silent: config?.silent || false,
       }),
+      new OTLPLogQueueTransport(
+      ),
     ],
   }) as WinstonLogger;
   logger.addContextMetadata = config?.addContextMetadata || false;
@@ -89,24 +93,46 @@ const consoleFormat = format.combine(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   format.printf((info) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { timestamp, level, message, stack, workflowUUID, authenticatedUser, traceId, spanId } = info;
+    const { timestamp, level, message, stack, includeContextMetadata, workflowUUID, authenticatedUser, traceId, spanId } = info;
     const applicationVersion = getApplicationVersion();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
     const ts = timestamp.slice(0, 19).replace("T", " ");
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
     const formattedStack = stack?.split("\n").slice(1).join("\n");
-    // If we have context metadata, we add it to the messageString
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
-    const contextualMetadata: ContextualMetadata = { workflowUUID, authenticatedUser, traceId, spanId };
-    const contextualMetadataString = Object.keys(contextualMetadata).reduce((acc, key) => {
-      const value = contextualMetadata[key];
-      return value !== undefined && value !== null ? `${acc}, "${key}": "${value}"` : acc;
-    }, "");
+
     const messageString: string = typeof message === "string" ? message : JSON.stringify(message);
-    const fullMessageString = `${messageString}${contextualMetadataString ? ` {${contextualMetadataString.substring(2)}}` : ""}`;
+    const contextualMetadata = { workflowUUID, authenticatedUser, traceId, spanId };
+    const fullMessageString = `${messageString}${includeContextMetadata ? ` ${JSON.stringify(contextualMetadata)}` : ""}`;
 
     const versionString = applicationVersion ? ` [version ${applicationVersion}]` : "";
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     return `${ts}${versionString} [${level}]: ${fullMessageString} ${stack ? "\n" + formattedStack : ""}`;
   })
 );
+
+class OTLPLogQueueTransport extends TransportStream {
+  readonly name = "OTLPLogQueueTransport";
+
+  log(info: any, callback: () => void): void {
+    const { level, message, stack, workflowUUID, authenticatedUser, traceId, spanId } = info;
+
+    const levelToSeverityNumber: { [key: string]: SeverityNumber } = {
+      error: SeverityNumber.ERROR,
+      warn: SeverityNumber.WARN,
+      info: SeverityNumber.INFO,
+      debug: SeverityNumber.DEBUG,
+    };
+
+    const logRecord: LogRecord = {
+      severityNumber: levelToSeverityNumber[level],
+      severityText: level,
+      body: message,
+      timestamp: new Date().getTime(),
+      attributes: { workflowUUID, authenticatedUser, traceId, spanId, stack },
+    };
+
+    console.log(logRecord);
+
+    callback();
+  }
+}
