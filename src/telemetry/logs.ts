88d516837c1bbd0e4bc1e5e7478de5252a62e9fa
maxdml <maxdml@dbos.dev>
2023-12-08 18:52:54 -0800

    Type contextual metadata and format them more nicely + remove overkill log functions

--- src/telemetry/logs.ts
+++ src/telemetry/logs.ts
@@ -13,13 +13,23 @@ export interface WinstonLogger extends IWinstonLogger {
   addContextMetadata: boolean;
 }
 
+type ContextualMetadata = {
+  workflowUUID: string;
+  authenticatedUser: string;
+  traceId: string;
+  spanId: string;
+  [key: string]: string | undefined; // Adding an index signature
+};
+
 /* This is a wrapper around a global Winston logger. It holds a reference to the global logger.
  * This class is expected to be instantiated by new DBOSContext such that they can share context information.
  **/
 export class Logger {
-  // Eventually this object will implement one of our TelemetrySignal interface
-  readonly metadata: object = {};
-  constructor(private readonly globalLogger: WinstonLogger, private readonly ctx: DBOSContext) {
+  readonly metadata?: ContextualMetadata;
+  constructor(
+    private readonly globalLogger: WinstonLogger,
+    private readonly ctx: DBOSContext
+  ) {
     if (this.globalLogger.addContextMetadata) {
       this.metadata = {
         workflowUUID: this.ctx.workflowUUID,
@@ -42,18 +52,6 @@ export class Logger {
     this.globalLogger.warn(message, this.metadata);
   }
 
-  emerg(message: string): void {
-    this.globalLogger.emerg(message, this.metadata);
-  }
-
-  alert(message: string): void {
-    this.globalLogger.alert(message, this.metadata);
-  }
-
-  crit(message: string): void {
-    this.globalLogger.crit(message, this.metadata);
-  }
-
   // We give users the same interface (message: string argument) but create an error to get a stack trace
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   error(inputError: any): void {
@@ -72,13 +70,13 @@ export class Logger {
 
 export function createGlobalLogger(config?: LoggerConfig): WinstonLogger {
   const logger: WinstonLogger = createLogger({
-    format: consoleFormat,
     transports: [
       new transports.Console({
+        format: consoleFormat,
         level: config?.logLevel || "info",
+        silent: config?.silent || false,
       }),
     ],
-    silent: config?.silent || false,
   }) as WinstonLogger;
   logger.addContextMetadata = config?.addContextMetadata || false;
   return logger;
@@ -91,15 +89,24 @@ const consoleFormat = format.combine(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   format.printf((info) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const { timestamp, level, message, stack, ...args } = info;
+    const { timestamp, level, message, stack, workflowUUID, authenticatedUser, traceId, spanId } = info;
     const applicationVersion = getApplicationVersion();
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
     const ts = timestamp.slice(0, 19).replace("T", " ");
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
     const formattedStack = stack?.split("\n").slice(1).join("\n");
+    // If we have context metadata, we add it to the messageString
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
+    const contextualMetadata: ContextualMetadata = { workflowUUID, authenticatedUser, traceId, spanId };
+    const contextualMetadataString = Object.keys(contextualMetadata).reduce((acc, key) => {
+      const value = contextualMetadata[key];
+      return value !== undefined && value !== null ? `${acc}, "${key}": "${value}"` : acc;
+    }, "");
     const messageString: string = typeof message === "string" ? message : JSON.stringify(message);
+    const fullMessageString = `${messageString}${contextualMetadataString ? ` {${contextualMetadataString.substring(2)}}` : ""}`;
+
     const versionString = applicationVersion ? ` [version ${applicationVersion}]` : "";
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    return `${ts}${versionString} [${level}]: ${messageString} ${Object.keys(args).length ? "\n" + JSON.stringify(args, null, 2) : ""} ${stack ? "\n" + formattedStack : ""}`;
+    return `${ts}${versionString} [${level}]: ${fullMessageString} ${stack ? "\n" + formattedStack : ""}`;
   })
 );
