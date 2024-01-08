0ff49fcc187bb4fea3f45f0ea82781a87c31c76f
chuck-dbos <134347445+chuck-dbos@users.noreply.github.com>
2024-01-08 15:20:03 -0500

    Initialize lint in hello example (#240)
    Also fix the lint issues it found
    Update koa/cors types (dependabot fixed a vuln but missed the types I
    think)

--- examples/hello/src/operations.ts
+++ examples/hello/src/operations.ts
@@ -1,4 +1,4 @@
-import { TransactionContext, Transaction, GetApi, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk'
+import { TransactionContext, Transaction, GetApi, ArgSource, ArgSources } from '@dbos-inc/dbos-sdk';
 import { Knex } from 'knex';
 
 // The schema of the database table used in this example.
@@ -13,7 +13,7 @@ export class Hello {
   @Transaction()  // Run this function as a database transaction
   static async helloTransaction(ctxt: TransactionContext<Knex>, @ArgSource(ArgSources.URL) user: string) {
     // Retrieve and increment the number of times this user has been greeted.
-    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;"
+    const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
     const { rows } = await ctxt.client.raw(query, [user]) as { rows: dbos_hello[] };
     const greet_count = rows[0].greet_count;
     return `Hello, ${user}! You have been greeted ${greet_count} times.\n`;
