ae7af68b6ba65cef41e3b5087352d164f422b886
Peter Kraft <peter.kraft@dbos.dev>
2024-01-23 07:50:42 -0800

    Postgres 16.1 (#254)
    Run all the SDK tests in Postgres 16.1.

--- examples/hello/start_postgres_docker.sh
+++ examples/hello/start_postgres_docker.sh
@@ -7,7 +7,7 @@ if [[ -z "${PGPASSWORD}" ]]; then
 fi
 
 # Start Postgres in a local Docker container
-docker run --rm --name=dbos-db --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:15.4
+docker run --rm --name=dbos-db --env=POSTGRES_PASSWORD=${PGPASSWORD} --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:16.1
 
 # Wait for PostgreSQL to start
 echo "Waiting for PostgreSQL to start..."
