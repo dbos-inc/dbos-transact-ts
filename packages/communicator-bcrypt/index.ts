4a7a5ae135484f5ec3866a00e319a199b1444022
Chuck B <chuck.bear@dbos.dev>
2024-01-24 14:13:59 -0500

    Implement bcrypt

--- packages/communicator-bcrypt/index.ts
+++ packages/communicator-bcrypt/index.ts
@@ -1,12 +1,25 @@
 
 import {Communicator, CommunicatorContext} from '@dbos-inc/dbos-sdk';
 
+import bcryptjs from 'bcryptjs';
+
+
 class BcryptCommunicator
 {
     @Communicator()
-    static bcrypt(_ctx: CommunicatorContext) : Promise<number> {
-        // TODO
-        return Promise.resolve(1);
+    static async bcryptGenSalt(_ctx: CommunicatorContext, saltRounds: number = 10) : Promise<string>
+    {
+        return await bcryptjs.genSalt(saltRounds);
+    }
+
+    @Communicator()
+    static async bcryptHash(_ctx: CommunicatorContext, txt: string, saltRounds: number = 10) : Promise<string> {
+        return await bcryptjs.hash(txt, saltRounds);
+    }
+
+    static async bcryptCompare(txt: string, hashedTxt: string): Promise<boolean> {
+        const isMatch = bcryptjs.compare(txt, hashedTxt);
+        return isMatch;
     }
 }
 
