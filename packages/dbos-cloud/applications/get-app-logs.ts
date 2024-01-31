18d860d00ac790850779c0e3f79646b61a32954e
Alex Poliakov <alex.poliakov@dbos.dev>
2024-01-31 10:32:30 -0500

    Support the --last parameter (#266)
    Send it to the cloud side and retrieve what comes back. Getting back an
    empty log is no longer an error.

--- packages/dbos-cloud/applications/get-app-logs.ts
+++ packages/dbos-cloud/applications/get-app-logs.ts
@@ -2,7 +2,13 @@ import axios , { AxiosError } from "axios";
 import { handleAPIErrors, getCloudCredentials, getLogger } from "../cloudutils";
 import path from "node:path";
 
-export async function getAppLogs(host: string): Promise<number> {
+export async function getAppLogs(host: string, last: number): Promise<number> {
+  if (last != undefined && (isNaN(last) || last <= 0)) {
+    throw new Error('The --last parmameter must be an integer greater than 0');
+  }
+  if (last == undefined) {
+    last = 0      //internally, 0 means "get all the logs." This is the default.
+  }
   const logger = getLogger();
   const userCredentials = getCloudCredentials();
   const bearerToken = "Bearer " + userCredentials.token;
@@ -13,15 +19,18 @@ export async function getAppLogs(host: string): Promise<number> {
   logger.info(`Retrieving logs for application: ${appName}`)
 
   try {
-    const res = await axios.get(`https://${host}/${userCredentials.userName}/logs/application/${appName}`, {
+    const res = await axios.get(`https://${host}/${userCredentials.userName}/logs/application/${appName}?last=${last}`, {
       headers: {
         "Content-Type": "application/json",
         Authorization: bearerToken,
-      },
+      }
     });
-
-    logger.info(`Successfully retrieved logs of application: ${appName}`);
-    logger.info(res.data)
+    if (res.data == "") {
+      logger.info(`No logs found for the specified parameters`);
+    } else {
+      logger.info(`Successfully retrieved logs of application: ${appName}`);
+      logger.info(res.data)
+    }
     return 0;
   } catch (e) {
     const errorLabel = `Failed to retrieve logs of application ${appName}`;
