342e873e8fad0d3ea4441b5d09225708ba019a0f
Qian Li <qian.li@dbos.dev>
2024-02-01 14:02:39 -0800

    add message_uuid as primary key

--- migrations/20240201213211_replica_identity.js
+++ migrations/20240201213211_replica_identity.js
@@ -2,8 +2,7 @@ exports.up = async function(knex) {
   await knex.raw('create extension if not exists "uuid-ossp"');
   return knex.schema.withSchema('dbos')
     .table('notifications', function(table) {
-      table.text('message_uuid').notNullable().defaultTo(knex.raw('uuid_generate_v4()'));
-      table.primary('message_uuid');
+      table.text('message_uuid').primary().defaultTo(knex.raw('uuid_generate_v4()'));
     })
 
 };
