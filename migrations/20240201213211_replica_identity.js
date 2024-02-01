6634bf1594f9c8f43e4e02094667f80dbaf188c3
Qian Li <qian.li@dbos.dev>
2024-02-01 13:54:46 -0800

    add message_uuid

--- migrations/20240201213211_replica_identity.js
+++ migrations/20240201213211_replica_identity.js
@@ -2,7 +2,8 @@ exports.up = async function(knex) {
   await knex.raw('create extension if not exists "uuid-ossp"');
   return knex.schema.withSchema('dbos')
     .table('notifications', function(table) {
-      table.text('message_uuid').notNullable().defaultTo(knex.raw('uuid_generate_v4()'))
+      table.text('message_uuid').notNullable().defaultTo(knex.raw('uuid_generate_v4()'));
+      table.primary('message_uuid');
     })
 
 };
