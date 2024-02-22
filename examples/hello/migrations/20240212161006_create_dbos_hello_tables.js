d659422f8d83558368bd45da3ee3f0e0ee268a2e
maxdml <maxdml@dbos.dev>
2024-02-21 17:15:50 -0800

    add back primary key

--- examples/hello/migrations/20240212161006_create_dbos_hello_tables.js
+++ examples/hello/migrations/20240212161006_create_dbos_hello_tables.js
@@ -2,7 +2,7 @@ const { Knex } = require("knex");
 
 exports.up = async function(knex) {
   return knex.schema.createTable('dbos_hello', table => {
-    table.text('name');
+    table.text('name').primary();
     table.text('greeting_note_content');
     table.text('greeting_note_uuid');
     table.integer('greet_count').defaultTo(0);
