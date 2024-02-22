357bc48838fbbb1dd5f4753c4d7389f2583b0123
maxdml <maxdml@dbos.dev>
2024-02-21 16:56:27 -0800

    update schema

--- examples/hello/migrations/20240212161006_create_dbos_hello_tables.js
+++ examples/hello/migrations/20240212161006_create_dbos_hello_tables.js
@@ -2,7 +2,9 @@ const { Knex } = require("knex");
 
 exports.up = async function(knex) {
   return knex.schema.createTable('dbos_hello', table => {
-    table.text('name').primary();
+    table.text('name');
+    table.text('greeting_note_content');
+    table.text('greeting_note_uuid');
     table.integer('greet_count').defaultTo(0);
   });
 };
