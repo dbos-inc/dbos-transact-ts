66b60acf8f1fb02689dc2957e00bb4cec44d744f
Max dml <maxdml@dbos.dev>
2024-02-23 10:18:42 -0800

    Update hello example (#297)
    Add new tables for the SDK programming guide.
    
    Tested with all DBOS cloud integration tests, using `npx -y
    @dbos-inc/dbos-sdk@test init`

--- examples/hello/migrations/20240212161006_create_dbos_hello_tables.js
+++ examples/hello/migrations/20240212161006_create_dbos_hello_tables.js
@@ -3,6 +3,8 @@ const { Knex } = require("knex");
 exports.up = async function(knex) {
   return knex.schema.createTable('dbos_hello', table => {
     table.text('name').primary();
+    table.text('greeting_name');
+    table.text('greeting_note_content');
     table.integer('greet_count').defaultTo(0);
   });
 };
