954b7dcd30305dc878b33bb2eb1a17383b4a3973
Peter Kraft <petereliaskraft@gmail.com>
2024-03-18 10:54:03 -0700

    Update with jtcoyle suggestions

--- examples/hello/start_postgres_docker.bat
+++ examples/hello/start_postgres_docker.bat
@@ -1,25 +1,36 @@
 @echo off
-setlocal EnableDelayedExpansion
 
-rem Check if PGPASSWORD is set
+setlocal ENABLEEXTENSIONS
+
+echo Checking if PGPASSWORD is set.
 if "%PGPASSWORD%"=="" (
   echo Error: PGPASSWORD is not set.
   exit /b 1
 )
 
-rem Start Postgres in a local Docker container
+echo Starting PostgreSQL in a local Docker container
 docker run --rm --name=dbos-db --env=POSTGRES_PASSWORD=%PGPASSWORD% --env=PGDATA=/var/lib/postgresql/data --volume=/var/lib/postgresql/data -p 5432:5432 -d postgres:16.1
 
-rem Wait for PostgreSQL to start
+	if %errorlevel% == 125 (
+		echo Error: Check if the Docker container already exists
+		exit /b 1
+		) else (
+		goto :start
+		)
+
+:start
 echo Waiting for PostgreSQL to start...
 for /l %%i in (1,1,30) do (
   docker exec dbos-db psql -U postgres -c "SELECT 1;" >NUL 2>&1
-  if !errorlevel! == 0 (
+
+if %errorlevel% equ 0 (
+(
     echo PostgreSQL started!
-    goto break
+    goto :break
   )
   timeout /t 1 /nobreak
 )
+  )
 :break
 
-echo Database started successfully^^!
+echo Database started successfully^!
