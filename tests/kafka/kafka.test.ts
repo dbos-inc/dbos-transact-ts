7ad3094b0f980f06d125c4e9c1d96190658304f3
Peter Kraft <petereliaskraft@gmail.com>
2024-03-24 12:56:14 -0700

    kafka test

---
+++ tests/kafka/kafka.test.ts
@@ -0,0 +1,68 @@
+import { TestingRuntime } from "../../src";
+import { DBOSConfig } from "../../src/dbos-executor";
+import { createInternalTestRuntime } from "../../src/testing/testing_runtime";
+import { generateDBOSTestConfig, setUpDBOSTestDb } from "../helpers";
+import { Kafka, Partitioners } from "kafkajs";
+import { sleep } from "../../src/utils";
+
+describe("kafka-tests", () => {
+  let username: string;
+  let config: DBOSConfig;
+  let testRuntime: TestingRuntime;
+
+  beforeAll(async () => {
+    config = generateDBOSTestConfig();
+    username = config.poolConfig.user || "postgres";
+    await setUpDBOSTestDb(config);
+  });
+
+  beforeEach(async () => {
+    testRuntime = await createInternalTestRuntime([DBOSTestClass], config);
+  });
+
+  afterEach(async () => {
+    await testRuntime.destroy();
+  });
+
+  test("simple-kafka", async () => {
+    const kafka = new Kafka({
+      clientId: 'my-app',
+      brokers: ['localhost:9092'],
+    })
+
+    const producer = kafka.producer({
+      createPartitioner: Partitioners.DefaultPartitioner
+    });
+
+    await producer.connect()
+    await producer.send({
+      topic: 'test-topic',
+      messages: [
+        { value: 'Hello KafkaJS user!' },
+      ],
+    })
+
+    await producer.disconnect()
+
+    const consumer = kafka.consumer({ groupId: 'test-group' })
+
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          value: message.value!.toString(),
+        })
+      },
+    })
+
+    await sleep(1000);
+
+    await consumer.disconnect()
+  }, 15000);
+});
+
+class DBOSTestClass {
+
+}
