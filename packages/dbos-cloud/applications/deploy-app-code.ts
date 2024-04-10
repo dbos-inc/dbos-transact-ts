7006e8051d137df9d1aa723797a9dcab1ecf02fd
Chuck B <chuck.bear@dbos.dev>
2024-04-10 19:13:44 -0400

    Use debug log level, cleans up the code a bit

--- packages/dbos-cloud/applications/deploy-app-code.ts
+++ packages/dbos-cloud/applications/deploy-app-code.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { existsSync, readFileSync } from 'fs';
-import { handleAPIErrors, dbosConfigFilePath, getCloudCredentials, getLogger, checkReadFile, sleep, isCloudAPIErrorResponse, retrieveApplicationName, dbosEnvPath, CloudAPIErrorResponse } from "../cloudutils.js";
+import { handleAPIErrors, dbosConfigFilePath, getCloudCredentials, getLogger, checkReadFile, sleep, isCloudAPIErrorResponse, retrieveApplicationName, dbosEnvPath, CloudAPIErrorResponse, CLILogger } from "../cloudutils.js";
 import path from "path";
 import { Application } from "./types.js";
 import JSZip from "jszip";
@@ -18,7 +18,7 @@ function convertPathForGlob(p: string) {
   }
   return p;
 }
-async function createZipData(verbose: boolean, logger: {info: (arg: string)=>void}): Promise<string> {
+async function createZipData(logger: CLILogger): Promise<string> {
     const zip = new JSZip();
 
     const globPattern = convertPathForGlob(path.join(process.cwd(), '**', '*'));
@@ -36,7 +36,7 @@ async function createZipData(verbose: boolean, logger: {info: (arg: string)=>voi
     });
 
     files.forEach(file => {
-        if (verbose) logger.info(`    Zipping file ${file}`);
+        logger.debug(`    Zipping file ${file}`);
         const relativePath = path.relative(process.cwd(), file).replace(/\\/g, '/');
         const fileData = readFileSync(file);
         zip.file(relativePath, fileData, { binary: true });
@@ -44,45 +44,44 @@ async function createZipData(verbose: boolean, logger: {info: (arg: string)=>voi
 
     // Add the interpolated config file at package root
 
-    if (verbose) logger.info(`    Interpreting configuration from ${dbosConfigFilePath}`);
-    const interpolatedConfig = readInterpolatedConfig(dbosConfigFilePath, verbose, logger);
+    logger.debug(`    Interpreting configuration from ${dbosConfigFilePath}`);
+    const interpolatedConfig = readInterpolatedConfig(dbosConfigFilePath, logger);
     zip.file(dbosConfigFilePath, interpolatedConfig, { binary: true });
 
     // Generate ZIP file as a Buffer
-    if (verbose) logger.info(`    Finalizing zip archive ...`);
+    logger.debug(`    Finalizing zip archive ...`);
     const buffer = await zip.generateAsync({ type: 'nodebuffer' });
-    if (verbose) logger.info(`    ... zip archive complete.`);
+    logger.debug(`    ... zip archive complete.`);
     return buffer.toString('base64');
 }
 
-
 export async function deployAppCode(host: string, rollback: boolean, verbose: boolean): Promise<number> {
-  const logger = getLogger();
-  if (verbose) logger.info("Getting cloud credentials...");
+  const logger = getLogger(verbose);
+  logger.debug("Getting cloud credentials...");
   const userCredentials = getCloudCredentials();
   const bearerToken = "Bearer " + userCredentials.token;
-  if (verbose) logger.info("  ... got cloud credentials");
+  logger.debug("  ... got cloud credentials");
 
-  if (verbose) logger.info("Retrieving app name...");
+  logger.debug("Retrieving app name...");
   const appName = retrieveApplicationName(logger);
   if (!appName) {
-    if (verbose) logger.warn("Failed to get app name.");
+    logger.warn("Failed to get app name.");
     return 1;
   }
-  if (verbose) logger.info(`  ... app name is ${appName}.`);
+  logger.debug(`  ... app name is ${appName}.`);
 
   // Verify that package-lock.json exists
-  if (verbose) logger.info("Checking for package-lock.json...");
+  logger.debug("Checking for package-lock.json...");
   if (!existsSync(path.join(process.cwd(), 'package-lock.json'))) {
     logger.error("package-lock.json not found. Please run 'npm install' before deploying.")
     return 1;
   }
-  if (verbose) logger.info("  ... package-lock.json exists.");
+  logger.debug("  ... package-lock.json exists.");
 
   try {
-    if (verbose) logger.info("Creating application zip ...");
-    const zipData = await createZipData(verbose, logger);
-    if (verbose) logger.info("  ... application zipped.");
+    logger.debug("Creating application zip ...");
+    const zipData = await createZipData(logger);
+    logger.debug("  ... application zipped.");
 
     // Submit the deploy request
     let url = '';
@@ -161,16 +160,16 @@ export async function deployAppCode(host: string, rollback: boolean, verbose: bo
   }
 }
 
-function readInterpolatedConfig(configFilePath: string, verbose: boolean, logger: {info: (arg: string)=>void}): string {
+function readInterpolatedConfig(configFilePath: string, logger: CLILogger): string {
   const configFileContent = checkReadFile(configFilePath) as string;
   const regex = /\${([^}]+)}/g;  // Regex to match ${VAR_NAME} style placeholders
   return configFileContent.replace(regex, (_, g1: string) => {
     if (process.env[g1] !== undefined) {
-      if (verbose) logger.info(`      Substituting value of '${g1}' from process environment.`);
+      logger.debug(`      Substituting value of '${g1}' from process environment.`);
       return process.env[g1] ?? "";
     }
 
-    if (verbose) logger.info(`      Variable '${g1}' would be substituted from the process environment, but is not defined.`);
+    logger.warn(`      Variable '${g1}' would be substituted from the process environment, but is not defined.`);
     return "";
   });
 }
