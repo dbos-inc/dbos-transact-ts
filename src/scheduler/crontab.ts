3dfd59c1b0e5c1f167ff7ce05b44648d49ce58df
Chuck B <chuck.bear@dbos.dev>
2024-04-25 13:12:48 -0400

    Add pattern validator

--- src/scheduler/crontab.ts
+++ src/scheduler/crontab.ts
@@ -124,7 +124,7 @@ function convertSteps(expressions: string[]){
         if (isStepValue){
             const baseDivider = match[2];
             if(isNaN(parseInt(baseDivider))){
-                throw baseDivider + ' is not a valid step value';
+                throw new Error(baseDivider + ' is not a valid step value');
             }
             const values = match[1].split(',');
             const stepValues = [];
@@ -152,6 +152,73 @@ function normalizeIntegers(expressions: string[]) {
     return expressions;
 }
 
+const validationRegex = /^(?:\d+|\*|\*\/\d+)$/;
+
+// Check comma-delimited list to see if elements are in range
+function isValidExpression(expression:string, min:number, max:number): boolean {
+    const options = expression.split(',');
+
+    for (const option of options) {
+        const optionAsInt = parseInt(option, 10);
+
+        if ((!Number.isNaN(optionAsInt) &&
+                (optionAsInt < min || optionAsInt > max)) ||
+            !validationRegex.test(option)
+        ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isInvalidSecond(expression: string) { return !isValidExpression(expression, 0, 59); }
+function isInvalidMinute(expression: string) { return !isValidExpression(expression, 0, 59); }
+function isInvalidHour(expression: string) { return !isValidExpression(expression, 0, 23); }
+function isInvalidDayOfMonth(expression: string) { return !isValidExpression(expression, 1, 31); }
+function isInvalidMonth(expression: string) { return !isValidExpression(expression, 1, 12); }
+function isInvalidWeekDay(expression: string) { return !isValidExpression(expression, 0, 7); }
+
+function validateFields(patterns: string[], executablePatterns :string[]) {
+    if (isInvalidSecond(executablePatterns[0]))
+        throw new Error(`${patterns[0]} is a invalid expression for second`);
+
+    if (isInvalidMinute(executablePatterns[1]))
+        throw new Error(`${patterns[1]} is a invalid expression for minute`);
+
+    if (isInvalidHour(executablePatterns[2]))
+        throw new Error(`${patterns[2]} is a invalid expression for hour`);
+
+    if (isInvalidDayOfMonth(executablePatterns[3]))
+        throw new Error(
+            `${patterns[3]} is a invalid expression for day of month`
+        );
+
+    if (isInvalidMonth(executablePatterns[4]))
+        throw new Error(`${patterns[4]} is a invalid expression for month`);
+
+    if (isInvalidWeekDay(executablePatterns[5]))
+        throw new Error(`${patterns[5]} is a invalid expression for week day`);
+}
+
+/**
+ * Validates a Cron-Job expression pattern.
+ *   Throws on error.
+ */
+export function validateCrontab(pattern: string) {
+    if (typeof pattern !== 'string')
+        throw new TypeError('pattern must be a string!');
+
+    const patterns = pattern.split(' ');
+    const executablePatterns = convertExpression(pattern).split(' ');
+
+    if (patterns.length === 5) patterns.unshift('0');
+
+    validateFields(patterns, executablePatterns);
+
+    return executablePatterns.join(' ');
+}
+
 /*
 * The node-cron core allows only numbers (including multiple numbers e.g 1,2).
 * This module is going to translate the month names, week day names and ranges
@@ -169,7 +236,7 @@ function normalizeIntegers(expressions: string[]) {
 *  - expression 1-5 * * * *
 *  - Will be translated to 1,2,3,4,5 * * * *
 */
-export function interpret(crontab: string){
+export function convertExpression(crontab: string){
     let expressions = removeExtraSpaces(crontab).split(' ');
     expressions = prependSecondExpression(expressions);
     expressions[4] = monthNamesConversion(expressions[4]);
