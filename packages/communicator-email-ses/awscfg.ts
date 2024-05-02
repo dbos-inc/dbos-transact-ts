f106ae12dabb46a4e1d870ba96174cd1aa633cc4
Chuck B <chuck.bear@dbos.dev>
2024-05-02 17:23:46 -0400

    Checkpoint

--- packages/communicator-email-ses/awscfg.ts
+++ packages/communicator-email-ses/awscfg.ts
@@ -27,10 +27,17 @@
  *   be specified to the call.  The default will be the first in the list.
  */
 
-import { DBOSContext } from "@dbos-inc/dbos-sdk";
+import { DBOSConfigKeyTypeError, DBOSError } from "../../src/error";
+
+interface ConfigProvider
+{
+    getConfig<T>(key: string): T | undefined;
+    getConfig<T>(key: string, defaultValue: T): T;
+}
 
 export interface AWSServiceConfig
 {
+    name: string,
     region: string,
     credentials: {
       accessKeyId: string,
@@ -43,8 +50,63 @@ export interface AWSServiceConfig
     maxRetries?: number
 }
 
-export function getAWSConfigs(_svccfgname: string, _ctx: DBOSContext) : AWSServiceConfig[]
+export interface AWSCfgFileItem
 {
+    aws_region?: string,
+    aws_access_key_id?: string,
+    aws_secret_access_key?: string,
+    aws_timeout?: number,
+    aws_connect_timeout?: number,
+    aws_max_retries?: number,
+}
+
+export function loadAWSConfigByName(ctx: ConfigProvider, cfgname: string): AWSServiceConfig {
+    const cfgstrs = ctx.getConfig<AWSCfgFileItem|undefined>(cfgname, undefined);
+    if (!cfgstrs) {
+        throw new DBOSConfigKeyTypeError(cfgname, 'AWSCfgFileItem', 'null');
+    }
+
+    if (!cfgstrs.aws_region || typeof(cfgstrs.aws_region) !== 'string') {
+        throw new DBOSConfigKeyTypeError(`${cfgname}.aws_region`, 'string', typeof(cfgstrs.aws_region));
+    }
+    if (!cfgstrs.aws_access_key_id || typeof(cfgstrs.aws_access_key_id) !== 'string') {
+        throw new DBOSConfigKeyTypeError(`${cfgname}.aws_access_key_id`, 'string', typeof(cfgstrs.aws_access_key_id));
+    }
+    if (!cfgstrs.aws_secret_access_key || typeof(cfgstrs.aws_secret_access_key) !== 'string') {
+        throw new DBOSConfigKeyTypeError(`${cfgname}.aws_secret_access_key`, 'string', typeof(cfgstrs.aws_secret_access_key));
+    }
+
+    return {name: cfgname, region: '', credentials: {accessKeyId: "", secretAccessKey:""}};
+
+}
+
+export function loadAWSCongfigsByNames(ctx: ConfigProvider, cfgnames: string) {
     const configs: AWSServiceConfig[] = [];
+    const cfgnamesarr = (cfgnames || 'aws_config').split(',');
+
+    for (const cfgname of cfgnamesarr) {
+        configs.push(loadAWSConfigByName(ctx, cfgname))
+    }
+
     return configs;
+}
+
+export function getAWSConfigs(ctx: ConfigProvider, svccfgname?: string) : AWSServiceConfig[]
+{
+    if (svccfgname) {
+        return loadAWSCongfigsByNames(ctx, ctx.getConfig<string>(svccfgname, ''));
+    }
+    return [loadAWSConfigByName(ctx, 'aws_config')];
+}
+
+export function getAWSConfigForService(ctx: ConfigProvider, svccfgname: string, cfgname: string) : AWSServiceConfig
+{
+    if (svccfgname) {
+        const cfgs = loadAWSCongfigsByNames(ctx, ctx.getConfig<string>(svccfgname, ''));
+        for (const cfg of cfgs) {
+            if (cfg.name.toLowerCase() === cfgname || !cfgname) return cfg;
+        }
+        throw new DBOSError(`Configuration '${cfgname}' does not exist in service '${svccfgname}'`);
+    }
+    return loadAWSConfigByName(ctx, cfgname || 'aws_config');
 }
--- packages/communicator-email-ses/index.ts
+++ packages/communicator-email-ses/index.ts
@@ -1,64 +1,90 @@
-import {Communicator, CommunicatorContext} from '@dbos-inc/dbos-sdk';
+import {Communicator, CommunicatorContext, DBOSInitializer, InitContext} from '@dbos-inc/dbos-sdk';
 
 import { SES } from '@aws-sdk/client-ses';
+import { AWSServiceConfig, getAWSConfigForService, getAWSConfigs } from './awscfg';
 
-class SendEmailCommunicator
+export class SendEmailCommunicator
 {
+    static AWS_SES_CONFIGURATIONS = 'aws_ses_configurations';
+    static AWS_SES_API_VERSION = '2010-12-01';
+
+    @DBOSInitializer()
+    static checkConfig(ctx: InitContext) : Promise<void> {
+        // Get the config and call the validation
+        getAWSConfigs(ctx, SendEmailCommunicator.AWS_SES_CONFIGURATIONS);
+        return Promise.resolve();
+    }
+
     @Communicator()
-    static getCurrentTime(_ctx: CommunicatorContext) : Promise<number> {
-        return Promise.resolve(new Date().getTime());
+    static async sendEmail(
+        _ctx: CommunicatorContext,
+        _cfg:{stage: string, awscfg: string},
+        _mail:{to?:string[], cc?:string[], bcc?:string, from:string, body:string})
+    {
+        /*
+        await ses.sendEmail({
+        Source:"",
+        Destination: {ToAddresses: [""], CcAddresses: [], BccAddresses: []},
+        Message: {Subject: {Data: ""}, Body: {Html: {Data: ""}, Text: {Data: "", Charset: 'utf-8'}}}
+        });
+        */
+    }
+
+    @Communicator()
+    static async sendTemplatedEmail(
+        _ctx: CommunicatorContext,
+        _cfg:{stage: string, awscfg: string},
+        _template:{recipients:string[], from:string, template:string, templateDataJSON:string}
+    )
+    {
+        /*
+        export async function sendTemplatedEmail(
+            ctxt: DBOSContext,
+            recipient: string,
+            templateData: Record<string, string>
+            ): Promise<void> {
+            const ses = new SES({
+              region: SES_REGION,
+              credentials: {
+                accessKeyId: ctxt.getConfig('access_key_id') ?? '',
+                secretAccessKey: ctxt.getConfig('secret_access_key') ?? '',
+              },
+            });
+            const sourceEmail = ctxt.getConfig('source_email') as string;
+            const params = {
+                Destination: {
+                    ToAddresses: [recipient], // Must be verified in SES
+                },
+                Source: sourceEmail, // Must be verified in SES
+                Template: VERIFICATION_LINK_TEMPLATE,
+                TemplateData: JSON.stringify(templateData),
+            };
+        
+            const command = new SendTemplatedEmailCommand(params);
+            const _result = await ses.send(command);
+        */
+    }
+
+
+    // Could also consider ...
+    // sendBulkTemplatedEmail, sendRawEmail, sendCustomVerificationEmail
+
+    static createSES(cfg: AWSServiceConfig) {
+        return new SES({
+            region: cfg.region,
+            credentials: cfg.credentials,
+            maxAttempts: cfg.maxRetries,
+            //apiVersion: SendEmailCommunicator.AWS_SES_API_VERSION,
+            //logger: console,
+        });
     }
 
     /*
-    export async function sendTemplatedEmail(
-    ctxt: DBOSContext,
-    recipient: string,
-    templateData: Record<string, string>
-    ): Promise<void> {
-    const ses = new SES({
-      region: SES_REGION,
-      credentials: {
-        accessKeyId: ctxt.getConfig('access_key_id') ?? '',
-        secretAccessKey: ctxt.getConfig('secret_access_key') ?? '',
-      },
-    });
-    const sourceEmail = ctxt.getConfig('source_email') as string;
-    const params = {
-        Destination: {
-            ToAddresses: [recipient], // Must be verified in SES
-        },
-        Source: sourceEmail, // Must be verified in SES
-        Template: VERIFICATION_LINK_TEMPLATE,
-        TemplateData: JSON.stringify(templateData),
-    };
-
-    const command = new SendTemplatedEmailCommand(params);
-    const _result = await ses.send(command);
-    }*/
-
-    // sendEmail, sendTemplatedEmail, sendBulkTemplatedEmail, sendRawEmail, sendCustomVerificationEmail
-
-    // @Communicator() This maybe should not be a communicator
-    static async createEmailTemplate(templateName: string, subject: string, text: string) {
-        const ses = new SES({
-            // TODO
-            region: "us-east-1",
-            credentials: {
-              accessKeyId: "",
-              secretAccessKey: "",
-            },
-        });
-
-        /*
-    apiVersion: '2010-12-01',
-    region: 'us-east-1',
-    credentials: new AWS.Credentials('ACCESS_KEY_ID', 'SECRET_ACCESS_KEY'),
-    httpOptions: {
-        timeout: 300000,  // 5 minutes
-        connectTimeout: 10000  // 10 seconds
-    },
-    maxRetries: 3,
-    logger: console  // Logs API requests and responses to console         */
+     * Create SES Email Template - This is the non-communicator version
+     */
+    static async createEmailTemplateFunction(cfg: AWSServiceConfig, templateName: string, subject: string, text: string) {
+        // Create SES
+        const ses = SendEmailCommunicator.createSES(cfg);
     
         // Define the email template
         const params = {
@@ -69,17 +95,16 @@ class SendEmailCommunicator
             },
         };
         
-        // Create the email template
+        // Send call to create the email template
         await ses.createTemplate(params);
-        await ses.sendEmail({
-            Source:"",
-            Destination: {ToAddresses: [""], CcAddresses: [], BccAddresses: []},
-            Message: {Subject: {Data: ""}, Body: {Html: {Data: ""}, Text: {Data: "", Charset: 'utf-8'}}}
-        });
     }
-}
 
-export
-{
-    SendEmailCommunicator,
+    /*
+     * Create SES Email template - Communicator version
+     */
+    @Communicator()
+    static async createEmailTemplate(ctx:CommunicatorContext, cfgname:string, templateName: string, subject: string, text: string) {
+        const cfg = getAWSConfigForService(ctx, SendEmailCommunicator.AWS_SES_CONFIGURATIONS, cfgname);
+        return await this.createEmailTemplateFunction(cfg, templateName, subject, text);
+    }
 }
