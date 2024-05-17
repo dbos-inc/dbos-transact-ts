8535ac42e855daa99e0f5d590e32e65362633dc0
Chuck B <chuck.bear@dbos.dev>
2024-05-17 18:13:14 -0400

    Add to row interface

--- migrations/20240517000000_status_class_config.js
+++ migrations/20240517000000_status_class_config.js
@@ -1,11 +1,9 @@
 exports.up = function(knex) {
     return knex.schema.withSchema('dbos')
         .table('workflow_status', function(table) {
-            // Add CREATED_AT column with default value as current epoch time in milliseconds
             table.string('class_name')
                 .defaultTo(null);
 
-            // Add UPDATED_AT column with default value as current epoch time in milliseconds
             table.string('config_name')
                 .defaultTo(null);
         });
