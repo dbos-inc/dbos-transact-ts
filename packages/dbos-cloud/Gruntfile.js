f38db88b49581703913c96d62fe8ede9b1005c09
Harry Pierson <harrypierson@hotmail.com>
2024-05-23 16:12:57 -0700

    remove grunt dependencies + files (#467)
    we originally used grunt to set package version via nerdbank git
    version. However, we now do this directly with the `npm version` command
    in the `artifact.yml` GH Workflow so we don't need grunt anymore.
    
    Assuming you have .NET runtime installed, you can stamp versions locally
    like this:
    
    ```sh
    version=$(npx nbgv get-version -v NpmPackageVersion)
    npm version --workspaces --include-workspace-root --no-git-tag-version "$version"
    ```

--- packages/dbos-cloud/Gruntfile.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-var nbgv = require('nerdbank-gitversioning')
-
-module.exports = function (grunt) {
-    grunt.registerTask('setversion', function () {
-        var done = this.async();
-        nbgv.setPackageVersion().then(() => done());
-    });
-};
