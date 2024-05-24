470ba19e879efe111d0a5b580fe385c0e47f6f84
Demetris Manikas <dmanikas@admin.grnet.gr>
2024-05-25 01:22:26 +0300

    add date handling in DBOSReplacer BBOSReviver

--- src/utils.ts
+++ src/utils.ts
@@ -18,7 +18,7 @@ export function readFileSync(path: string, encoding: BufferEncoding = "utf8"): s
   });
 
   // Then, read its content
-  return fs.readFileSync(path, { encoding } );
+  return fs.readFileSync(path, { encoding });
 }
 
 export const sleepms = (ms: number) => new Promise((r) => setTimeout(r, ms));
@@ -27,25 +27,25 @@ export type ValuesOf<T> = T[keyof T];
 
 // Adapated and translated from from: https://github.com/junosuarez/find-root
 export function findPackageRoot(start: string | string[]): string {
-    if (typeof start === 'string') {
-        if (!start.endsWith(path.sep)) {
-            start += path.sep;
-        }
-        start = start.split(path.sep);
+  if (typeof start === 'string') {
+    if (!start.endsWith(path.sep)) {
+      start += path.sep;
     }
+    start = start.split(path.sep);
+  }
 
-    if (start.length === 0) {
-        throw new Error('package.json not found in path');
-    }
+  if (start.length === 0) {
+    throw new Error('package.json not found in path');
+  }
 
-    start.pop();
-    const dir = start.join(path.sep);
+  start.pop();
+  const dir = start.join(path.sep);
 
-    if (fs.existsSync(path.join(dir, 'package.json'))) {
-        return dir;
-    }
+  if (fs.existsSync(path.join(dir, 'package.json'))) {
+    return dir;
+  }
 
-    return findPackageRoot(start);
+  return findPackageRoot(start);
 }
 
 /**
@@ -60,19 +60,32 @@ export function findPackageRoot(start: string | string[]): string {
  * TODO: Use in other contexts where we perform serialization and deserialization.
  */
 
-export function DBOSReplacer(_key: string, value: unknown) {
-  return value;
-}
-
+const JSON_DATE_VALUE_PREFIX = 'date:'
 interface SerializedBuffer {
   type: 'Buffer';
   data: number[];
+}
+
+//https://www.typescriptlang.org/docs/handbook/2/functions.html#declaring-this-in-a-function
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function DBOSReplacer(this: any, key: string, value: unknown) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-assignment
+  const actualValue = this[key];
+  if (actualValue instanceof Date) {
+    return `${JSON_DATE_VALUE_PREFIX}${actualValue.getTime()}`;
+    // return `${JSON_DATE_VALUE_PREFIX}${actualValue.toUTCString()}`;
+  }
+  return value;
 }
 
 export function DBOSReviver(_key: string, value: unknown): unknown {
   const candidate = value as SerializedBuffer;
   if (candidate && candidate.type === 'Buffer' && Array.isArray(candidate.data)) {
     return Buffer.from(candidate.data);
+  }
+  if (typeof value === 'string' && value.startsWith(JSON_DATE_VALUE_PREFIX)) {
+    return new Date(parseInt(value.slice(JSON_DATE_VALUE_PREFIX.length)))
+    //return new Date(Date.parse(value.slice(JSON_DATE_VALUE_PREFIX.length)))
   }
   return value;
 }
