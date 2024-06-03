18e1e9e6f009569fd13410df800de0127d05aae8
Harry Pierson <harry.pierson@dbos.dev>
2024-06-03 15:18:34 -0700

    fix stored-proc-tests

--- tests/proc.test.ts
+++ tests/proc.test.ts
@@ -1,13 +1,13 @@
 import { StoredProcedure, StoredProcedureContext, TestingRuntime, Workflow, WorkflowContext, WorkflowHandle } from "../src";
 import { DBOSConfig } from "../src/dbos-executor";
 import { createInternalTestRuntime } from "../src/testing/testing_runtime";
-import { generateDBOSTestConfig, setUpDBOSTestDb } from "./helpers";
+import { TestKvTable, generateDBOSTestConfig, setUpDBOSTestDb } from "./helpers";
 
 class ProcTest {
     @StoredProcedure()
     static async testProc(ctx: StoredProcedureContext, name: string): Promise<string> {
-        await Promise.resolve();
-        return 'Hello, ' + name;
+        const { rows } = await ctx.query<TestKvTable>(`INSERT INTO dbos_test_kv(id, value) VALUES (1, $1) RETURNING id`, [name]);
+        return `hello ${rows[0].id}`;
     }
 
     @Workflow()
@@ -16,7 +16,7 @@ class ProcTest {
     }
 }
 
-describe("dbos-tests", () => {
+describe("stored-proc-tests", () => {
     let username: string;
     let config: DBOSConfig;
     let testRuntime: TestingRuntime;
@@ -29,16 +29,43 @@ describe("dbos-tests", () => {
 
     beforeEach(async () => {
         testRuntime = await createInternalTestRuntime([ProcTest], config);
+        await testRuntime.queryUserDB(create);
     });
 
     afterEach(async () => {
+        await testRuntime.queryUserDB(drop);
         await testRuntime.destroy();
     });
 
     test("simple-function", async () => {
         const workflowHandle: WorkflowHandle<string> = await testRuntime.startWorkflow(ProcTest).testWorkflow(username);
         const workflowResult: string = await workflowHandle.getResult();
-        expect(JSON.parse(workflowResult)).toEqual({ current_user: username });
+        expect(workflowResult).toEqual("hello 1");
       });
-    
 })
+
+const create = `
+CREATE OR REPLACE PROCEDURE "testProc_proc"(
+    buffered_results JSONB,
+    _workflow_uuid TEXT, 
+    _function_id INT, 
+    preset BOOLEAN, 
+    OUT return_value JSONB,
+    "name" TEXT
+)
+LANGUAGE plpgsql
+as $$
+DECLARE
+    _output JSONB;
+    _error JSONB;
+    _snapshot TEXT;
+    _txn_id TEXT;
+BEGIN
+
+    -- fake output for the test
+    SELECT jsonb_build_object('output', 'hello 1', 'txn_snapshot', '1234', 'txn_id', '1234:1234:') INTO return_value;
+
+END; $$;
+`
+
+const drop = `DROP ROUTINE IF EXISTS "testProc_proc";`
