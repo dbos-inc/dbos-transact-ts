765d76cfe15fda439d4ec8a3eb77e2100ecc07cb
Qian Li <qian.li@dbos.dev>
2024-06-05 12:02:37 -0700

    Add Prisma Template (#487)
    This PR adds a new create template using Prisma. Also fixes an issue
    where the prisma client wasn't loaded from the correct folder if we run
    our template app inside of Transact.
    
    To use this template:
    ```
    npx @dbos-inc/create@latest -t hello-prisma -n <app-name>
    ```

---
+++ packages/create/templates/hello-prisma/src/operations.ts
@@ -0,0 +1,18 @@
+import { TransactionContext, Transaction, GetApi } from '@dbos-inc/dbos-sdk';
+import { PrismaClient } from "@prisma/client";
+
+export class Hello {
+
+  @GetApi('/greeting/:name')
+  @Transaction()
+  static async helloTransaction(txnCtxt: TransactionContext<PrismaClient>, name: string)  {
+    const greeting = `Hello, ${name}!`;
+    const p: PrismaClient = txnCtxt.client as PrismaClient;
+    const res = await p.dbosHello.create({
+      data: {
+        greeting: greeting,
+      },
+    });
+    return `Greeting ${res.greeting_id}: ${greeting}`;
+  }
+}
