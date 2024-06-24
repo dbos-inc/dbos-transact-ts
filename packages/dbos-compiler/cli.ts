eb3d836aad4a25c0a08c5aa0bc19003c1ed3546c
Harry Pierson <harry.pierson@dbos.dev>
2024-06-24 09:45:29 -0700

    add drop command

--- packages/dbos-compiler/cli.ts
+++ packages/dbos-compiler/cli.ts
@@ -41,6 +41,18 @@ async function deployToDatabase(config: pg.ClientConfig, result: CompileResult, 
   }
 }
 
+async function dropFromDatabase(config: pg.ClientConfig, result: CompileResult, appVersion?: string | boolean) {
+  const client = new pg.default.Client(config);
+  try {
+    await client.connect();
+    console.log(`Dropping from database: ${client.host}:${client.port ?? 5432}/${client.database}`);
+    const executeSql = async (sql: string) => { await client.query(sql); };
+    await generateDrop(executeSql, result, appVersion);
+  } finally {
+    await client.end();
+  }
+}
+
 function getPackageVersion(): string {
   const __dirname = import.meta.dirname;
   const packageJsonPath = path.join(__dirname, "..", "package.json");
@@ -100,7 +112,6 @@ interface DeployOptions {
 
 program
   .command("deploy")
-  .description("Start the server")
   .argument('[tsconfigPath]', 'path to tsconfig.json')
   .option("-d, --appDir <string>", "Specify the application root directory")
   .option('--app-version <string>', 'override DBOS__APPVERSION environment variable')
@@ -113,6 +124,24 @@ program
       if (compileResult) {
         const [dbosConfig,] = parseConfigFile(options);
         await deployToDatabase(dbosConfig.poolConfig, compileResult, options.appVersion);
+      }
+    }
+  });
+
+program
+  .command("drop")
+  .argument('[tsconfigPath]', 'path to tsconfig.json')
+  .option("-d, --appDir <string>", "Specify the application root directory")
+  .option('--app-version <string>', 'override DBOS__APPVERSION environment variable')
+  .option('--no-app-version', 'ignore DBOS__APPVERSION environment variable')
+  .action(async (tsconfigPath: string | undefined, options: DeployOptions) => {
+    tsconfigPath = verifyTsConfigPath(tsconfigPath, options.appDir);
+
+    if (tsconfigPath) {
+      const compileResult = compile(tsconfigPath);
+      if (compileResult) {
+        const [dbosConfig,] = parseConfigFile(options);
+        await dropFromDatabase(dbosConfig.poolConfig, compileResult, options.appVersion);
       }
     }
   });
