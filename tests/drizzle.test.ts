a56d542b627537ac6a6fced638c0ae352dcfbc46
Peter Kraft <petereliaskraft@gmail.com>
2024-08-05 16:14:32 -0700

    more test

--- tests/drizzle.test.ts
+++ tests/drizzle.test.ts
@@ -1,5 +1,12 @@
+import request from "supertest";
 
 import {
+  Authentication,
+  GetApi,
+  HandlerContext,
+  MiddlewareContext,
+  PostApi,
+  RequiredRole,
   TestingRuntime, Transaction,
   TransactionContext,
   Workflow,
@@ -15,6 +22,7 @@ import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { eq } from "drizzle-orm";
 import { v1 as uuidv1 } from "uuid";
 import { DatabaseError } from "pg";
+import { DBOSNotAuthorizedError } from "../src/error";
 
 const testTableName = "dbos_test_kv";
 
@@ -106,5 +114,89 @@ describe("drizzle-tests", () => {
       const err: DatabaseError = e as DatabaseError;
       expect(err.code).toBe("23505");
     }
+  });
+});
+
+const userTableName = 'dbos_test_user';
+
+const userTable = pgTable(userTableName, {
+  id: serial('id').primaryKey(),
+  username: text('username')
+});
+
+
+@Authentication(DUserManager.authMiddlware)
+export class DUserManager {
+  @Transaction()
+  @PostApi('/register')
+  static async createUser(ctx: TransactionContext<NodePgDatabase>, uname: string) {
+    return await ctx.client.insert(userTable).values({ username: uname }).returning({ id: userTable.id })
+  }
+
+  @GetApi('/hello')
+  @RequiredRole(['user'])
+  static async hello(hCtxt: HandlerContext) {
+    return Promise.resolve({ messge: "hello " + hCtxt.authenticatedUser });
+  }
+
+  static async authMiddlware(ctx: MiddlewareContext) {
+    if (!ctx.requiredRole || !ctx.requiredRole.length) {
+      return;
+    }
+    const { user } = ctx.koaContext.query;
+    if (!user) {
+      throw new DBOSNotAuthorizedError("User not provided", 401);
+    }
+    const u = await ctx.query(
+      async (c: NodePgDatabase, uname: string) => {
+        return await c.select().from(userTable).where(eq(userTable.username, uname));
+      }, user as string
+    );
+
+    if (!u || !u.length) {
+      throw new DBOSNotAuthorizedError("User does not exist", 403);
+    }
+    ctx.logger.info(`Allowed in user: ${u[0].username}`);
+    return {
+      authenticatedUser: u[0].username!,
+      authenticatedRoles: ["user"],
+    };
+  }
+}
+
+describe("drizzle-auth-tests", () => {
+  let config: DBOSConfig;
+  let testRuntime: TestingRuntime;
+
+  beforeAll(async () => {
+    config = generateDBOSTestConfig(UserDatabaseName.DRIZZLE);
+    await setUpDBOSTestDb(config);
+  });
+
+  beforeEach(async () => {
+    testRuntime = await createInternalTestRuntime(undefined, config);
+    await testRuntime.queryUserDB(`DROP TABLE IF EXISTS ${userTableName};`);
+    await testRuntime.queryUserDB(`CREATE TABLE IF NOT EXISTS ${userTableName} (id SERIAL PRIMARY KEY, username TEXT);`);
+  });
+
+  afterEach(async () => {
+    await testRuntime.queryUserDB(`DROP TABLE IF EXISTS ${userTableName};`);
+    await testRuntime.destroy();
+  });
+
+  test("simple-drizzle-auth", async () => {
+    // No user name
+    const response1 = await request(testRuntime.getHandlersCallback()).get("/hello");
+    expect(response1.statusCode).toBe(401);
+
+    // User name doesn't exist
+    const response2 = await request(testRuntime.getHandlersCallback()).get("/hello?user=paul");
+    expect(response2.statusCode).toBe(403);
+
+    const response3 = await request(testRuntime.getHandlersCallback()).post("/register").send({ uname: "paul" });
+    expect(response3.statusCode).toBe(200);
+
+    const response4 = await request(testRuntime.getHandlersCallback()).get("/hello?user=paul");
+    expect(response4.statusCode).toBe(200);
   });
 });
