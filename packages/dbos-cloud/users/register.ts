a19112bea99c77bc3d1481e994e002c27a5b884b
Qian Li <qian.li@dbos.dev>
2024-08-18 14:19:03 -0700

    warning message

--- packages/dbos-cloud/users/register.ts
+++ packages/dbos-cloud/users/register.ts
@@ -2,6 +2,9 @@ import { getCloudCredentials, getLogger } from "../cloudutils.js";
 
 export async function registerUser(username: string, secret: string | undefined, host: string): Promise<number> {
   const logger = getLogger();
-  await getCloudCredentials(host, logger, username, secret);
+  const credentials = await getCloudCredentials(host, logger, username, secret);
+  if (credentials.userName !== username) {
+    logger.warn(`You are trying to register ${username}, but are currently authenticated as ${credentials.userName}. To register a new user, please run "npx dbos-cloud logout".`)
+  }
   return 0;
 }
