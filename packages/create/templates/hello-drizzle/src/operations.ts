703b6974acd1bd5734797c9631f508ce4fc5a37d
Alex Poliakov <alex.poliakov@dbos.dev>
2024-08-27 21:20:31 -0400

    Add readme to each template with links back to doc (#612)

--- packages/create/templates/hello-drizzle/src/operations.ts
+++ packages/create/templates/hello-drizzle/src/operations.ts
@@ -2,6 +2,12 @@ import { HandlerContext, TransactionContext, Transaction, GetApi } from '@dbos-i
 import { dbosHello } from './schema';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 
+const app_notes = `
+To learn how to run this app on your computer, visit the
+<a href="https://docs.dbos.dev/getting-started/quickstart" >DBOS Quickstart</a>.<br>
+After that, to learn how to build apps, visit the
+<a href="https://docs.dbos.dev/getting-started/quickstart-programming" >DBOS Programming Guide</a>.`;
+
 export class Hello {
 
   @GetApi('/') // Serve a quick readme for the app
@@ -10,7 +16,8 @@ export class Hello {
            Welcome to the DBOS Hello App!<br><br>
            Visit the route /greeting/:name to be greeted!<br>
            For example, visit <a href="/greeting/dbos">/greeting/dbos</a>.<br>
-           The counter increments with each page visit.<br>
+           The counter increments with each page visit.<br><br>
+           ${app_notes}
            </p></body></html>`;
     return Promise.resolve(readme);
   }
@@ -20,6 +27,8 @@ export class Hello {
   static async helloTransaction(ctxt: TransactionContext<NodePgDatabase>, user: string) {
     const greeting = `Hello, ${user}!`;
     const greetings_output = await ctxt.client.insert(dbosHello).values({greeting}).returning({greet_count: dbosHello.greet_count});
-    return `${greeting} We have made ${greetings_output[0].greet_count} greetings.\n`;
+    const greeting_message = `${greeting} We have made ${greetings_output[0].greet_count} greetings.`;
+    const page = `<html><body><p>${greeting_message}<br><br>${app_notes}</p></body></html>`;
+    return page;
   }
 }
--- packages/create/templates/hello-prisma/src/operations.ts
+++ packages/create/templates/hello-prisma/src/operations.ts
@@ -1,6 +1,12 @@
 import { HandlerContext, TransactionContext, Transaction, GetApi } from '@dbos-inc/dbos-sdk';
 import { PrismaClient } from "@prisma/client";
 
+const app_notes = `
+To learn how to run this app on your computer, visit the
+<a href="https://docs.dbos.dev/getting-started/quickstart" >DBOS Quickstart</a>.<br>
+After that, to learn how to build apps, visit the
+<a href="https://docs.dbos.dev/getting-started/quickstart-programming" >DBOS Programming Guide</a>.`;
+
 export class Hello {
 
   @GetApi('/') // Serve a quick readme for the app
@@ -9,7 +15,8 @@ export class Hello {
           Welcome to the DBOS Hello App!<br><br>
           Visit the route /greeting/:name to be greeted!<br>
           For example, visit <a href="/greeting/dbos">/greeting/dbos</a>.<br>
-          The counter increments with each page visit.<br>
+          The counter increments with each page visit.<br><br>
+          ${app_notes}
           </p></body></html>`;
     return Promise.resolve(readme);
   }
@@ -23,6 +30,8 @@ export class Hello {
         greeting: greeting,
       },
     });
-    return `Greeting ${res.greeting_id}: ${greeting}`;
+    const greeting_note = `Greeting ${res.greeting_id}: ${greeting}`;
+    const page = `<html><body><p>${greeting_note}<br><br>${app_notes}</p></body></html>`;
+    return page;
   }
 }
