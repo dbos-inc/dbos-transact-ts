adb12526591f8edbb45463e81af7d7ccdc68bb53
Chuck B <chuck.bear@dbos.dev>
2024-09-26 10:15:09 -0400

    Test serial Q

--- tests/wfqueue.test.ts
+++ tests/wfqueue.test.ts
@@ -4,9 +4,11 @@ import { createInternalTestRuntime } from "../src/testing/testing_runtime";
 import { generateDBOSTestConfig, setUpDBOSTestDb } from "./helpers";
 import { WorkflowQueue } from "../src";
 import { v4 as uuidv4 } from "uuid";
+import { sleepms } from "../src/utils";
 
 
 const queue = new WorkflowQueue("testQ");
+const serialqueue = new WorkflowQueue("serialQ", 1);
 
 describe("scheduled-wf-tests-simple", () => {
     let config: DBOSConfig;
@@ -14,6 +16,7 @@ describe("scheduled-wf-tests-simple", () => {
   
     beforeAll(async () => {
         TestWFs.reset();
+        TestWFs2.reset();
         config = generateDBOSTestConfig();
         await setUpDBOSTestDb(config);  
     });
@@ -40,6 +43,24 @@ describe("scheduled-wf-tests-simple", () => {
 
         expect((await wfh.getStatus())?.queueName).toBe('testQ');
     });
+
+    test("one-at-a-time", async() => {
+        let wfRes: ()=>void = ()=>{};
+        TestWFs2.wfPromise = new Promise<void>((resolve, _rj) => {wfRes = resolve});
+        const mainPromise  = new Promise<void>((resolve, _rj) => {TestWFs2.mainResolve = resolve;});
+        const wfh1 = await testRuntime.startWorkflow(TestWFs2, undefined, undefined, serialqueue).workflowOne();
+        expect((await wfh1.getStatus())?.queueName).toBe('serialQ');
+        const wfh2 = await testRuntime.startWorkflow(TestWFs2, undefined, undefined, serialqueue).workflowTwo();
+        expect((await wfh2.getStatus())?.queueName).toBe('serialQ');
+        await mainPromise;
+        await sleepms(2000);
+        expect(TestWFs2.flag).toBeFalsy();
+        wfRes?.();
+        await wfh1.getResult();
+        await wfh2.getResult();
+        expect(TestWFs2.flag).toBeTruthy();
+        expect(TestWFs2.wfCounter).toBe(1);
+    }, 10000);
 });
 
 
@@ -69,36 +90,31 @@ class TestWFs
     }
 }
 
-/*
-+def test_one_at_a_time(dbos: DBOS) -> None:
-+    wf_counter = 0
-+    flag = False
-+    workflow_event = threading.Event()
-+    main_thread_event = threading.Event()
-+
-+    @DBOS.workflow()
-+    def workflow_one() -> None:
-+        nonlocal wf_counter
-+        wf_counter += 1
-+        main_thread_event.set()
-+        workflow_event.wait()
-+
-+    @DBOS.workflow()
-+    def workflow_two() -> None:
-+        nonlocal flag
-+        flag = True
-+
-+    queue = Queue("test_queue", 1)
-+    handle1 = queue.enqueue(workflow_one)
-+    handle2 = queue.enqueue(workflow_two)
-+
-+    main_thread_event.wait()
-+    time.sleep(2)  # Verify the other task isn't scheduled on subsequent poller ticks.
-+    assert not flag
-+    workflow_event.set()
-+    assert handle1.get_result() == None
-+    assert handle2.get_result() == None
-+    assert flag
-+    assert wf_counter == 1
-+
-*/
+class TestWFs2
+{
+    static wfCounter = 0;
+    static flag = false;
+    static wfid: string;
+    static mainResolve?: () => void;
+    static wfPromise?: Promise<void>;
+
+    static reset() {
+        TestWFs2.wfCounter = 0;
+        TestWFs2.flag = false;
+    }
+
+    @Workflow()
+    static async workflowOne(_ctx: WorkflowContext) {
+        ++TestWFs2.wfCounter;
+        TestWFs2.mainResolve?.();
+        await TestWFs2.wfPromise;
+        return Promise.resolve();
+    }
+
+    @Workflow()
+    static async workflowTwo(_ctx: WorkflowContext) {
+        TestWFs2.flag = true; // Tell if this ran yet
+        return Promise.resolve();
+    }
+}
+
