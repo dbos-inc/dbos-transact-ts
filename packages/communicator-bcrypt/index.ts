2ac766d321aff26abe43eb62ac3725e9f5635920
Chuck B <chuck.bear@dbos.dev>
2024-10-01 09:11:47 -0400

    Probably better to be backwards compat

--- packages/communicator-bcrypt/index.ts
+++ packages/communicator-bcrypt/index.ts
@@ -1,19 +1,19 @@
 
-import {Step, StepContext} from '@dbos-inc/dbos-sdk';
+import {Communicator, CommunicatorContext} from '@dbos-inc/dbos-sdk';
 
 import bcryptjs from 'bcryptjs';
 
 
 class BcryptCommunicator
 {
-    @Step()
-    static async bcryptGenSalt(_ctx: StepContext, saltRounds: number = 10) : Promise<string>
+    @Communicator()
+    static async bcryptGenSalt(_ctx: CommunicatorContext, saltRounds: number = 10) : Promise<string>
     {
         return await bcryptjs.genSalt(saltRounds);
     }
 
-    @Step()
-    static async bcryptHash(_ctx: StepContext, txt: string, saltRounds: number = 10) : Promise<string> {
+    @Communicator()
+    static async bcryptHash(_ctx: CommunicatorContext, txt: string, saltRounds: number = 10) : Promise<string> {
         return await bcryptjs.hash(txt, saltRounds);
     }
 
