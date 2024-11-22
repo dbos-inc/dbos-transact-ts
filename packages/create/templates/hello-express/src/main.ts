be7fc252d4949e18e18d9641848f237e081cdf6c
Chuck B <chuck.bear@dbos.dev>
2024-11-22 10:27:02 -0500

    Fix port

--- packages/create/templates/hello-express/src/main.ts
+++ packages/create/templates/hello-express/src/main.ts
@@ -2,10 +2,10 @@ import { app } from './operations';
 import { DBOS } from '@dbos-inc/dbos-sdk';
 
 async function main() {
-  const PORT = 9000;
-  const ENV = process.env.NODE_ENV || 'development';
-
   await DBOS.launch({expressApp: app});
+
+  const PORT = DBOS.runtimeConfig?.port ?? 9000;
+  const ENV = process.env.NODE_ENV || 'development';
 
   app.listen(PORT, () => {
     console.log(`🚀 Server is running on http://localhost:${PORT}`);
