76007007123d7999d636eaa7d820de6b7cbc8e36
Chuck B <chuck.bear@dbos.dev>
2024-12-13 22:39:12 -0500

    Name back

--- src/dbos.ts
+++ src/dbos.ts
@@ -815,7 +815,7 @@ export class DBOS {
     return decorator;
   }
 
-  static storedTransaction(config: StoredProcedureConfig={}) {
+  static storedProcedure(config: StoredProcedureConfig={}) {
     function decorator<This, Args extends unknown[], Return>(
       target: object,
       propertyKey: string,
