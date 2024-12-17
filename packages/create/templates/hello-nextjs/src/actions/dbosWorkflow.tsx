198789c440722b6cff0d837d8fd95f923aab6b6b
manojdbos <manoj.khangaonkar@dbos.dev>
2024-12-17 15:05:08 -0800

    working template

---
+++ packages/create/templates/hello-nextjs/src/actions/dbosWorkflow.tsx
@@ -0,0 +1,32 @@
+"use server";
+
+import { DBOS } from "@dbos-inc/dbos-sdk";
+
+// The schema of the database table used in this example.
+export interface dbos_hello {
+    name: string;
+    greet_count: number;
+  }
+
+// Order matters: DBOS decorators must be declared before being used
+class dbosWorkflowClass {
+    @DBOS.transaction()
+    static async helloDBOS(userName: string) {
+        DBOS.logger.info("Hello from DBOS Transaction!");
+            // Retrieve and increment the number of times this user has been greeted.
+            const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
+            const { rows } = await DBOS.sqlClient.client.raw(query, [userName]) as { rows: dbos_hello[] };
+            const greet_count = rows[0].greet_count;
+            const greeting = `Hello, ${userName}! You have been greeted ${greet_count} times.`;
+            return greeting;
+    }
+}
+
+if (process.env.NEXT_PHASE !== "phase-production-build") {
+    await DBOS.launch();
+}
+
+export async function dbosWorkflow(userName: string) {
+    DBOS.logger.info("Hello from DBOS!");
+    return await dbosWorkflowClass.helloDBOS(userName);
+}
---
+++ packages/create/templates/hello-nextjs/src/components/server/callDBOSWorkflow.tsx
@@ -0,0 +1,7 @@
+"use server";
+
+import CallDBOSWorkflowClient from "../client/callDBOSWorkflow";
+
+export default async function CallDBOSWorkflow() {
+    return <CallDBOSWorkflowClient wfResult="" />;
+}
