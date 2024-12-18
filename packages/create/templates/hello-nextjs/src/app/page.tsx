65635a85c2019220b1ca2e61d885072d76cbc79d
manojdbos <manoj.khangaonkar@dbos.dev>
2024-12-18 15:42:02 -0800

    change nextjs links to dbos

--- packages/create/templates/hello-nextjs/src/app/page.tsx
+++ packages/create/templates/hello-nextjs/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://docs.dbos.dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -27,7 +27,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://docs.dbos.dev/#example-applications"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -42,7 +42,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.dbos.dev/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -53,7 +53,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to dbos.dev →
         </a>
       </footer>
     </div>
