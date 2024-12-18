a30cc7d3e5306cd31eeee1606daf32351a666066
manojdbos <manoj.khangaonkar@dbos.dev>
2024-12-18 12:16:14 -0800

    review comments

--- packages/create/templates/hello-nextjs/src/actions/dbosWorkflow.tsx
+++ packages/create/templates/hello-nextjs/src/actions/dbosWorkflow.tsx
@@ -13,12 +13,12 @@ class dbosWorkflowClass {
     @DBOS.transaction()
     static async helloDBOS(userName: string) {
         DBOS.logger.info("Hello from DBOS Transaction!");
-            // Retrieve and increment the number of times this user has been greeted.
-            const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
-            const { rows } = await DBOS.sqlClient.client.raw(query, [userName]) as { rows: dbos_hello[] };
-            const greet_count = rows[0].greet_count;
-            const greeting = `Hello, ${userName}! You have been greeted ${greet_count} times.`;
-            return greeting;
+        // Retrieve and increment the number of times this user has been greeted.
+        const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
+        const { rows } = await DBOS.sqlClient.client.raw(query, [userName]) as { rows: dbos_hello[] };
+        const greet_count = rows[0].greet_count;
+        const greeting = `Hello, ${userName}! You have been greeted ${greet_count} times.`;
+        return greeting;
     }
 }
 
@@ -29,6 +29,8 @@ if (process.env.NEXT_PHASE !== "phase-production-build") {
     await DBOS.launch();
 }
 
+// The exported function is the entry point for the workflow
+// The function is exported and not the class because Next does not support exporting classes
 export async function dbosWorkflow(userName: string) {
     DBOS.logger.info("Hello from DBOS!");
     return await dbosWorkflowClass.helloDBOS(userName);
