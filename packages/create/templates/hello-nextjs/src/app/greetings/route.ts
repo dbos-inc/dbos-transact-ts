fd975b315ddeac9262cb1de54b758bd4d6e8b5d1
manojdbos <141425445+manojdbos@users.noreply.github.com>
2024-12-19 06:13:32 -0800

    Template for dbos + nextjs (#682)

---
+++ packages/create/templates/hello-nextjs/src/app/greetings/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { dbosWorkflow } from "../../actions/dbosWorkflow";
+import { DBOS } from "@dbos-inc/dbos-sdk";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { userName } = body;
+  DBOS.logger.info(`Received request with name: ${userName}`);
+  const response = await dbosWorkflow(userName);
+  return NextResponse.json(response);
+}
