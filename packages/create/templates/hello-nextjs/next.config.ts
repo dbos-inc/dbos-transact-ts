ecc67c72b29560529589a12e5931935e56e7a81b
Chuck B <chuck.bear@dbos.dev>
2025-01-09 13:59:25 -0500

    Turn off min for now

--- packages/create/templates/hello-nextjs/next.config.ts
+++ packages/create/templates/hello-nextjs/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
+  webpack: (config, { /*isServer,*/ dev }) => {
     // Treat @dbos-inc/dbos-sdk as an external package for client builds
     config.externals = [
       ...config.externals,
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
         "@dbos-inc/dbos-sdk": "commonjs @dbos-inc/dbos-sdk",
       },
     ];
+
+    if (dev || true) {
+      config.optimization.minimize = false;
+    }
 
     return config;
   },
