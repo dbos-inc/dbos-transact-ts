bf302f52a4a3a085ac58f90e4d559d8f43367d17
Chuck B <chuck.bear@dbos.dev>
2025-01-09 20:46:42 -0500

    Less draconian solution to name mangling

--- packages/create/templates/hello-nextjs/next.config.ts
+++ packages/create/templates/hello-nextjs/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { /*isServer,*/ dev }) => {
+  webpack: (config, { isServer, dev }) => {
     // Treat @dbos-inc/dbos-sdk as an external package for client builds
     config.externals = [
       ...config.externals,
@@ -11,8 +11,19 @@ const nextConfig: NextConfig = {
       },
     ];
 
-    if (dev || true) {
-      config.optimization.minimize = false;
+    if (isServer || dev) {
+      const TerserPlugin = require('terser-webpack-plugin');
+
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true, // Preserve class names
+            keep_fnames: true,    // Preserve function names
+          },
+        }),
+      ];
+      // Or, more draconian:
+      // config.optimization.minimize = false;
     }
 
     return config;
