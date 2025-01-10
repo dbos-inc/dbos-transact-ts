d7aca0607d6ab951ffd8b4af8a6f9d9b7b7cc484
manojdbos <manoj.khangaonkar@dbos.dev>
2025-01-10 15:16:44 -0800

    updates to template

--- packages/create/templates/hello-nextjs/src/app/greetings/route.ts
+++ packages/create/templates/hello-nextjs/src/app/greetings/route.ts
@@ -1,11 +1,13 @@
 import { NextResponse } from "next/server";
 import { dbosWorkflow } from "../../actions/dbosWorkflow";
+import { fooWorkflow } from "../../actions/foo";
 import { DBOS } from "@dbos-inc/dbos-sdk";
 
 export async function POST(request: Request) {
   const body = await request.json();
   const { userName } = body;
   DBOS.logger.info(`Received request with name: ${userName}`);
-  const response = await dbosWorkflow(userName);
-  return NextResponse.json(response);
+  // const response = await dbosWorkflow(userName);
+  await fooWorkflow(userName);
+  return NextResponse.json("Workflow executed successfully");
 }
