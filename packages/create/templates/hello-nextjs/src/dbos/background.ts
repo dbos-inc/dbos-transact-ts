dd50804803177017ffd237df405110770b3fea71
Qian Li <qian.li@dbos.dev>
2025-01-10 17:04:18 -0800

    Fix compiler issue

--- packages/create/templates/hello-nextjs/src/dbos/background.ts
+++ packages/create/templates/hello-nextjs/src/dbos/background.ts
@@ -1,10 +1,13 @@
 
 import { DBOS } from "@dbos-inc/dbos-sdk";
+import { Knex } from "knex";
 
 export interface dbos_hello {
     name: string;
     greet_count: number;
   }
+
+function getClient() {return DBOS.knexClient as Knex;}
 
 // Order matters: DBOS decorators must be declared before being used
 export class dbosWorkflowClass {
@@ -15,7 +18,7 @@ export class dbosWorkflowClass {
         DBOS.logger.info("Hello from DBOS Transaction!");
         // Retrieve and increment the number of times this user has been greeted.
         const query = "INSERT INTO dbos_hello (name, greet_count) VALUES (?, 1) ON CONFLICT (name) DO UPDATE SET greet_count = dbos_hello.greet_count + 1 RETURNING greet_count;";
-        const { rows } = await DBOS.sqlClient.client.raw(query, [userName]) as { rows: dbos_hello[] };
+        const { rows } = await getClient().raw(query, [userName]) as { rows: dbos_hello[] };
         const greet_count = rows[0].greet_count;
         const greeting = `Hello! You have been greeted ${greet_count} times.`;
         return greeting;
