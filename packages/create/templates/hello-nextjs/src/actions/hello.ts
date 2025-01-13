1fffcae7cec233c802e51103f78a4cbe6995d011
manojdbos <manoj.khangaonkar@dbos.dev>
2025-01-13 12:05:16 -0800

    foo is now hello

--- packages/create/templates/hello-nextjs/src/actions/hello.ts
+++ packages/create/templates/hello-nextjs/src/actions/hello.ts
@@ -1,11 +1,10 @@
 "use server";
 
 import { DBOS } from "@dbos-inc/dbos-sdk";
-// import {dbos_hello, dbosWorkflow, dbosBackgroundTask} from "./dbosWorkflow";
 import { helloWorkflowClass } from "../dbos/operations";
 
 
- console.log("Hello from foo.ts");
+console.log("Hello from hello.ts");
 
 // The exported function is the entry point for the workflow
 // The function is exported and not the class because Next does not support exporting classes
