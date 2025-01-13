149d1f95053bf4e5c4925417073f497e47fc72d8
Peter Kraft <petereliaskraft@gmail.com>
2025-01-13 14:23:52 -0800

    docker start

--- src/dbos-runtime/db_wizard.ts
+++ src/dbos-runtime/db_wizard.ts
@@ -2,6 +2,7 @@ import { Pool, PoolConfig } from "pg";
 import { DBOSInitializationError } from "../error";
 import { transports, createLogger, format, Logger } from "winston";
 import Docker from 'dockerode';
+import { sleepms } from "../utils";
 
 export type CLILogger = ReturnType<typeof createLogger>;
 let curLogger: Logger | undefined = undefined;
@@ -61,14 +62,22 @@ export async function db_wizard(poolConfig: PoolConfig): Promise<PoolConfig> {
     logger.warn('Postgres not detected locally');
 
     // 3. If the database config is the default one, check if the user has Docker properly installed.
-
     logger.info("Attempting to start Postgres via Docker")
     const hasDocker = await checkDockerInstalled()
-    console.log(hasDocker)
 
     // 4. If Docker is installed, prompt the user to start a local Docker based Postgres, and then set the PGPASSWORD to 'dbos' and try to connect to the database.
+    let dockerStarted = false;
+    if (hasDocker) {
+        dockerStarted = await startDockerPostgres(logger, poolConfig)
+    } else {
+        logger.warn("Docker not detected locally")
+    }
 
     // 5. If no Docker, then prompt the user to log in to DBOS Cloud and provision a DB there. Wait for the remote DB to be ready, and then create a copy of the original config file, and then load the remote connection string to the local config file.
+
+    if (!dockerStarted) {
+        throw new DBOSInitializationError("Cloud not done yet");
+    }
 
     // 6. Save the config to the config file and return the updated config.
     // TODO: make the config file prettier
@@ -105,12 +114,63 @@ async function checkDbConnectivity(config: PoolConfig): Promise<Error | null> {
 }
 
 
+async function startDockerPostgres(logger: Logger, poolConfig: PoolConfig) {
+    logger.info("Starting a Postgres Docker container...");
+
+    const docker = new Docker();
+    poolConfig.password = "dbos"
+    const containerName = "dbos-db";
+    const pgData = "/var/lib/postgresql/data";
+
+    try {
+        // Create and start the container
+        const container = await docker.createContainer({
+            Image: "pgvector/pgvector:pg16",
+            name: containerName,
+            Env: [
+                `POSTGRES_PASSWORD=${poolConfig.password}`,
+                `PGDATA=${pgData}`
+            ],
+            HostConfig: {
+                PortBindings: {
+                    "5432/tcp": [{ HostPort: poolConfig.port!.toString() }]
+                },
+                Binds: [`${pgData}:${pgData}:rw`],
+                AutoRemove: true
+            }
+        });
+
+        await container.start();
+
+        // Wait for PostgreSQL to be ready
+        let attempts = 30;
+        while (attempts > 0) {
+            if (attempts % 5 === 0) {
+                logger.info("Waiting for Postgres Docker container to start...");
+            }
+            if (await checkDbConnectivity(poolConfig) === null) {
+                return true
+            }
+            attempts--;
+            await sleepms(1000);
+        }
+
+        logger.error("Failed to start Postgres Docker container.")
+        return false;
+
+    } catch (error) {
+        logger.error("Error starting container:", error);
+        return false;
+    }
+}
+
+
 async function checkDockerInstalled() {
     try {
-      const docker = new Docker();
-      await docker.ping();
-      return true;
+        const docker = new Docker();
+        await docker.ping();
+        return true;
     } catch (error) {
-      return false;
+        return false;
     }
-  }
+}
