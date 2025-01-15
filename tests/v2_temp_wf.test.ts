1af7d1736386e7c07d244de1322661dd058f2cd9
Harry Pierson <harry.pierson@dbos.dev>
2025-01-15 11:18:33 -0800

    format test

--- tests/v2_temp_wf.test.ts
+++ tests/v2_temp_wf.test.ts
@@ -2,88 +2,88 @@ import { DBOS, DBOSConfig } from "../src";
 import { generateDBOSTestConfig, setUpDBOSTestDb } from "./helpers";
 
 class TempWorkflowTest {
-    @DBOS.transaction()
-    static async tx_GetWorkflowID() {
-        return Promise.resolve(DBOS.workflowID);
-    }
+  @DBOS.transaction()
+  static async tx_GetWorkflowID() {
+    return Promise.resolve(DBOS.workflowID);
+  }
 
-    @DBOS.step()
-    static async st_GetWorkflowID() {
-        return Promise.resolve(DBOS.workflowID);
-    }
+  @DBOS.step()
+  static async st_GetWorkflowID() {
+    return Promise.resolve(DBOS.workflowID);
+  }
 
-    @DBOS.workflow()
-    static async wf_GetWorkflowID() {
-        return Promise.resolve(DBOS.workflowID);
-    }
+  @DBOS.workflow()
+  static async wf_GetWorkflowID() {
+    return Promise.resolve(DBOS.workflowID);
+  }
 
-    @DBOS.workflow()
-    static async wrap_tx_GetWorkflowID() {
-        return TempWorkflowTest.tx_GetWorkflowID();
-    }
+  @DBOS.workflow()
+  static async wrap_tx_GetWorkflowID() {
+    return TempWorkflowTest.tx_GetWorkflowID();
+  }
 
-    @DBOS.workflow()
-    static async wrap_st_GetWorkflowID() {
-        return TempWorkflowTest.st_GetWorkflowID();
-    }
+  @DBOS.workflow()
+  static async wrap_st_GetWorkflowID() {
+    return TempWorkflowTest.st_GetWorkflowID();
+  }
 }
 
 describe("v2api-temp-wf", () => {
-    let config: DBOSConfig;
-    
-    beforeAll(async () => {
-        config = generateDBOSTestConfig();
-        await setUpDBOSTestDb(config);
-      });
-    
-      beforeEach(async () => {
-        DBOS.setConfig(config);
-        await DBOS.launch();
-      });
-    
-      afterEach(async () => {
-        await DBOS.shutdown();
-      });
+  let config: DBOSConfig;
 
-      test("wf_GetWorkflowID", async () => {
-        const wfUUID = `wf-${Date.now()}`;
-        const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
-          return await TempWorkflowTest.wf_GetWorkflowID();
-        });
-        expect(actual).toBe(wfUUID);
-      });
+  beforeAll(async () => {
+    config = generateDBOSTestConfig();
+    await setUpDBOSTestDb(config);
+  });
 
-      test("tx_GetWorkflowID", async () => {
-        const wfUUID = `tx-${Date.now()}`;
-        const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
-          return await TempWorkflowTest.tx_GetWorkflowID();
-        });
-        expect(actual).toBe(wfUUID);
-      });
+  beforeEach(async () => {
+    DBOS.setConfig(config);
+    await DBOS.launch();
+  });
 
-      test("st_GetWorkflowID", async () => {
-        const wfUUID = `st-${Date.now()}`;
-        const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
-          return await TempWorkflowTest.st_GetWorkflowID();
-        });
-        expect(actual).toBe(wfUUID);
-      });
+  afterEach(async () => {
+    await DBOS.shutdown();
+  });
 
-      test("wrap_tx_GetWorkflowID", async () => {
-        const wfUUID = `wtx-${Date.now()}`;
-        const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
-          return await TempWorkflowTest.wrap_tx_GetWorkflowID();
-        });
-        expect(actual).toBe(wfUUID);
-      });
+  test("wf_GetWorkflowID", async () => {
+    const wfUUID = `wf-${Date.now()}`;
+    const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
+      return await TempWorkflowTest.wf_GetWorkflowID();
+    });
+    expect(actual).toBe(wfUUID);
+  });
 
-      test("wrap_st_GetWorkflowID", async () => {
-        const wfUUID = `wst-${Date.now()}`;
+  test("tx_GetWorkflowID", async () => {
+    const wfUUID = `tx-${Date.now()}`;
+    const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
+      return await TempWorkflowTest.tx_GetWorkflowID();
+    });
+    expect(actual).toBe(wfUUID);
+  });
 
-        const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
-          return await TempWorkflowTest.wrap_st_GetWorkflowID();
-        });
-        expect(actual).toBe(wfUUID);
-      });
+  test("st_GetWorkflowID", async () => {
+    const wfUUID = `st-${Date.now()}`;
+    const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
+      return await TempWorkflowTest.st_GetWorkflowID();
+    });
+    expect(actual).toBe(wfUUID);
+  });
+
+  test("wrap_tx_GetWorkflowID", async () => {
+    const wfUUID = `wtx-${Date.now()}`;
+    const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
+      return await TempWorkflowTest.wrap_tx_GetWorkflowID();
+    });
+    expect(actual).toBe(wfUUID);
+  });
+
+  test("wrap_st_GetWorkflowID", async () => {
+    const wfUUID = `wst-${Date.now()}`;
+
+    const actual = await DBOS.withNextWorkflowID(wfUUID, async () => {
+      return await TempWorkflowTest.wrap_st_GetWorkflowID();
+    });
+    expect(actual).toBe(wfUUID);
+  });
 
 });
