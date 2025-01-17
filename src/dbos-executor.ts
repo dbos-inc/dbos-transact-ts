626cee15a98a2f4394b53cce56e037a28258aed0
chuck-dbos <134347445+chuck-dbos@users.noreply.github.com>
2025-01-17 17:36:04 -0500

    Use status for queue fix and optimization (#721)
    When starting a workflow, look at any existing record in the workflow
    status table.
    If the workflow is already complete (SUCCESS or ERROR) do nothing (let
    the caller retrieve the result from the handle)
    If the workflow is now ENQUEUED, ensure there is a workflow_queue record
    for it, but do not add such records if the workflow has moved past that
    state.

--- src/dbos-executor.ts
+++ src/dbos-executor.ts
@@ -673,6 +673,8 @@ export class DBOSExecutor implements DBOSExecutorContext {
       internalStatus.className = params.tempWfClass ?? "";
     }
 
+    let status: string | undefined = undefined;
+
     // Synchronously set the workflow's status to PENDING and record workflow inputs (for non single-transaction workflows).
     // We have to do it for all types of workflows because operation_outputs table has a foreign key constraint on workflow status table.
     if ((wCtxt.tempWfOperationType !== TempWorkflowType.transaction
@@ -680,7 +682,9 @@ export class DBOSExecutor implements DBOSExecutorContext {
       || params.queueName !== undefined
     ) {
       // TODO: Make this transactional (and with the queue step below)
-      args = await this.systemDatabase.initWorkflowStatus(internalStatus, args);
+      const ires = await this.systemDatabase.initWorkflowStatus(internalStatus, args);
+      args = ires.args;
+      status = ires.status;
       await debugTriggerPoint(DEBUG_TRIGGER_WORKFLOW_ENQUEUE);
     }
 
@@ -752,7 +756,7 @@ export class DBOSExecutor implements DBOSExecutorContext {
       return result;
     };
 
-    if (params.queueName === undefined || params.executeWorkflow) {
+    if (status !=='SUCCESS' && status !=='ERROR' && (params.queueName === undefined || params.executeWorkflow)) {
       const workflowPromise: Promise<R> = runWorkflow();
 
       // Need to await for the workflow and capture errors.
@@ -770,7 +774,9 @@ export class DBOSExecutor implements DBOSExecutorContext {
       return new InvokedHandle(this.systemDatabase, workflowPromise, workflowUUID, wf.name, callerUUID, callerFunctionID);
     }
     else {
-      await this.systemDatabase.enqueueWorkflow(workflowUUID, this.#getQueueByName(params.queueName));
+      if (params.queueName && status === 'ENQUEUED') {
+        await this.systemDatabase.enqueueWorkflow(workflowUUID, this.#getQueueByName(params.queueName));
+      }
       return new RetrievedHandle(this.systemDatabase, workflowUUID, callerUUID, callerFunctionID);
     }
   }
@@ -1245,11 +1251,30 @@ export class DBOSExecutor implements DBOSExecutorContext {
   async deactivateEventReceivers() {
     this.logger.info("Deactivating event receivers");
     for (const evtRcvr of this.eventReceivers || []) {
-      await evtRcvr.destroy();
+      try {
+        await evtRcvr.destroy();
+      }
+      catch (err) {
+        const e = err as Error;
+        this.logger.warn(`Error destroying event receiver: ${e.message}`);
+      }
     }
-    await this.scheduler?.destroyScheduler();
-    wfQueueRunner.stop();
-    await this.wfqEnded;
+    try {
+      await this.scheduler?.destroyScheduler();
+    }
+    catch (err) {
+      const e = err as Error;
+      this.logger.warn(`Error destroying scheduler: ${e.message}`);
+    }
+
+    try {
+      wfQueueRunner.stop();
+      await this.wfqEnded;
+    }
+    catch (err) {
+      const e = err as Error;
+      this.logger.warn(`Error destroying wf queue runner: ${e.message}`);
+    }
   }
 
   async executeWorkflowUUID(workflowUUID: string, startNewWorkflow: boolean = false): Promise<WorkflowHandle<unknown>> {
