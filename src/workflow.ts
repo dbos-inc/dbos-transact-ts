1858d90beacca8135b0fa6bd2bda61acad1d05e1
maxdml <maxdml@dbos.dev>
2025-01-29 22:27:43 +0000

    add back missing new property

--- src/workflow.ts
+++ src/workflow.ts
@@ -89,6 +89,7 @@ export interface GetWorkflowQueueInput {
 export interface GetWorkflowQueueOutput {
   workflows: {
     workflowID: string; // Workflow ID
+    executorID?: string; // Workflow executor ID
     queueName: string; // Workflow queue name
     createdAt: number; // Time that queue entry was created
     startedAt?: number; // Time that workflow was started, if started
