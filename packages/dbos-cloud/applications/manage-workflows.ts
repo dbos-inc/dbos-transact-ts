0cc4b59cf3063134da3ae98ef57c4ecfada43506
Peter Kraft <peter.kraft@dbos.dev>
2025-03-04 10:58:35 -0600

    Better Workflows List (#776)
    Allow specifying the sort order when listing workflows or queued
    workflows.
    
    ---------
    
    Signed-off-by: Peter Kraft <petereliaskraft@gmail.com>
    Co-authored-by: Qian Li <qian.li@dbos.dev>

--- packages/dbos-cloud/applications/manage-workflows.ts
+++ packages/dbos-cloud/applications/manage-workflows.ts
@@ -17,6 +17,7 @@ export interface ListWorkflowsInput {
   application_version?: string; // The application version that ran this workflow.
   limit?: number; // Return up to this many workflows IDs. IDs are ordered by workflow creation time.
   offset?: number; // Skip this many workflows IDs. IDs are ordered by workflow creation time.
+  sort_desc?: boolean; // Sort in DESC order by created_at (default ASC)
 }
 
 export interface ListQueuedWorkflowsInput {
@@ -27,6 +28,7 @@ export interface ListQueuedWorkflowsInput {
   status?: string; // The status of the workflow.
   limit?: number; // Return up to this many workflows IDs. IDs are ordered by workflow creation time.
   offset?: number; // Skip this many workflows IDs. IDs are ordered by workflow creation time.
+  sort_desc?: boolean; // Sort in DESC order by created_at (default ASC)
 }
 
 export async function listWorkflows(host: string, input: ListWorkflowsInput, appName?: string): Promise<number> {
