7fd4d56a9ad07b786a8f5ddfa8d84e2a2d45a70b
Chuck B <chuck.bear@dbos.dev>
2025-03-24 10:54:44 -0400

    Remove testing runtime

--- tests/testing/debugger_configuredinst.test.ts
+++ tests/testing/debugger_configuredinst.test.ts
@@ -1,20 +1,7 @@
-import {
-  Step,
-  StepContext,
-  ConfiguredInstance,
-  InitContext,
-  Transaction,
-  TransactionContext,
-  Workflow,
-  WorkflowContext,
-} from '../../src/';
+import { ConfiguredInstance, DBOS, InitContext } from '../../src/';
 import { generateDBOSTestConfig, setUpDBOSTestDb } from '../helpers';
 import { v1 as uuidv1 } from 'uuid';
 import { DBOSConfig, DebugMode } from '../../src/dbos-executor';
-import { PoolClient } from 'pg';
-import { TestingRuntime, TestingRuntimeImpl, createInternalTestRuntime } from '../../src/testing/testing_runtime';
-
-type TestTransactionContext = TransactionContext<PoolClient>;
 
 class DebuggerCCTest extends ConfiguredInstance {
   constructor(name: string) {
@@ -25,33 +12,33 @@ class DebuggerCCTest extends ConfiguredInstance {
     return Promise.resolve();
   }
 
-  @Transaction({ readOnly: true })
-  async testReadOnlyFunction(_txnCtxt: TestTransactionContext, number: number) {
+  @DBOS.transaction({ readOnly: true })
+  async testReadOnlyFunction(number: number) {
     expect(this.name).toBe('configA');
     return Promise.resolve(number);
   }
 
-  @Workflow()
-  async testWorkflow(ctxt: WorkflowContext, name: string) {
+  @DBOS.workflow()
+  async testWorkflow(name: string) {
     expect(this.name).toBe('configA');
-    const funcResult = await ctxt.invoke(this).testReadOnlyFunction(5);
+    const funcResult = await this.testReadOnlyFunction(5);
     return `${name}${funcResult}`;
   }
 
-  @Step()
-  async testStep(_ctxt: StepContext, inp: string) {
+  @DBOS.step()
+  async testStep(inp: string) {
     expect(this.name).toBe('configA');
     return Promise.resolve(inp);
   }
 
   // Workflow that sleep, call comm, call tx, call child WF
-  @Workflow()
-  async mixedWorkflow(ctxt: WorkflowContext, num: number) {
+  @DBOS.workflow()
+  async mixedWorkflow(num: number) {
     expect(this.name).toBe('configA');
-    await ctxt.sleep(1);
-    const txResult = await ctxt.invoke(this).testReadOnlyFunction(num);
-    const cResult = await ctxt.invoke(this).testStep('comm');
-    const wfResult = await ctxt.invokeWorkflow(this).testWorkflow('cwf');
+    await DBOS.sleepSeconds(1);
+    const txResult = await this.testReadOnlyFunction(num);
+    const cResult = await this.testStep('comm');
+    const wfResult = await this.testWorkflow('cwf');
     return `${this.name}${txResult}${cResult}${wfResult}-${num}`;
   }
 }
@@ -62,9 +49,6 @@ describe('debugger-test', () => {
   let config: DBOSConfig;
   let debugConfig: DBOSConfig;
   let debugProxyConfig: DBOSConfig;
-  let testRuntime: TestingRuntime;
-  let debugRuntime: TestingRuntime;
-  let debugProxyRuntime: TestingRuntime;
 
   beforeAll(async () => {
     config = generateDBOSTestConfig();
@@ -73,44 +57,33 @@ describe('debugger-test', () => {
     await setUpDBOSTestDb(config);
   });
 
-  beforeEach(async () => {
-    testRuntime = await createInternalTestRuntime(undefined, config);
-    debugRuntime = await createInternalTestRuntime(undefined, debugConfig, { debugMode: DebugMode.ENABLED });
-    debugProxyRuntime = await createInternalTestRuntime(undefined, debugProxyConfig, {
-      debugMode: DebugMode.TIME_TRAVEL,
-    }); // TODO: connect to the real proxy.
-  });
-
-  afterEach(async () => {
-    await debugRuntime.destroy();
-    await testRuntime.destroy();
-    await debugProxyRuntime.destroy();
-  });
-
   test('debug-workflow', async () => {
     const wfUUID = uuidv1();
     // Execute the workflow and destroy the runtime
-    const res = await testRuntime.invokeWorkflow(configR, wfUUID).mixedWorkflow(23);
-    expect(res).toBe('configA23commcwf5-23');
-    await testRuntime.destroy();
+    DBOS.setConfig(config);
+    await DBOS.launch();
+    await DBOS.withNextWorkflowID(wfUUID, async () => {
+      const res = await configR.mixedWorkflow(23);
+      expect(res).toBe('configA23commcwf5-23');
+    });
+    await DBOS.shutdown();
 
     // Execute again in debug mode.
-    const debugRes = await debugRuntime.invokeWorkflow(configR, wfUUID).mixedWorkflow(23);
-    expect(debugRes).toBe('configA23commcwf5-23');
+    DBOS.setConfig(debugConfig);
+    await DBOS.launch({ debugMode: DebugMode.ENABLED });
+    await DBOS.withNextWorkflowID(wfUUID, async () => {
+      const res = await configR.mixedWorkflow(23);
+      expect(res).toBe('configA23commcwf5-23');
+    });
 
     // Execute again with the provided UUID.
-    await expect(
-      (debugRuntime as TestingRuntimeImpl)
-        .getDBOSExec()
-        .executeWorkflowUUID(wfUUID)
-        .then((x) => x.getResult()),
-    ).resolves.toBe('configA23commcwf5-23');
+    await expect(DBOS.executeWorkflowById(wfUUID).then((x) => x.getResult())).resolves.toBe('configA23commcwf5-23');
+    await DBOS.shutdown();
 
-    await expect(
-      (debugProxyRuntime as TestingRuntimeImpl)
-        .getDBOSExec()
-        .executeWorkflowUUID(wfUUID)
-        .then((x) => x.getResult()),
-    ).resolves.toBe('configA23commcwf5-23');
+    // TT Mode
+    DBOS.setConfig(debugProxyConfig);
+    await DBOS.launch({ debugMode: DebugMode.TIME_TRAVEL });
+    await expect(DBOS.executeWorkflowById(wfUUID).then((x) => x.getResult())).resolves.toBe('configA23commcwf5-23');
+    await DBOS.shutdown();
   });
 });
