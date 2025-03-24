c253fce3c85d53f486f6f9241d9a018347588e7c
Harry Pierson <harry.pierson@dbos.dev>
2025-03-24 13:15:14 -0700

    add more enqueue tests

--- tests/client.test.ts
+++ tests/client.test.ts
@@ -1,6 +1,6 @@
 import { workflow_status } from '../schemas/system_db_schema';
 import { DBOS, DBOSConfig, DBOSClient, WorkflowQueue } from '../src';
-import { globalParams } from '../src/utils';
+import { globalParams, sleepms } from '../src/utils';
 import { generateDBOSTestConfig, setUpDBOSTestDb } from './helpers';
 import { Client } from 'pg';
 
@@ -34,9 +34,9 @@ describe('DBOSClient', () => {
     await DBOS.shutdown();
   });
 
-  test('invokeTest', async () => {
+  test('DBOSClient.enqueue (app ver not set)', async () => {
     const client = new DBOSClient(config.poolConfig, config.system_database);
-    const wfid = `invokeTest-${Date.now()}`;
+    const wfid = `client-enqueue-${Date.now()}`;
 
     try {
       type InvokeTest = typeof ClientTest.invokeTest;
@@ -70,7 +70,92 @@ describe('DBOSClient', () => {
       );
       expect(result.rows).toHaveLength(1);
       expect(result.rows[0].workflow_uuid).toBe(wfid);
+      expect(result.rows[0].status).toBe('SUCCESS');
       expect(result.rows[0].application_version).toBe(globalParams.appVersion);
+    } finally {
+      await dbClient.end();
+    }
+  }, 10000);
+
+  test('DBOSClient.enqueue (app ver set)', async () => {
+    const client = new DBOSClient(config.poolConfig, config.system_database);
+    const wfid = `client-enqueue-${Date.now()}`;
+
+    try {
+      type InvokeTest = typeof ClientTest.invokeTest;
+
+      await client.init();
+      await client.enqueue<Parameters<InvokeTest>>(
+        {
+          workflowName: 'invokeTest',
+          workflowClassName: 'ClientTest',
+          queueName: 'testQueue',
+          workflowUUID: wfid,
+          appVersion: globalParams.appVersion,
+        },
+        42,
+        'test',
+        { first: 'John', last: 'Doe', age: 30 },
+      );
+
+      const handle = DBOS.retrieveWorkflow<Awaited<ReturnType<InvokeTest>>>(wfid);
+      const result = await handle.getResult();
+      expect(result).toBe('42-test-{"first":"John","last":"Doe","age":30}');
+    } finally {
+      await client.destroy();
+    }
+
+    const dbClient = new Client({ ...config.poolConfig, database: config.system_database });
+    try {
+      await dbClient.connect();
+      const result = await dbClient.query<workflow_status>(
+        'SELECT * FROM dbos.workflow_status WHERE workflow_uuid = $1',
+        [wfid],
+      );
+      expect(result.rows).toHaveLength(1);
+      expect(result.rows[0].workflow_uuid).toBe(wfid);
+      expect(result.rows[0].status).toBe('SUCCESS');
+      expect(result.rows[0].application_version).toBe(globalParams.appVersion);
+    } finally {
+      await dbClient.end();
+    }
+  }, 10000);
+
+  test('DBOSClient.enqueue (incorrect app ver set)', async () => {
+    const client = new DBOSClient(config.poolConfig, config.system_database);
+    const wfid = `client-enqueue-${Date.now()}`;
+
+    try {
+      type InvokeTest = typeof ClientTest.invokeTest;
+
+      await client.init();
+      await client.enqueue<Parameters<InvokeTest>>(
+        {
+          workflowName: 'invokeTest',
+          workflowClassName: 'ClientTest',
+          queueName: 'testQueue',
+          appVersion: '1234567890ABCDEF',
+        },
+        422,
+        'test2',
+        { first: 'John2', last: 'Doe2', age: 32 },
+      );
+    } finally {
+      await client.destroy();
+    }
+
+    await sleepms(10000);
+
+    const dbClient = new Client({ ...config.poolConfig, database: config.system_database });
+    try {
+      await dbClient.connect();
+      const result = await dbClient.query<workflow_status>(
+        'SELECT * FROM dbos.workflow_status WHERE application_version = $1',
+        ['1234567890ABCDEF'],
+      );
+      expect(result.rows).toHaveLength(1);
+      expect(result.rows[0].status).toBe('ENQUEUED');
+      expect(result.rows[0].application_version).toBe('1234567890ABCDEF');
     } finally {
       await dbClient.end();
     }
