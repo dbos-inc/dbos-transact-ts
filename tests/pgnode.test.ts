693dfba7d89537b86cdabbd60fdf9e5501cea5e1
maxdml <maxdml@dbos.dev>
2025-03-27 13:13:15 -0700

    test sys db pool size

--- tests/pgnode.test.ts
+++ tests/pgnode.test.ts
@@ -1,4 +1,6 @@
 import { DBOS } from '../src';
+import { DBOSExecutor } from '../src/dbos-executor';
+import { PostgresSystemDatabase } from '../src/system_database';
 import { UserDatabaseName } from '../src/user_database';
 import { generatePublicDBOSTestConfig, setUpDBOSTestDb } from './helpers';
 
@@ -13,16 +15,20 @@ class TestEngine {
   }
 }
 
-describe.only('pgnode-engine-config-tests', () => {
+describe('pgnode-engine-config-tests', () => {
   test('engine-config', async () => {
     const config = generatePublicDBOSTestConfig({
       userDbclient: UserDatabaseName.PGNODE,
       userDbPoolSize: 2,
+      sysDbPoolSize: 42,
     });
     await setUpDBOSTestDb(config);
     DBOS.setConfig(config);
     await DBOS.launch();
     try {
+      const sysDbClient = ((DBOS.executor as DBOSExecutor).systemDatabase as PostgresSystemDatabase).knexDB;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      expect((sysDbClient as any).context.client.config.pool.max).toEqual(42);
       await TestEngine.testEngine();
     } finally {
       await DBOS.shutdown();
