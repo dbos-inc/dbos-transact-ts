013b75211976092649ae87e9b958b0356a028edb
Max dml <maxdml@dbos.dev>
2025-04-03 11:20:12 -0700

    Consolidate sysdb reset code (#831)
    Have reset db CLI command use the internal
    PostgresSystemDatabase.dropSystemDB function
    
    Note the internal function *does not* terminate existing connection --
    which is a good thing.

--- src/dbos-runtime/reset.ts
+++ src/dbos-runtime/reset.ts
@@ -1,7 +1,7 @@
 import { GlobalLogger } from '../telemetry/logs';
-import { Client } from 'pg';
 import { confirm } from '@inquirer/prompts';
 import { DBOSConfigInternal } from '../dbos-executor';
+import { PostgresSystemDatabase } from '../system_database';
 
 export async function reset(config: DBOSConfigInternal, logger: GlobalLogger, cnf: boolean) {
   if (cnf) {
@@ -16,34 +16,8 @@ export async function reset(config: DBOSConfigInternal, logger: GlobalLogger, cn
       process.exit(0); // Exit the process if the user cancels
     }
   }
-
   const sysDbName = config.system_database;
-
   logger.info(`Resetting ${sysDbName} if it exists`);
-
-  const pgClient = new Client({
-    user: config.poolConfig.user,
-    host: config.poolConfig.host,
-    database: 'postgres', // Connect to the default PostgreSQL database
-    password: config.poolConfig.password,
-    port: config.poolConfig.port,
-  });
-
-  await pgClient.connect();
-
-  console.log('Terminating connections to system database');
-
-  await pgClient.query(
-    `SELECT pg_terminate_backend(pg_stat_activity.pid)
-                FROM pg_stat_activity
-                WHERE pg_stat_activity.datname = $1
-                AND pid <> pg_backend_pid()`,
-    [sysDbName],
-  );
-
-  await pgClient.query(`DROP DATABASE IF EXISTS ${sysDbName};`);
-
-  await pgClient.end();
-
+  await PostgresSystemDatabase.dropSystemDB(config);
   return 0;
 }
