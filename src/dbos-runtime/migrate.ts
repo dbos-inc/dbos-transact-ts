0d76ca0a9330a1a409a90711088e3f01fdfd3602
manojdbos <manoj.khangaonkar@dbos.dev>
2025-04-09 13:12:54 -0700

    fix migrate

--- src/dbos-runtime/migrate.ts
+++ src/dbos-runtime/migrate.ts
@@ -91,19 +91,18 @@ async function createDBOSTables(systemDbName: string, userPoolConfig: PoolConfig
   const schemaExists = await pgUserClient.query<ExistenceCheck>(schemaExistsQuery);
   if (!schemaExists.rows[0].exists) {
     await pgUserClient.query(createUserDBSchema);
-  } else {
-    const columnExists = await pgUserClient.query<ExistenceCheck>(columnExistsQuery);
-
-    if (!columnExists.rows[0].exists) {
-      // Column does not exist - alter table to add column
-
-      await pgUserClient.query(addColumnQuery);
-    }
   }
   const txnOutputTableExists = await pgUserClient.query<ExistenceCheck>(txnOutputTableExistsQuery);
   if (!txnOutputTableExists.rows[0].exists) {
     await pgUserClient.query(userDBSchema);
+  } else {
+    const columnExists = await pgUserClient.query<ExistenceCheck>(columnExistsQuery);
+
+    if (!columnExists.rows[0].exists) {
+      await pgUserClient.query(addColumnQuery);
+    }
   }
+
   const txnIndexExists = await pgUserClient.query<ExistenceCheck>(txnOutputIndexExistsQuery);
   if (!txnIndexExists.rows[0].exists) {
     await pgUserClient.query(userDBIndex);
