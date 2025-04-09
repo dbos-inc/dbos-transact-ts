8b722fd4126ac99ae5cc24994b258170e6ffcc18
maxdml <maxdml@dbos.dev>
2025-04-09 08:44:16 -0700

    Revert "explicit db wizard"
    This reverts commit 4263518c3b1937b8dcec42451dba90fdeaaaa91b.

--- src/dbos-runtime/runtime.ts
+++ src/dbos-runtime/runtime.ts
@@ -9,6 +9,7 @@ import { pathToFileURL } from 'url';
 import { DBOSScheduler } from '../scheduler/scheduler';
 import { wfQueueRunner } from '../wfqueue';
 import { DBOS } from '../dbos';
+import { db_wizard } from './db_wizard';
 
 interface ModuleExports {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -49,6 +50,7 @@ export class DBOSRuntime {
       if (!this.dbosConfig.poolConfig) {
         throw new Error('DBOS pool configuration is not initialized');
       }
+      this.dbosConfig.poolConfig = await db_wizard(this.dbosConfig.poolConfig);
       this.dbosExec = new DBOSExecutor(this.dbosConfig);
       DBOS.globalLogger = this.dbosExec.logger;
       this.dbosExec.logger.debug(`Loading classes from entrypoints ${JSON.stringify(this.runtimeConfig.entrypoints)}`);
