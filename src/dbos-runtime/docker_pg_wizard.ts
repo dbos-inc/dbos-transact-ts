ff1fb935acef886f1e163af9d3c8a97b10b3c122
maxdml <maxdml@dbos.dev>
2025-04-11 10:13:59 -0700

    docker helper

--- src/dbos-runtime/docker_pg_wizard.ts
+++ src/dbos-runtime/docker_pg_wizard.ts
@@ -6,52 +6,28 @@ import { getLogger } from './cloudutils/cloudutils';
 import { promisify } from 'util';
 import { exec } from 'child_process';
 
-export async function db_wizard(poolConfig: PoolConfig) {
+/**
+ * Helper to create a dockerized Postgres database if the user has not already set up a local Postgres database.
+ *
+ * @returns null
+ */
+
+export async function dockerPgWizard() {
   const logger = getLogger();
+  logger.info('Attempting to create a Docker Postgres container...');
 
-  // 1. Check the connectivity to the database. Return if successful. If cannot connect, continue to the following steps.
-  const dbConnectionError = await checkDbConnectivity(poolConfig);
-  if (dbConnectionError === null) {
-    logger.info(
-      `Postgres is already available locally at postgres://${poolConfig.user}:***@${poolConfig.host}:${poolConfig.port}`,
-    );
-    return poolConfig;
-  }
-
-  // 2. If the error is due to password authentication or the configuration is non-default, surface the error and exit.
-  let errorStr = dbConnectionError.toString();
-  if (dbConnectionError instanceof AggregateError) {
-    let combinedMessage = 'AggregateError: ';
-    for (const error of dbConnectionError.errors) {
-      combinedMessage += `${(error as Error).toString()}; `;
-    }
-    errorStr = combinedMessage;
-  }
-
-  if (
-    errorStr.includes('password authentication failed') ||
-    errorStr.includes('28P01') ||
-    errorStr.includes('no password supplied') ||
-    errorStr.includes('client password must be a string')
-  ) {
-    throw new DBOSInitializationError(`Could not connect to Postgres: password authentication failed: ${errorStr}`);
-  }
-
-  // If the provided poolConfig database hostname/port/username/password are not the defaults, skip the wizard.
-  if (
-    poolConfig.host !== 'localhost' ||
-    poolConfig.port !== 5432 ||
-    poolConfig.user !== 'postgres' ||
-    poolConfig.password !== 'dbos'
-  ) {
-    throw new DBOSInitializationError(`Could not connect to the database. Exception: ${errorStr}`);
-  }
-
-  // 3. If the database config is the default one, check if the user has Docker properly installed.
-  logger.info('Attempting to start Postgres via Docker');
   const hasDocker = await checkDockerInstalled();
 
-  // 4. If Docker is installed, prompt the user to start a local Docker based Postgres, and then set the PGPASSWORD to 'dbos' and try to connect to the database.
+  const poolConfig = {
+    host: 'localhost',
+    port: 5432,
+    password: process.env.PGPASSWORD || 'dbos',
+    user: 'postgres',
+    database: 'postgres',
+    connectionTimeoutMillis: 2000,
+  };
+
+  // If Docker is installed, prompt the user to start a local Docker based Postgres, and then set the PGPASSWORD to 'dbos' and try to connect to the database.
   if (hasDocker) {
     await startDockerPostgres(logger, poolConfig);
     logger.info(
@@ -63,14 +39,7 @@ export async function db_wizard(poolConfig: PoolConfig) {
 }
 
 async function checkDbConnectivity(config: PoolConfig): Promise<Error | null> {
-  // Add a default connection timeout if not specified
-  const configWithTimeout: PoolConfig = {
-    ...config,
-    connectionTimeoutMillis: config.connectionTimeoutMillis ?? 2000, // 2 second timeout
-    database: 'postgres',
-  };
-
-  const pool = new Pool(configWithTimeout);
+  const pool = new Pool(config);
 
   try {
     const client = await pool.connect();
@@ -94,7 +63,6 @@ const execAsync = promisify(exec);
 
 async function startDockerPostgres(logger: Logger, poolConfig: PoolConfig): Promise<boolean> {
   logger.info('Starting a Postgres Docker container...');
-  poolConfig.password = 'dbos';
   const containerName = 'dbos-db';
   const pgData = '/var/lib/postgresql/data';
 
@@ -102,9 +70,9 @@ async function startDockerPostgres(logger: Logger, poolConfig: PoolConfig): Prom
     // Create and start the container
     const dockerCmd = `docker run -d \
             --name ${containerName} \
-            -e POSTGRES_PASSWORD=${poolConfig.password} \
+            -e POSTGRES_PASSWORD=${poolConfig.password as string} \
             -e PGDATA=${pgData} \
-            -p ${poolConfig.port}:5432 \
+            -p 5432:5432 \
             -v ${pgData}:${pgData}:rw \
             --rm \
             pgvector/pgvector:pg16`;
@@ -126,11 +94,13 @@ async function startDockerPostgres(logger: Logger, poolConfig: PoolConfig): Prom
       await sleepms(1000);
     }
 
-    logger.error('Failed to start Postgres Docker container.');
-    return false;
+    throw new DBOSInitializationError(
+      `Failed to start Docker container: Container ${containerName} did not start in time.`,
+    );
   } catch (error) {
-    logger.error('Error starting container:', error);
-    return false;
+    throw new DBOSInitializationError(
+      `Failed to start Docker container: ${error instanceof Error ? error.message : 'Unknown error'}`,
+    );
   }
 }
 
