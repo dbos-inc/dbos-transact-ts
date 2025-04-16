c933fd167d50b83745d085195ed7ec70fbfb799e
Chuck B <chuck.bear@dbos.dev>
2025-04-16 11:23:15 -0400

    Test for preemptive cancel of sleep

--- tests/wfcancel.test.ts
+++ tests/wfcancel.test.ts
@@ -1,5 +1,6 @@
 import { StatusString, DBOS } from '../src';
 import { DBOSConfig } from '../src/dbos-executor';
+import { DBOSTargetWorkflowCancelledError } from '../src/error';
 import { generateDBOSTestConfig, setUpDBOSTestDb } from './helpers';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -111,6 +112,16 @@ describe('wf-cancel-tests', () => {
     expect(WFwith2Steps.stepsExecuted).toBe(2);
   });
 
+  test('test-preempt-sleepms', async () => {
+    const wfid = uuidv4();
+    const wfh = await DBOS.startWorkflow(DeepSleep, { workflowID: wfid }).sleepTooLong();
+
+    await expect(DBOS.getResult(wfh.workflowID, 0.2)).resolves.toBeNull();
+    await DBOS.cancelWorkflow(wfid);
+
+    await expect(DBOS.getResult(wfh.workflowID)).rejects.toThrow(DBOSTargetWorkflowCancelledError);
+  });
+
   class WFwith2Steps {
     static stepsExecuted = 0 as number;
 
@@ -159,6 +170,14 @@ describe('wf-cancel-tests', () => {
       await DBOS.sleepSeconds(1);
       await WFwith2Transactions.transaction2();
       return Promise.resolve();
+    }
+  }
+
+  class DeepSleep {
+    @DBOS.workflow()
+    static async sleepTooLong() {
+      await DBOS.sleepms(1000 * 1000);
+      return 'Done';
     }
   }
 });
