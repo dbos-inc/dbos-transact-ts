096709fa7eed5a8f6edf90d0bc3c421cdcf849f2
Chuck B <chuck.bear@dbos.dev>
2025-04-25 09:38:58 -0400

    Lint

--- src/workflow.ts
+++ src/workflow.ts
@@ -3,7 +3,7 @@ import { DBOSExecutor, OperationType } from './dbos-executor';
 import { Transaction, TransactionContext } from './transaction';
 import { StepFunction, StepContext } from './step';
 import { SystemDatabase } from './system_database';
-import { DBOSContext, DBOSContextImpl, HTTPRequest, assertCurrentWorkflowContext } from './context';
+import { DBOSContext, DBOSContextImpl, HTTPRequest } from './context';
 import { ConfiguredInstance, getRegisteredOperations } from './decorators';
 import { StoredProcedure, StoredProcedureContext } from './procedure';
 import { InvokeFuncsInst } from './httpServer/handler';
