466f4a80c5cbf455a3fdf2c06cc65a48375c3807
Chuck B <chuck.bear@dbos.dev>
2025-04-28 21:43:52 -0400

    Empty test files fail

--- tests/decoratorlessapi.test.ts
+++ tests/decoratorlessapi.test.ts
@@ -4,3 +4,29 @@
 // registerTransaction ()
 // registerStep ()
 // registerWorkflow ()
+
+import { DBOS, DBOSConfig } from '../src';
+import { generateDBOSTestConfig, setUpDBOSTestDb } from './helpers';
+
+describe('decoratorless-api-tests', () => {
+  let config: DBOSConfig;
+
+  beforeAll(async () => {
+    config = generateDBOSTestConfig();
+    await setUpDBOSTestDb(config);
+    DBOS.setConfig(config);
+  });
+
+  beforeEach(async () => {
+    await DBOS.launch();
+  });
+
+  afterEach(async () => {
+    await DBOS.shutdown();
+  });
+
+  test('simple-functions', async () => {
+    expect(1).toBe(1);
+    return Promise.resolve();
+  });
+});
