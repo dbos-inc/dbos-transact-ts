4f844ffe22def6920a45fb946615de0fd04b589f
Peter Kraft <petereliaskraft@gmail.com>
2025-05-08 10:47:58 -0700

    fix

--- packages/dbos-cloud/applications/deploy-app-code.ts
+++ packages/dbos-cloud/applications/deploy-app-code.ts
@@ -197,7 +197,7 @@ export async function deployAppCode(
     } else {
       logger.info('Time travel is disabled for this application');
     }
-    const ret = await registerApp(userDBName, host, enableTimeTravel, appName);
+    const ret = await registerApp(userDBName, host, enableTimeTravel, appName, deployConfigFile);
     if (ret !== 0) {
       return 1;
     }
