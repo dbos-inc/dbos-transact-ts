78e6279d83f3b4e7e9ef0f9787b4931096c8f11c
Qian Li <qian.li@dbos.dev>
2025-05-12 15:48:20 -0700

    comment

--- migrations/20252512000000_queue_priority.js
+++ migrations/20252512000000_queue_priority.js
@@ -1,3 +1,6 @@
+// Due to a naming issue, this migration is named "20252512000000_queue_priority" instead of "20250512000000_queue_priority".
+// Please see the 20252101000000_workflow_queues_executor_id migration for more details.
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
