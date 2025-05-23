65a58d719cfd5db2001b462d96dcdc3bf2f9294d
Harry Pierson <harry.pierson@dbos.dev>
2025-05-22 17:31:03 -0700

    static and instance method tests

--- packages/knex-datasource/datasource.test.ts
+++ packages/knex-datasource/datasource.test.ts
@@ -113,6 +113,30 @@ describe('KnexDataSource', () => {
     const { rows } = await userDB.query('SELECT * FROM dbos.transaction_outputs WHERE workflow_id = $1', [workflowID]);
     expect(rows.length).toBe(0);
   });
+
+  test('static test', async () => {
+    const user = 'staticTest1';
+
+    await userDB.query('DELETE FROM greetings WHERE name = $1', [user]);
+
+    const workflowID = randomUUID();
+    await expect(DBOS.withNextWorkflowID(workflowID, () => regStaticWorkflow(user))).resolves.toEqual([
+      { user, greet_count: 1 },
+      { user, greet_count: 1 },
+    ]);
+  });
+
+  test('instance test', async () => {
+    const user = 'instanceTest1';
+
+    await userDB.query('DELETE FROM greetings WHERE name = $1', [user]);
+
+    const workflowID = randomUUID();
+    await expect(DBOS.withNextWorkflowID(workflowID, () => regInstanceWorkflow(user))).resolves.toEqual([
+      { user, greet_count: 1 },
+      { user, greet_count: 1 },
+    ]);
+  });
 });
 
 export interface greetings {
@@ -163,3 +187,62 @@ async function readWorkflow2(user: string) {
 }
 
 const regReadWorkflow2 = DBOS.registerWorkflow(readWorkflow2, { name: 'readWorkflow2' });
+
+class StaticClass {
+  static async helloFunction(user: string) {
+    const rows = await KnexDataSource.client<greetings>('greetings')
+      .insert({ name: user, greet_count: 1 })
+      .onConflict('name')
+      .merge({ greet_count: KnexDataSource.client.raw('greetings.greet_count + 1') })
+      .returning('greet_count');
+    const row = rows.length > 0 ? rows[0] : undefined;
+
+    return { user, greet_count: row?.greet_count };
+  }
+
+  static async readFunction(user: string) {
+    const row = await KnexDataSource.client<greetings>('greetings').select('greet_count').where('name', user).first();
+    return { user, greet_count: row?.greet_count };
+  }
+}
+
+StaticClass.helloFunction = dataSource.register(StaticClass.helloFunction, 'helloFunction');
+StaticClass.readFunction = dataSource.register(StaticClass.readFunction, 'readFunction');
+
+async function staticWorkflow(user: string) {
+  const result = await StaticClass.helloFunction(user);
+  const readResult = await StaticClass.readFunction(user);
+  return [result, readResult];
+}
+
+const regStaticWorkflow = DBOS.registerWorkflow(staticWorkflow, { name: 'staticWorkflow' });
+
+class InstanceClass {
+  async helloFunction(user: string) {
+    const rows = await KnexDataSource.client<greetings>('greetings')
+      .insert({ name: user, greet_count: 1 })
+      .onConflict('name')
+      .merge({ greet_count: KnexDataSource.client.raw('greetings.greet_count + 1') })
+      .returning('greet_count');
+    const row = rows.length > 0 ? rows[0] : undefined;
+
+    return { user, greet_count: row?.greet_count };
+  }
+
+  async readFunction(user: string) {
+    const row = await KnexDataSource.client<greetings>('greetings').select('greet_count').where('name', user).first();
+    return { user, greet_count: row?.greet_count };
+  }
+}
+
+InstanceClass.prototype.helloFunction = dataSource.register(InstanceClass.prototype.helloFunction, 'helloFunction');
+InstanceClass.prototype.readFunction = dataSource.register(InstanceClass.prototype.readFunction, 'readFunction');
+
+async function instanceWorkflow(user: string) {
+  const instance = new InstanceClass();
+  const result = await instance.helloFunction(user);
+  const readResult = await instance.readFunction(user);
+  return [result, readResult];
+}
+
+const regInstanceWorkflow = DBOS.registerWorkflow(instanceWorkflow, { name: 'instanceWorkflow' });
