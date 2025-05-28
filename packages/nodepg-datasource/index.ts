11d21225a7870277f769cc6d16b19786facae350
Harry Pierson <harry.pierson@dbos.dev>
2025-05-28 14:02:37 -0700

    rework existing DataSources to adhere closer to original UserDB executiion flow

--- packages/nodepg-datasource/index.ts
+++ packages/nodepg-datasource/index.ts
@@ -1,8 +1,12 @@
 // using https://github.com/brianc/node-postgres
 
-import { DBOS, type DBOSTransactionalDataSource } from '@dbos-inc/dbos-sdk';
+import { DBOS, DBOSWorkflowConflictError, type DBOSTransactionalDataSource } from '@dbos-inc/dbos-sdk';
 import { Client, type ClientBase, type ClientConfig, DatabaseError, Pool, type PoolConfig } from 'pg';
 import { AsyncLocalStorage } from 'node:async_hooks';
+
+interface NodePostgresDataSourceContext {
+  client: ClientBase;
+}
 
 export const IsolationLevel = Object.freeze({
   serializable: 'SERIALIZABLE',
@@ -13,18 +17,84 @@ export const IsolationLevel = Object.freeze({
 
 type ValuesOf<T> = T[keyof T];
 
-type IsolationLevel = ValuesOf<typeof IsolationLevel>;
 export interface NodePostgresTransactionOptions {
-  isolationLevel?: IsolationLevel;
+  isolationLevel?: ValuesOf<typeof IsolationLevel>;
   readOnly?: boolean;
 }
 
-interface NodePostgresDataSourceContext {
-  client: ClientBase;
+function getErrorCode(error: unknown) {
+  return error instanceof DatabaseError ? error.code : undefined;
 }
-const asyncLocalCtx = new AsyncLocalStorage<NodePostgresDataSourceContext>();
+
+function JsonReviver(_key: string, value: any): any {
+  if (value && typeof value === 'object' && 'json_type' in value && 'json_value' in value) {
+    if (value.json_type === 'Date') {
+      return new Date(value.json_value);
+    }
+    if (value.json_type === 'BigInt') {
+      return BigInt(value.json_value);
+    }
+  }
+  return value;
+}
+
+function JsonReplacer(_key: string, value: any): any {
+  if (value instanceof Date) {
+    return {
+      json_type: 'Date',
+      json_value: value.toISOString(),
+    };
+  }
+  if (typeof value === 'bigint') {
+    return {
+      json_type: 'BigInt',
+      json_value: value.toString(),
+    };
+  }
+  return value;
+}
 
 export class NodePostgresDataSource implements DBOSTransactionalDataSource {
+  static readonly #asyncLocalCtx = new AsyncLocalStorage<NodePostgresDataSourceContext>();
+
+  static async runTxStep<T>(
+    callback: () => Promise<T>,
+    funcName: string,
+    options: { dsName?: string; config?: NodePostgresTransactionOptions } = {},
+  ) {
+    return await DBOS.runAsWorkflowTransaction(callback, funcName, options);
+  }
+
+  static get client(): ClientBase {
+    if (!DBOS.isInTransaction()) {
+      throw new Error('invalid use of PostgresDataSource.client outside of a DBOS transaction.');
+    }
+    const ctx = NodePostgresDataSource.#asyncLocalCtx.getStore();
+    if (!ctx) {
+      throw new Error('No async local context found.');
+    }
+    return ctx.client;
+  }
+
+  static async configure(config: ClientConfig): Promise<void> {
+    const client = new Client(config);
+    try {
+      await client.connect();
+      await client.query(
+        /*sql*/
+        `CREATE SCHEMA IF NOT EXISTS dbos;
+         CREATE TABLE IF NOT EXISTS dbos.transaction_outputs (
+            workflow_id TEXT NOT NULL,
+            function_num INT NOT NULL,
+            output TEXT,
+            created_at BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM now())*1000)::bigint,
+            PRIMARY KEY (workflow_id, function_num));`,
+      );
+    } finally {
+      await client.end();
+    }
+  }
+
   readonly name: string;
   readonly dsType = 'NodePostgresDataSource';
   readonly #pool: Pool;
@@ -34,23 +104,12 @@ export class NodePostgresDataSource implements DBOSTransactionalDataSource {
     this.#pool = new Pool(config);
   }
 
-  static get client(): ClientBase {
-    if (!DBOS.isInTransaction()) {
-      throw new Error('invalid use of PostgresDataSource.client outside of a DBOS transaction.');
-    }
-    const ctx = asyncLocalCtx.getStore();
-    if (!ctx) {
-      throw new Error('No async local context found.');
-    }
-    return ctx.client;
+  initialize(): Promise<void> {
+    return Promise.resolve();
   }
 
-  static async runTxStep<T>(
-    callback: () => Promise<T>,
-    funcName: string,
-    options: { dsName?: string; config?: NodePostgresTransactionOptions } = {},
-  ) {
-    return await DBOS.runAsWorkflowTransaction(callback, funcName, options);
+  destroy(): Promise<void> {
+    return this.#pool.end();
   }
 
   async runTxStep<T>(callback: () => Promise<T>, funcName: string, config?: NodePostgresTransactionOptions) {
@@ -65,12 +124,62 @@ export class NodePostgresDataSource implements DBOSTransactionalDataSource {
     return DBOS.registerTransaction(this.name, func, { name }, config);
   }
 
-  initialize(): Promise<void> {
-    return Promise.resolve();
+  static async #checkExecution(
+    client: ClientBase,
+    workflowID: string,
+    functionNum: number,
+  ): Promise<{ output: string | null } | undefined> {
+    const { rows } = await client.query<{ output: string }> /*sql*/(
+      `SELECT output FROM dbos.transaction_outputs
+       WHERE workflow_id = $1 AND function_num = $2`,
+      [workflowID, functionNum],
+    );
+    return rows.length > 0 ? { output: rows[0].output } : undefined;
   }
 
-  destroy(): Promise<void> {
-    return this.#pool.end();
+  static async #recordOutput(
+    client: ClientBase,
+    workflowID: string,
+    functionNum: number,
+    output: string | null,
+  ): Promise<void> {
+    try {
+      await client.query(
+        /*sql*/
+        `INSERT INTO dbos.transaction_outputs (workflow_id, function_num, output) 
+         VALUES ($1, $2, $3)`,
+        [workflowID, functionNum, output],
+      );
+    } catch (error) {
+      // 24505 is a duplicate key error in PostgreSQL
+      if (getErrorCode(error) === '23505') {
+        throw new DBOSWorkflowConflictError(workflowID);
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async #transaction<Return>(
+    func: (client: ClientBase) => Promise<Return>,
+    config: NodePostgresTransactionOptions = {},
+  ): Promise<Return> {
+    const isolationLevel = config?.isolationLevel ? `ISOLATION LEVEL ${config.isolationLevel}` : '';
+    const readOnly = config?.readOnly ?? false;
+    const accessMode = config?.readOnly === undefined ? '' : readOnly ? 'READ ONLY' : 'READ WRITE';
+
+    const client = await this.#pool.connect();
+    try {
+      await client.query(/*sql*/ `BEGIN ${isolationLevel} ${accessMode}`);
+      const result = await func(client);
+      await client.query(/*sql*/ `COMMIT`);
+      return result;
+    } catch (error) {
+      await client.query(/*sql*/ `ROLLBACK`);
+      throw error;
+    } finally {
+      client.release();
+    }
   }
 
   async invokeTransactionFunction<This, Args extends unknown[], Return>(
@@ -88,82 +197,71 @@ export class NodePostgresDataSource implements DBOSTransactionalDataSource {
       throw new Error('Function Number is not set.');
     }
 
-    return this.#runLocal(() => func.call(target, ...args), workflowID, functionNum, config);
-  }
-
-  async #runLocal<Return>(
-    func: () => Promise<Return>,
-    workflowID: string,
-    functionNum: number,
-    config: NodePostgresTransactionOptions | undefined,
-  ): Promise<Return> {
-    const isolationLevel = config?.isolationLevel ? `ISOLATION LEVEL ${config.isolationLevel}` : '';
     const readOnly = config?.readOnly ?? false;
-    const accessMode = config?.readOnly === undefined ? '' : readOnly ? 'READ ONLY' : 'READ WRITE';
+    let retryWaitMS = 1;
+    const backoffFactor = 1.5;
+    const maxRetryWaitMS = 2000;
 
     while (true) {
-      if (!readOnly) {
-        const { rows } = await this.#pool.query<{ output: string }>(
-          /*sql*/ `SELECT output FROM dbos.transaction_outputs
-                 WHERE workflow_id = $1 AND function_num = $2`,
-          [workflowID, functionNum],
-        );
-        if (rows.length > 0) {
-          return JSON.parse(rows[0].output) as Return;
-        }
-      }
-
-      const client = await this.#pool.connect();
       try {
-        await client.query(/*sql*/ `BEGIN ${isolationLevel} ${accessMode}`);
-
-        const output = await asyncLocalCtx.run({ client }, func);
-
-        if (!readOnly) {
-          try {
-            await client.query(
-              /*sql*/
-              `INSERT INTO dbos.transaction_outputs (workflow_id, function_num, output) VALUES ($1, $2, $3)`,
-              [workflowID, functionNum, JSON.stringify(output)],
-            );
-          } catch (error) {
-            // 23505 is a duplicate key error
-            if (error instanceof DatabaseError && error.code === '23505') {
-              await client.query(/*sql*/ `ROLLBACK`);
-              continue;
-            } else {
-              throw error;
+        const result = await this.#transaction<Return>(
+          async (client) => {
+            // Check to see if this tx has already been executed
+            const previousResult = readOnly
+              ? undefined
+              : await NodePostgresDataSource.#checkExecution(client, workflowID, functionNum);
+            if (previousResult) {
+              return (previousResult.output ? JSON.parse(previousResult.output, JsonReviver) : null) as Return;
             }
-          }
-        }
 
-        await client.query(/*sql*/ `COMMIT`);
-        return output;
+            // execute user's transaction function
+            const result = await NodePostgresDataSource.#asyncLocalCtx.run({ client }, async () => {
+              return (await func.call(target, ...args)) as Return;
+            });
+
+            // save the output of read/write transactions
+            if (!readOnly) {
+              await NodePostgresDataSource.#recordOutput(
+                client,
+                workflowID,
+                functionNum,
+                JSON.stringify(result, JsonReplacer),
+              );
+
+              // Note, existing code wraps #recordOutput call in a try/catch block that
+              // converts DB error with code 25P02 to DBOSFailedSqlTransactionError.
+              // However, existing code doesn't make any logic decisions based on that error type.
+              // DBOSFailedSqlTransactionError does stored WF ID and function name, so I assume that info is logged out somewhere
+            }
+
+            return result;
+          },
+          { isolationLevel: config?.isolationLevel, readOnly: config?.readOnly },
+        );
+        // TODO: span.setStatus({ code: SpanStatusCode.OK });
+        // TODO: this.tracer.endSpan(span);
+
+        return result;
       } catch (error) {
-        await client.query(/*sql*/ `ROLLBACK`);
-        throw error;
-      } finally {
-        client.release();
-      }
-    }
-  }
+        if (getErrorCode(error) === '40001') {
+          // 400001 is a serialization failure in PostgreSQL
 
-  static async configure(config: ClientConfig): Promise<void> {
-    const client = new Client(config);
-    try {
-      await client.connect();
-      await client.query(
-        /*sql*/
-        `CREATE SCHEMA IF NOT EXISTS dbos;
-                     CREATE TABLE IF NOT EXISTS dbos.transaction_outputs (
-                        workflow_id TEXT NOT NULL,
-                        function_num INT NOT NULL,
-                        output TEXT,
-                        created_at BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM now())*1000)::bigint,
-                        PRIMARY KEY (workflow_id, function_num));`,
-      );
-    } finally {
-      await client.end();
+          // TODO: span.addEvent('TXN SERIALIZATION FAILURE', { retryWaitMillis: retryWaitMillis }, performance.now());
+          await new Promise((resolve) => setTimeout(resolve, retryWaitMS));
+          retryWaitMS = Math.min(retryWaitMS * backoffFactor, maxRetryWaitMS);
+          continue;
+        } else {
+          // TODO: span.setStatus({ code: SpanStatusCode.ERROR, message: e.message });
+          // TODO: this.tracer.endSpan(span);
+
+          // TODO: currently, we are *not* recording errors in the txOutput table.
+          // For normal execution, this is fine because we also store tx step results (output and error) in the sysdb operation output table.
+          // However, I'm concerned that we have a dueling execution hole where one tx fails while another succeeds.
+          // This implies that we can end up in a situation where the step output records an error but the txOutput table records success.
+
+          throw error;
+        }
+      }
     }
   }
 }
--- packages/postgres-datasource/index.ts
+++ packages/postgres-datasource/index.ts
@@ -1,16 +1,13 @@
 // using https://github.com/porsager/postgres
 
 import postgres, { type Sql } from 'postgres';
-import { DBOS, type DBOSTransactionalDataSource } from '@dbos-inc/dbos-sdk';
+import { DBOS, type DBOSTransactionalDataSource, DBOSWorkflowConflictError } from '@dbos-inc/dbos-sdk';
 import { AsyncLocalStorage } from 'node:async_hooks';
 
 interface PostgresDataSourceContext {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   client: postgres.TransactionSql<{}>;
 }
-const asyncLocalCtx = new AsyncLocalStorage<PostgresDataSourceContext>();
-
-class TxOutputDuplicateKeyError extends Error {}
 
 export const IsolationLevel = Object.freeze({
   serializable: 'SERIALIZABLE',
@@ -26,7 +23,78 @@ export interface PostgresTransactionOptions {
   readOnly?: boolean;
 }
 
+function getErrorCode(error: unknown) {
+  return error instanceof postgres.PostgresError ? error.code : undefined;
+}
+
+function JsonReviver(_key: string, value: any): any {
+  if (value && typeof value === 'object' && 'json_type' in value && 'json_value' in value) {
+    if (value.json_type === 'Date') {
+      return new Date(value.json_value);
+    }
+    if (value.json_type === 'BigInt') {
+      return BigInt(value.json_value);
+    }
+  }
+  return value;
+}
+
+function JsonReplacer(_key: string, value: any): any {
+  if (value instanceof Date) {
+    return {
+      json_type: 'Date',
+      json_value: value.toISOString(),
+    };
+  }
+  if (typeof value === 'bigint') {
+    return {
+      json_type: 'BigInt',
+      json_value: value.toString(),
+    };
+  }
+  return value;
+}
+
 export class PostgresDataSource implements DBOSTransactionalDataSource {
+  static readonly #asyncLocalCtx = new AsyncLocalStorage<PostgresDataSourceContext>();
+
+  static async runTxStep<T>(
+    callback: () => Promise<T>,
+    funcName: string,
+    options: { dsName?: string; config?: PostgresTransactionOptions } = {},
+  ) {
+    return await DBOS.runAsWorkflowTransaction(callback, funcName, options);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  static get client(): postgres.TransactionSql<{}> {
+    if (!DBOS.isInTransaction()) {
+      throw new Error('invalid use of PostgresDataSource.client outside of a DBOS transaction.');
+    }
+    const ctx = PostgresDataSource.#asyncLocalCtx.getStore();
+    if (!ctx) {
+      throw new Error('No async local context found.');
+    }
+    return ctx.client;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  static async configure(options: postgres.Options<{}> = {}): Promise<void> {
+    const pg = postgres({ ...options, onnotice: () => {} });
+    try {
+      await pg/*sql*/ `
+        CREATE SCHEMA IF NOT EXISTS dbos;
+        CREATE TABLE IF NOT EXISTS dbos.transaction_outputs (
+            workflow_id TEXT NOT NULL,
+            function_num INT NOT NULL,
+            output TEXT,
+            created_at BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM now())*1000)::bigint,
+            PRIMARY KEY (workflow_id, function_num));`.simple();
+    } finally {
+      await pg.end();
+    }
+  }
+
   readonly name: string;
   readonly dsType = 'PostgresDataSource';
   readonly #db: Sql;
@@ -37,24 +105,12 @@ export class PostgresDataSource implements DBOSTransactionalDataSource {
     this.#db = postgres(options);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  static get client(): postgres.TransactionSql<{}> {
-    if (!DBOS.isInTransaction()) {
-      throw new Error('invalid use of PostgresDataSource.client outside of a DBOS transaction.');
-    }
-    const ctx = asyncLocalCtx.getStore();
-    if (!ctx) {
-      throw new Error('No async local context found.');
-    }
-    return ctx.client;
+  initialize(): Promise<void> {
+    return Promise.resolve();
   }
 
-  static async runTxStep<T>(
-    callback: () => Promise<T>,
-    funcName: string,
-    options: { dsName?: string; config?: PostgresTransactionOptions } = {},
-  ) {
-    return await DBOS.runAsWorkflowTransaction(callback, funcName, options);
+  destroy(): Promise<void> {
+    return this.#db.end();
   }
 
   async runTxStep<T>(callback: () => Promise<T>, funcName: string, config?: PostgresTransactionOptions) {
@@ -69,20 +125,39 @@ export class PostgresDataSource implements DBOSTransactionalDataSource {
     return DBOS.registerTransaction(this.name, func, { name }, config);
   }
 
-  initialize(): Promise<void> {
-    return Promise.resolve();
-  }
-
-  destroy(): Promise<void> {
-    return this.#db.end();
-  }
-
-  async #getResult(workflowID: string, functionNum: number): Promise<string | undefined> {
+  static async #checkExecution(
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    client: postgres.TransactionSql<{}>,
+    workflowID: string,
+    functionNum: number,
+  ): Promise<{ output: string | null } | undefined> {
     type Result = { output: string };
-    const result = await this.#db<Result[]>/*sql*/ `
+    const result = await client<Result[]>/*sql*/ `
             SELECT output FROM dbos.transaction_outputs
             WHERE workflow_id = ${workflowID} AND function_num = ${functionNum}`;
-    return result[0]?.output;
+
+    return result.length > 0 ? { output: result[0].output } : undefined;
+  }
+
+  static async #recordOutput(
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    client: postgres.TransactionSql<{}>,
+    workflowID: string,
+    functionNum: number,
+    output: string | null,
+  ): Promise<void> {
+    try {
+      await client/*sql*/ `
+        INSERT INTO dbos.transaction_outputs (workflow_id, function_num, output)
+        VALUES (${workflowID}, ${functionNum}, ${output})`;
+    } catch (error) {
+      // 24505 is a duplicate key error in PostgreSQL
+      if (getErrorCode(error) === '23505') {
+        throw new DBOSWorkflowConflictError(workflowID);
+      } else {
+        throw error;
+      }
+    }
   }
 
   async invokeTransactionFunction<This, Args extends unknown[], Return>(
@@ -99,99 +174,72 @@ export class PostgresDataSource implements DBOSTransactionalDataSource {
     if (functionNum === undefined) {
       throw new Error('Function Number is not set.');
     }
+
     const isolationLevel = config?.isolationLevel ? `ISOLATION LEVEL ${config.isolationLevel}` : '';
     const readOnly = config?.readOnly ?? false;
     const accessMode = config?.readOnly === undefined ? '' : readOnly ? 'READ ONLY' : 'READ WRITE';
 
+    let retryWaitMS = 1;
+    const backoffFactor = 1.5;
+    const maxRetryWaitMS = 2000;
+
     while (true) {
-      if (!readOnly) {
-        const result = await this.#getResult(workflowID, functionNum);
-        if (result) {
-          return JSON.parse(result) as Return;
-        }
-      }
-
       try {
-        const output = await this.#db.begin<Return>(`${isolationLevel} ${accessMode}`, async (client) => {
-          const output = await asyncLocalCtx.run({ client }, async () => {
-            return await func.call(target, ...args);
-          });
-
-          if (!readOnly) {
-            try {
-              await client/*sql*/ `
-                            INSERT INTO dbos.transaction_outputs (workflow_id, function_num, output)
-                            VALUES (${workflowID}, ${functionNum}, ${JSON.stringify(output)})`;
-            } catch (error) {
-              // 23505 is a duplicate key error
-              if (error instanceof postgres.PostgresError && error.code === '23505') {
-                // I dislike using try/catch for flow control, but we need to throw an error here
-                // in order to trigger transacation rollback
-                throw new TxOutputDuplicateKeyError('Duplicate key error');
-              } else {
-                throw error;
-              }
-            }
+        const result = await this.#db.begin<Return>(`${isolationLevel} ${accessMode}`, async (client) => {
+          // Check to see if this tx has already been executed
+          const previousResult = readOnly
+            ? undefined
+            : await PostgresDataSource.#checkExecution(client, workflowID, functionNum);
+          if (previousResult) {
+            return (previousResult.output ? JSON.parse(previousResult.output, JsonReviver) : null) as Return;
           }
 
-          return output;
-        });
+          // execute user's transaction function
+          const result = await PostgresDataSource.#asyncLocalCtx.run({ client }, async () => {
+            return (await func.call(target, ...args)) as Return;
+          });
 
-        return output as Return;
-      } catch (e) {
-        if (e instanceof TxOutputDuplicateKeyError) {
+          // save the output of read/write transactions
+          if (!readOnly) {
+            await PostgresDataSource.#recordOutput(
+              client,
+              workflowID,
+              functionNum,
+              JSON.stringify(result, JsonReplacer),
+            );
+
+            // Note, existing code wraps #recordOutput call in a try/catch block that
+            // converts DB error with code 25P02 to DBOSFailedSqlTransactionError.
+            // However, existing code doesn't make any logic decisions based on that error type.
+            // DBOSFailedSqlTransactionError does stored WF ID and function name, so I assume that info is logged out somewhere
+          }
+
+          return result;
+        });
+        // TODO: span.setStatus({ code: SpanStatusCode.OK });
+        // TODO: this.tracer.endSpan(span);
+
+        return result as Return;
+      } catch (error) {
+        if (getErrorCode(error) === '40001') {
+          // 400001 is a serialization failure in PostgreSQL
+
+          // TODO: span.addEvent('TXN SERIALIZATION FAILURE', { retryWaitMillis: retryWaitMillis }, performance.now());
+          await new Promise((resolve) => setTimeout(resolve, retryWaitMS));
+          retryWaitMS = Math.min(retryWaitMS * backoffFactor, maxRetryWaitMS);
           continue;
         } else {
-          throw e;
+          // TODO: span.setStatus({ code: SpanStatusCode.ERROR, message: e.message });
+          // TODO: this.tracer.endSpan(span);
+
+          // TODO: currently, we are *not* recording errors in the txOutput table.
+          // For normal execution, this is fine because we also store tx step results (output and error) in the sysdb operation output table.
+          // However, I'm concerned that we have a dueling execution hole where one tx fails while another succeeds.
+          // This implies that we can end up in a situation where the step output records an error but the txOutput table records success.
+
+          throw error;
         }
       }
     }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  static async ensureDatabase(name: string, options: postgres.Options<{}> = {}): Promise<void> {
-    const pg = postgres({ ...options, onnotice: () => {} });
-    try {
-      await ensureDB(pg, name);
-    } finally {
-      await pg.end();
-    }
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  static async configure(options: postgres.Options<{}> = {}): Promise<void> {
-    const pg = postgres({ ...options, onnotice: () => {} });
-    try {
-      await pg/*sql*/ `
-                CREATE SCHEMA IF NOT EXISTS dbos;
-                CREATE TABLE IF NOT EXISTS dbos.transaction_outputs (
-                    workflow_id TEXT NOT NULL,
-                    function_num INT NOT NULL,
-                    output TEXT,
-                    created_at BIGINT NOT NULL DEFAULT (EXTRACT(EPOCH FROM now())*1000)::bigint,
-                    PRIMARY KEY (workflow_id, function_num));`.simple();
-    } finally {
-      await pg.end();
-    }
-  }
-}
-
-// helper functions to create/drop the database
-async function checkDB(sql: Sql, name: string) {
-  const results = await sql/*sql*/ `SELECT 1 FROM pg_database WHERE datname = ${name}`.values();
-  return results.length > 0;
-}
-
-export async function ensureDB(sql: Sql, name: string) {
-  const exists = await checkDB(sql, name);
-  if (!exists) {
-    await sql.unsafe(/*sql*/ `CREATE DATABASE ${name}`).simple();
-  }
-}
-
-export async function dropDB(sql: Sql, name: string) {
-  const exists = await checkDB(sql, name);
-  if (exists) {
-    await sql.unsafe(/*sql*/ `DROP DATABASE ${name}`).simple();
   }
 }
