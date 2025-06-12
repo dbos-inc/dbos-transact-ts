94c673996e646e1a972fca0e40fd22889697efac
Harry Pierson <harry.pierson@dbos.dev>
2025-06-12 09:26:11 -0700

    update wf-scheduled-recover test timeout

--- tests/scheduler/scheduler.test.ts
+++ tests/scheduler/scheduler.test.ts
@@ -174,7 +174,7 @@ describe('scheduled-wf-tests-oaoo', () => {
     } finally {
       await DBOS.shutdown();
     }
-  }, 30000);
+  }, 20000);
 });
 
 describe('scheduled-wf-tests-when-active', () => {
