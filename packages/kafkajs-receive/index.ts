b05d8599e83b9beb75ffcb63b52be47f82c4c988
Harry Pierson <harry.pierson@dbos.dev>
2025-06-23 17:21:32 -0700

    WIP

---
+++ packages/kafkajs-receive/index.ts
@@ -0,0 +1,148 @@
+import { DBOS, DBOSLifecycleCallback } from '@dbos-inc/dbos-sdk';
+import {
+  Kafka as KafkaJS,
+  Consumer,
+  ConsumerConfig,
+  KafkaConfig,
+  KafkaMessage,
+  Message,
+  KafkaJSProtocolError,
+} from 'kafkajs';
+
+type KafkaMessageHandler = (topic: string, partition: number, message: KafkaMessage) => Promise<void>;
+
+const sleepms = (ms: number) => new Promise((r) => setTimeout(r, ms));
+
+interface KafkaMethodConfig {
+  topics?: Array<string | RegExp>;
+  config?: ConsumerConfig;
+  queueName?: string;
+}
+
+interface RetryConfig {
+  maxRetries: number;
+  retryTime: number;
+  multiplier: number;
+}
+
+function safeGroupName(className: string, methodName: string, topics: Array<string | RegExp>) {
+  const safeGroupIdPart = [className, methodName, ...topics]
+    .map((r) => r.toString())
+    .map((r) => r.replaceAll(/[^a-zA-Z0-9\\-]/g, ''))
+    .join('-');
+  return `dbos-kafka-group-${safeGroupIdPart}`.slice(0, 255);
+}
+
+export class KafkaReceiver extends DBOSLifecycleCallback {
+  #kafka: KafkaJS | undefined;
+  readonly #consumers = new Array<Consumer>();
+
+  constructor(
+    private readonly config: KafkaConfig,
+    private readonly retryConfig: RetryConfig = { maxRetries: 5, retryTime: 300, multiplier: 2 },
+  ) {
+    super();
+  }
+
+  override async initialize() {
+    if (this.#kafka === undefined) {
+      this.#kafka = new KafkaJS(this.config);
+    }
+
+    const { maxRetries, multiplier } = this.retryConfig;
+
+    for (const regOp of DBOS.getAssociatedInfo(this)) {
+      const func = regOp.methodReg.registeredFunction as KafkaMessageHandler | undefined;
+      if (func === undefined) {
+        // TODO: log?
+        continue;
+      }
+
+      const methodConfig = regOp.methodConfig as KafkaMethodConfig;
+      const topics = methodConfig.topics ?? [];
+      if (topics.length === 0) {
+        // log?
+        continue;
+      }
+
+      const { name, className } = regOp.methodReg;
+      const config = methodConfig.config ?? { groupId: safeGroupName(className, name, topics) };
+      const consumer = this.#kafka.consumer(config);
+      await consumer.connect();
+
+      // A temporary workaround for https://github.com/tulios/kafkajs/pull/1558 until it gets fixed
+      // If topic autocreation is on and you try to subscribe to a nonexistent topic, KafkaJS should retry until the topic is created.
+      // However, it has a bug where it won't. Thus, we retry instead.
+      let { retryTime } = this.retryConfig;
+      for (let i = 1; i <= maxRetries; i++) {
+        try {
+          await consumer.subscribe({ topics: topics, fromBeginning: true });
+        } catch (e) {
+          if (e instanceof KafkaJSProtocolError && e.code === 3 && i < maxRetries) {
+            await sleepms(retryTime);
+            retryTime *= multiplier;
+          } else {
+            throw e;
+          }
+        }
+      }
+
+      await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          try {
+            const workflowID = `kafka-unique-id-${topic}-${partition}-${config.groupId}-${message.offset}`;
+            const wfParams = { workflowID, queueName: methodConfig.queueName };
+            await DBOS.startWorkflow(func, wfParams)(topic, partition, message);
+          } catch (e) {
+            const message = e instanceof Error ? e.message : String(e);
+            DBOS.logger.error(`Error processing Kafka message ${message}`);
+            throw e;
+          }
+        },
+      });
+
+      this.#consumers.push(consumer);
+    }
+  }
+
+  override async destroy() {
+    const disconnectPromises = this.#consumers.splice(0, this.#consumers.length).map((c) => c.disconnect());
+    this.#kafka = undefined;
+    await Promise.allSettled(disconnectPromises);
+  }
+
+  override logRegisteredEndpoints() {
+    DBOS.logger.info('KafkaJS receiver endpoints:');
+
+    const regOps = DBOS.getAssociatedInfo(this);
+    for (const regOp of regOps) {
+      const methodConfig = regOp.methodConfig as KafkaMethodConfig;
+      const { name, className } = regOp.methodReg;
+      for (const topic of methodConfig.topics ?? []) {
+        DBOS.logger.info(`    ${topic} -> ${className}.${name}`);
+      }
+    }
+  }
+
+  eventConsumer(topics: string | RegExp | Array<string | RegExp>, config?: ConsumerConfig, queueName?: string) {
+    const $this = this;
+    function methodDecorator<This, Args extends [Message], Return>(
+      target: object,
+      propertyKey: PropertyKey,
+      descriptor: TypedPropertyDescriptor<(this: This, ...args: Args) => Promise<Return>>,
+    ) {
+      const { regInfo } = DBOS.associateFunctionWithInfo($this, descriptor.value!, {
+        classOrInst: target,
+        name: String(propertyKey),
+      });
+      const kafkaRegInfo = regInfo as KafkaMethodConfig;
+
+      kafkaRegInfo.topics = Array.isArray(topics) ? topics : [topics];
+      kafkaRegInfo.config = config;
+      kafkaRegInfo.queueName = queueName;
+
+      return descriptor;
+    }
+    return methodDecorator;
+  }
+}
