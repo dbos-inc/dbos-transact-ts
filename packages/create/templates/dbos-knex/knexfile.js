b7781bb0c9f3f3b7705d7492cdd5719901d5ab9c
Qian Li <qian.li@dbos.dev>
2025-07-28 15:11:11 -0700

    Use systemDatabaseUrl in setConfig

--- packages/create/templates/dbos-knex/knexfile.js
+++ packages/create/templates/dbos-knex/knexfile.js
@@ -1,7 +1,6 @@
-const { readConfigFile, getDatabaseUrl } = require('@dbos-inc/dbos-sdk');
-
-const dbosConfig = readConfigFile();
-const databaseUrl = getDatabaseUrl(dbosConfig);
+const databaseUrl =
+  process.env.DBOS_DATABASE_URL ||
+  `postgresql://${process.env.PGUSER || 'postgres'}:${process.env.PGPASSWORD || 'dbos'}@${process.env.PGHOST || 'localhost'}:${process.env.PGPORT || '5432'}/${process.env.PGDATABASE || 'dbos_knex'}`;
 
 const config = {
   client: 'pg',
