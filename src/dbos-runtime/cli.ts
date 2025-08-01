aeffc8dc24efa5ee0cc1c7a5fb4adf6f41f521d1
Peter Kraft <petereliaskraft@gmail.com>
2025-08-01 11:44:00 -0700

    nit

--- src/dbos-runtime/cli.ts
+++ src/dbos-runtime/cli.ts
@@ -96,7 +96,7 @@ program
 
 program
   .command('schema')
-  .description('Create the system database and its internal tables')
+  .description('Create the DBOS system database and its internal tables')
   .argument('[systemDatabaseUrl]', 'System database URL')
   .option('-d, --appDir <string>', 'Specify the application root directory')
   .action(async (systemDatabaseUrl: string | undefined, options: { appDir?: string }) => {
