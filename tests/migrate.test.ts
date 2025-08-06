9c95e09874ace258038e8bb87cd15359fe7dcb31
Peter Kraft <petereliaskraft@gmail.com>
2025-08-06 15:26:16 -0700

    test

--- tests/migrate.test.ts
+++ tests/migrate.test.ts
@@ -2,6 +2,8 @@ import { execSync } from 'child_process';
 import { Client } from 'pg';
 import { generateDBOSTestConfig } from './helpers';
 import { ExistenceCheck } from '../src/system_database';
+import { DBOS } from '../src';
+import { DBOSConfig } from '../dist/src';
 
 describe('schema-command-tests', () => {
   test('test schema command with system database URL argument', async () => {
@@ -34,5 +36,80 @@ describe('schema-command-tests', () => {
     expect(tableExists.rows[0].exists).toBe(true);
 
     await pgSystemClient.end();
+  });
+
+  test('test schema command with permissions for application role', async () => {
+    const databaseName = 'schema_migrate_test';
+    const roleName = 'schema-migrate-test-role';
+    const rolePassword = 'schema_migrate_test_password';
+
+    const config = generateDBOSTestConfig();
+    const baseUrl = new URL(config.systemDatabaseUrl!);
+    const dbUrl = new URL(baseUrl.toString());
+    dbUrl.pathname = `/${databaseName}`;
+
+    // Drop the DBOS database if it exists. Create a test role with no permissions.
+    const pgClient = new Client({
+      connectionString: baseUrl.toString(),
+    });
+    await pgClient.connect();
+    await pgClient.query(`DROP DATABASE IF EXISTS ${databaseName} WITH (FORCE)`);
+    await pgClient.query(`DROP ROLE IF EXISTS "${roleName}"`);
+    await pgClient.query(`CREATE ROLE "${roleName}" WITH LOGIN PASSWORD '${rolePassword}'`);
+    await pgClient.end();
+
+    // Using the admin role, create the DBOS database and verify it exists.
+    // Set permissions for the test role.
+    execSync(`npx dbos schema ${dbUrl.toString()}`, {
+      env: process.env,
+      stdio: 'inherit',
+    });
+
+    const pgVerifyClient = new Client({
+      connectionString: baseUrl.toString(),
+    });
+    await pgVerifyClient.connect();
+    const result = await pgVerifyClient.query<{ count: string }>(
+      `SELECT COUNT(*) FROM pg_database WHERE datname = $1`,
+      [databaseName],
+    );
+    expect(result.rows[0].count).toBe('1');
+    await pgVerifyClient.end();
+
+    // Initialize DBOS with the test role. Verify various operations work.
+    const testDbUrl = new URL(dbUrl.toString());
+    testDbUrl.username = roleName;
+    testDbUrl.password = rolePassword;
+
+    await DBOS.shutdown();
+    const roleConfig: DBOSConfig = {
+      name: 'schema-migrate-test',
+      databaseUrl: testDbUrl.toString(),
+      systemDatabaseUrl: testDbUrl.toString(),
+    };
+    DBOS.setConfig(roleConfig);
+
+    const testWorkflow = DBOS.registerWorkflow(
+      async () => {
+        const id = DBOS.workflowID;
+        expect(id).toBeTruthy();
+        await DBOS.setEvent(id!, id!);
+        return id!;
+      },
+      { name: 'migrate-test-workflow' },
+    );
+
+    await DBOS.launch();
+
+    const workflowId = await testWorkflow();
+    expect(workflowId).toBeTruthy();
+    expect(await DBOS.getEvent(workflowId, workflowId)).toBe(workflowId);
+
+    const steps = await DBOS.listWorkflowSteps(workflowId);
+    expect(steps).toHaveLength(2);
+    expect(steps![0].name).toBe('testTransaction');
+    expect(steps![1].name).toBe('setEvent');
+
+    await DBOS.shutdown();
   });
 });
