08267b6331b08e994f1cbb89b3b3bbb15a8a8656
Peter Kraft <petereliaskraft@gmail.com>
2025-09-24 12:58:08 -0700

    remove

--- tests/bundler-test/webpack.config.js
+++ tests/bundler-test/webpack.config.js
@@ -24,17 +24,6 @@ module.exports = {
       },
     ],
   },
-  externals: {
-    // Keep these optional dependencies of Knex as external dependencies to avoid bundling issues
-    sqlite3: 'commonjs sqlite3',
-    mysql: 'commonjs mysql',
-    mysql2: 'commonjs mysql2',
-    oracledb: 'commonjs oracledb',
-    mssql: 'commonjs mssql',
-    'better-sqlite3': 'commonjs better-sqlite3',
-    tedious: 'commonjs tedious',
-    'pg-query-stream': 'commonjs pg-query-stream',
-  },
   optimization: {
     minimize: false, // Disable minification for easier debugging
   },
