8a3c11b55326e7fd19041f017644a617c2ec1980
Peter Kraft <petereliaskraft@gmail.com>
2025-09-25 15:10:15 -0700

    cleanup

--- src/telemetry/logs.ts
+++ src/telemetry/logs.ts
@@ -1,11 +1,39 @@
 import { transports, createLogger, format, Logger as IWinstonLogger } from 'winston';
 import TransportStream = require('winston-transport'); // eslint-disable-line @typescript-eslint/no-require-imports
-import { Logger as OTelLogger, LogAttributes, SeverityNumber } from '@opentelemetry/api-logs';
-import { LogRecord, LoggerProvider } from '@opentelemetry/sdk-logs';
+import type { Logger as OTelLogger, LogAttributes } from '@opentelemetry/api-logs';
+import { LoggerProvider, type LogRecord } from '@opentelemetry/sdk-logs';
 import { TelemetryCollector } from './collector';
 import { DBOSJSON, globalParams, interceptStreams } from '../utils';
 import { LoggerConfig } from '../dbos-executor';
 import { DBOSSpan } from './traces';
+
+export enum SeverityNumber {
+  UNSPECIFIED = 0,
+  TRACE = 1,
+  TRACE2 = 2,
+  TRACE3 = 3,
+  TRACE4 = 4,
+  DEBUG = 5,
+  DEBUG2 = 6,
+  DEBUG3 = 7,
+  DEBUG4 = 8,
+  INFO = 9,
+  INFO2 = 10,
+  INFO3 = 11,
+  INFO4 = 12,
+  WARN = 13,
+  WARN2 = 14,
+  WARN3 = 15,
+  WARN4 = 16,
+  ERROR = 17,
+  ERROR2 = 18,
+  ERROR3 = 19,
+  ERROR4 = 20,
+  FATAL = 21,
+  FATAL2 = 22,
+  FATAL3 = 23,
+  FATAL4 = 24,
+}
 
 /*****************/
 /* GLOBAL LOGGER */
