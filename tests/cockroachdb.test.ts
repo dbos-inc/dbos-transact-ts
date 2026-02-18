21b71e8c8fa32d8bcb48fc8dd86e603fa723a587
Peter Kraft <petereliaskraft@gmail.com>
2026-02-18 11:53:22 -0800

    crdb

---
+++ tests/cockroachdb.test.ts
@@ -0,0 +1,40 @@
+import { DBOS } from '../src/';
+import { DBOSConfig } from '../src/dbos-executor';
+import { Client } from 'pg';
+
+const cockroachdbUrl = process.env.DBOS_COCKROACHDB_URL;
+const describeIf = cockroachdbUrl ? describe : describe.skip;
+
+describeIf('cockroachdb', () => {
+  let config: DBOSConfig;
+
+  beforeAll(async () => {
+    const url = new URL(cockroachdbUrl!);
+    url.pathname = '/dbos_test';
+    const systemDatabaseUrl = url.toString();
+
+    const client = new Client({ connectionString: cockroachdbUrl });
+    await client.connect();
+    await client.query('DROP DATABASE IF EXISTS dbos_test');
+    await client.query('CREATE DATABASE dbos_test');
+    await client.end();
+    config = {
+      name: 'cockroachdb-test',
+      systemDatabaseUrl,
+    };
+    DBOS.setConfig(config);
+  });
+
+  beforeEach(async () => {
+    await DBOS.launch();
+  });
+
+  afterEach(async () => {
+    await DBOS.shutdown();
+  });
+
+  test('launch-and-shutdown', async () => {
+    // DBOS launched successfully against CockroachDB
+    await expect(DBOS.listWorkflows({})).resolves.toBeDefined();
+  });
+});
