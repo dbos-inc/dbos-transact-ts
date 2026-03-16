69ffd2341716ed010d35d37e8e521c83600b13b6
Peter Kraft <petereliaskraft@gmail.com>
2026-03-16 14:39:51 -0700

    nit

--- src/conductor/conductor.ts
+++ src/conductor/conductor.ts
@@ -306,13 +306,13 @@ export class Conductor {
             const getWFMsg = baseMsg as protocol.GetWorkflowRequest;
             let wfOutput: protocol.WorkflowsOutput | undefined = undefined;
             try {
-              const workflows = await this.dbosExec.listWorkflows({
+              const workflow = await this.dbosExec.listWorkflows({
                 workflowIDs: [getWFMsg.workflow_id],
                 loadInput: getWFMsg.load_input ?? true,
                 loadOutput: getWFMsg.load_output ?? true,
               });
-              if (workflows.length > 0) {
-                wfOutput = new protocol.WorkflowsOutput(workflows[0]);
+              if (workflow) {
+                wfOutput = new protocol.WorkflowsOutput(workflow[0]);
               }
             } catch (e) {
               errorMsg = `Exception encountered when getting workflow ${getWFMsg.workflow_id}: ${(e as Error).message}`;
